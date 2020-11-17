587d0b08f1a964362cc45ecb83b81787ec212cf6
DaniilNov <xa86@mail.ru>
2020-11-17 15:59:15 +0300

    first commit

---
+++ src/main/java/com/trade_accounting/models/dto/TypesOfPrices.java
@@ -0,0 +1,16 @@
+package com.trade_accounting.models.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class TypesOfPrices {
+    private long id;
+    private String name;
+    private long sortNumber;
+}
