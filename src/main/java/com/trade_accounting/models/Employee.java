f266088714e79a061976463edc19cf98245f906e
maslak_andrey <masluck@gmail.com>
2020-11-18 14:50:05 +0500

    add annotation v.2

--- src/main/java/com/trade_accounting/models/Employee.java
+++ src/main/java/com/trade_accounting/models/Employee.java
@@ -1,5 +1,6 @@
 package com.trade_accounting.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,24 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "employee")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String lastname;
+
     private String firstname;
+
     private String middlename;
+
     private String sortNumber;
+
     private String phone;
+
     private String inn;
+
     private String description;
+
     private String image;
+
     private String login;
+
     private String password;
+    
     private String email;
 }
