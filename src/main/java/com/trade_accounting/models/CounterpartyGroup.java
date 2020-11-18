2eb6958819065079cf0237d9b3bce4098465fd8b
DaniilNov <xa86@mail.ru>
2020-11-18 09:18:56 +0300

    Добавил аннотации

--- src/main/java/com/trade_accounting/models/CounterpartyGroup.java
+++ src/main/java/com/trade_accounting/models/CounterpartyGroup.java
@@ -3,14 +3,24 @@ package com.trade_accounting.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class CounterpartyGroup {
+
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name = "Name")
     private String name;
+    @Column(name = "sortNumber")
     private String sortNumber;
 
 }
