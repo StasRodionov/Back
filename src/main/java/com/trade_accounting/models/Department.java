99fc074adcf99a0b0d9f00265a9e03536f2b39c6
maslak_andrey <masluck@gmail.com>
2020-11-18 14:48:27 +0500

    add annotation v.2

--- src/main/java/com/trade_accounting/models/Department.java
+++ src/main/java/com/trade_accounting/models/Department.java
@@ -1,5 +1,6 @@
 package com.trade_accounting.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,15 +8,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "department")
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+    
     private String sortNumber;
 }
