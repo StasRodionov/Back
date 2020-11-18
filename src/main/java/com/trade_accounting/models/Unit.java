58fa5f5a96d1b2000efc5afb3f6eb5acabcbb49d
Mrvelik <alihopoi@yandex.ru>
2020-11-18 22:02:48 +0500

    добавил аннотации для класса Unit

--- src/main/java/com/trade_accounting/models/Unit.java
+++ src/main/java/com/trade_accounting/models/Unit.java
@@ -3,12 +3,7 @@ package com.trade_accounting.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -21,9 +16,12 @@ public class Unit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "short_name")
     private String shortName;
 
+    @Column(name = "full_name")
     private String fullName;
-    
+
+    @Column(name = "sort_number")
     private String sortNumber;
 }
