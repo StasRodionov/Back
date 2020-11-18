7d1f24b0a166d8fe9e25fea0dfd334da5ec29f12
Denis Pavlov <pavler495@gmail.com>
2020-11-18 20:23:29 +0300

    Company REST controller - DONE!

---
+++ src/main/java/com/trade_accounting/controllers/rest/CompanyRestController.java
@@ -0,0 +1,34 @@
+package com.trade_accounting.controllers.rest;
+
+import com.trade_accounting.models.Company;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CompanyRestController {
+
+    @GetMapping
+    public Response<Company> getCompany(@PathVariable(name = "id") Long id) {}
+
+    @GetMapping
+    public Response<List<Company>> getCompanies() {}
+
+    //TODO Company - refactor to DTO
+    @PostMapping
+    public Response<?> postCompany(@RequestBody Company company) {}
+
+    //TODO Company - refactor to DTO
+    @PutMapping
+    public Response<?> putCompany(@RequestBody Company company) {}
+
+    @DeleteMapping
+    public Response<?> deleteCompany(@PathVariable(name = "id") Long id) {}
+
+}
