d426f7064a94e017b2b8af25719f2691757811c3
Denis Pavlov <pavler495@gmail.com>
2020-11-18 16:21:34 +0300

    Role/done

---
+++ src/main/java/com/trade_accounting/models/Role.java
@@ -0,0 +1,29 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+
+    private Product sortNumber;
+}
