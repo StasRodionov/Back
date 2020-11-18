91a61edb20681a7697e531bbffdc7dda36478c7a
maslak_andrey <masluck@gmail.com>
2020-11-18 14:13:59 +0500

    add annotation Entity + Id

--- src/main/java/com/trade_accounting/models/Company.java
+++ src/main/java/com/trade_accounting/models/Company.java
@@ -4,11 +4,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Company {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String sortNumber;
