1c82963cab0aaa65454b08dcb1815466fc990e81
maslak_andrey <masluck@gmail.com>
2020-11-18 14:22:26 +0500

    add annotation Entity + Id

--- src/main/java/com/trade_accounting/models/Unit.java
+++ src/main/java/com/trade_accounting/models/Unit.java
@@ -3,10 +3,18 @@ package com.trade_accounting.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
+@Entity
 public class Unit {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String shortName;
     private String fullName;
