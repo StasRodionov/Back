32f5f22d77e0c81c07c2be9b88c824bc9c75d9f9
DaniilNov <xa86@mail.ru>
2020-11-18 10:36:14 +0300

    Поставил аннотации

--- src/main/java/com/trade_accounting/models/Product.java
+++ src/main/java/com/trade_accounting/models/Product.java
@@ -4,11 +4,30 @@ package com.trade_accounting.models;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @NoArgsConstructor
 @Data
 @Entity
 public class Product {
-    private long id;
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "Name")
+    private String name;
+    @Column(name = "Weight", scale = 3)
+    private BigDecimal weight;
+    @Column(name = "Volume", scale = 9)
+    private BigDecimal volume;
+    @Column(name = "PurchasePrice", scale = 2)
+    private BigDecimal purchasePrice;
+    @Column(name = "Description")
+    private String description;
+
 }
