cf946aa99f810ca2650d670826078aeb1f01d92a
maslak_andrey <masluck@gmail.com>
2020-11-18 21:50:05 +0500

    add DepartmentRestController

---
+++ src/main/java/com/trade_accounting/controllers/rest/DepartmentRestController.java
@@ -0,0 +1,37 @@
+package com.trade_accounting.controllers.rest;
+
+import com.trade_accounting.models.Department;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DepartmentRestController {
+
+    //Get all departments
+    @GetMapping
+    public Response<List<Department>> getDepartments() {}
+
+    //Create new department
+    @PostMapping
+    public Response<?> saveDepartment(@RequestBody Department department){}
+
+    //Get department by id
+    @GetMapping
+    public Response<Department> getDepartment(@PathVariable(name = "id") long id) {}
+
+    //Update department by id
+    @PutMapping
+    public Response<?> updateDepartment(@RequestBody Department department) {}
+
+    //Delete department by id
+    @DeleteMapping
+    public Response<?> deleteDepartment(@PathVariable(name = "id") long id) {}
+
+}
