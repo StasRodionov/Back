15ef8fb93dd9f67efbc1e21e7fde119ff0e09c6f
DaniilNov <xa86@mail.ru>
2020-11-19 12:37:33 +0300

    fix

--- src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RequestMapping(value = "/api/warehouse")
+@RequestMapping("/api/warehouse")
 @RestController
 public class WarehouseRestController {
 }
