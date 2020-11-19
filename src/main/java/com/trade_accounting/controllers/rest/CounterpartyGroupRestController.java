a425cce0df047be1d82a35a6d3cd7e5ff398763e
relizerel <relizerel@gmail.com>
2020-11-19 15:18:19 +0200

    Пустой класс с маппингом

--- src/main/java/com/trade_accounting/controllers/rest/CounterpartyGroupRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/CounterpartyGroupRestController.java
@@ -1,16 +1,8 @@
 package com.trade_accounting.controllers.rest;
 
-import com.trade_accounting.models.CounterpartyGroup;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/counterpartygroup")
