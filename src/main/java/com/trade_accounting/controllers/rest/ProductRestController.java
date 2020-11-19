a52e5fca401ed275cb1475ca09b0e361e876ee14
maslak_andrey <masluck@gmail.com>
2020-11-19 14:27:53 +0500

    fix ProductRestController

--- src/main/java/com/trade_accounting/controllers/rest/ProductRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/ProductRestController.java
@@ -1,37 +1,10 @@
 package com.trade_accounting.controllers.rest;
 
-import com.trade_accounting.models.Product;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/api/product")
 public class ProductRestController {
 
-
-    //Get all Products
-    @GetMapping
-    public Response<List<Product>> getProducts() {}
-
-    //Create new Product
-    @PostMapping
-    public Response<?> addProduct(@RequestBody Product product){}
-
-    //Get Product by id
-    @GetMapping
-    public Response<Product> getProduct(@PathVariable(name = "id") Long id) {}
-
-    //Update Product
-    @PutMapping
-    public Response<?> updateProduct(@RequestBody Product product) {}
-
-    //Delete Product by id
-    @DeleteMapping
-    public Response<?> deleteProduct(@PathVariable(name = "id") Long id) {}
 }
