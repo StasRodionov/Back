baedf2442d567edddb65c9c284cdcaf777c80ed6
zsalamandra <zaur121079@yandex.ru>
2020-11-19 13:52:03 +0300

    ConfigurableApplicationContext убрал

--- src/main/java/com/trade_accounting/TradeAccountingApplication.java
+++ src/main/java/com/trade_accounting/TradeAccountingApplication.java
@@ -2,13 +2,12 @@ package com.trade_accounting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TradeAccountingApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext run = SpringApplication.run(TradeAccountingApplication.class, args);
+        SpringApplication.run(TradeAccountingApplication.class, args);
     }
 
 }
