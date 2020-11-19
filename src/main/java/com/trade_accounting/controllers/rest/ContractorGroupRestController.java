6710114f8971dcb9aff3732c8bd201a967a077ba
relizerel <relizerel@gmail.com>
2020-11-19 21:15:09 +0200

    Провел рефактор рест-контроллера

--- src/main/java/com/trade_accounting/controllers/rest/ContractorGroupRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/ContractorGroupRestController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/counterpartygroup")
+@RequestMapping(value = "/api/contractorgroup")
 public class ContractorGroupRestController {
 }
