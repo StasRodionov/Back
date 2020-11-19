23613490b77dad36803b14cb3c3619f4372850cd
DaniilNov <xa86@mail.ru>
2020-11-19 09:26:03 +0300

    second commit

--- src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
@@ -1,8 +1,11 @@
 package com.trade_accounting.controllers.rest;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequestMapping(value = "/api/warehouse")
 @RestController
 public class WarehouseRestController {
 }
