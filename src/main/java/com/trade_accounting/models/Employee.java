76be184a542fbb504e9f83bd0dd8d64ca564308d
Maslak <andrey.maslak@softwareone.com>
2020-11-20 17:45:26 +0500

    add field department

--- src/main/java/com/trade_accounting/models/Employee.java
+++ src/main/java/com/trade_accounting/models/Employee.java
@@ -4,12 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -54,4 +49,7 @@ public class Employee {
     @Column(name = "password")
     private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "departments_id")
+    private Department department;
 }
