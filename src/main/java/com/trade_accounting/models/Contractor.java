90708556e6019dada223a0ed8436878b399b4b05
zsalamandra <zaur121079@yandex.ru>
2020-11-20 15:32:43 +0300

    В таблицу Contractor добавлено поле ContractorGroup

--- src/main/java/com/trade_accounting/models/Contractor.java
+++ src/main/java/com/trade_accounting/models/Contractor.java
@@ -5,9 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Pattern;
 
@@ -49,4 +53,8 @@ public class Contractor {
 
     @Column(name = "comment")
     private String comment;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private ContractorGroup contractorGroup;
+
 }
