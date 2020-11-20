3e48605b750204d040a23e53382b2bcc01c0944e
Denis Pavlov <pavler495@gmail.com>
2020-11-20 17:21:53 +0300

    Add tax systems model

---
+++ src/main/java/com/trade_accounting/models/TaxSystem.java
@@ -0,0 +1,30 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tax_systems")
+public class TaxSystem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "sort_number")
+    private String sortNumber;
+}
