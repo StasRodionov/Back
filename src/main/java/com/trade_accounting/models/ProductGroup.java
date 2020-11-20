1e9d68dd4f46cc4848168a1763718b5eb12d02b2
relizerel <relizerel@gmail.com>
2020-11-20 21:24:15 +0200

    добавил модель

---
+++ src/main/java/com/trade_accounting/models/ProductGroup.java
@@ -0,0 +1,37 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product_groups")
+public class ProductGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "sort_number")
+    private String sortNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="main_group", referencedColumnName = "id")
+    private ProductGroup productGroup;
+}
