53cb287dd7eebde7a379c274f9ad55c1479dd4f9
Maslak <andrey.maslak@softwareone.com>
2020-11-20 19:58:51 +0500

    add correctly annotations v2

--- src/main/java/com/trade_accounting/models/Employee.java
+++ src/main/java/com/trade_accounting/models/Employee.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -56,7 +57,6 @@ public class Employee {
     @Column(name = "password")
     private String password;
 
-    @ManyToOne
-    @JoinColumn(name = "departments_id")
+    @ManyToOne(fetch = FetchType.LAZY)
     private Department department;
 }
