1b9ca8c0f5a158a05aa1dd0030172b1d0a355658
zsalamandra <zaur121079@yandex.ru>
2020-11-22 09:40:25 +0300

    Добавл таблицу Contract (Договор)

---
+++ src/main/java/com/trade_accounting/models/Contract.java
@@ -0,0 +1,51 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contracts")
+public class Contract {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "contract_date", columnDefinition = "date default current_date")
+    private LocalDate contractDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    //@Column(name = "payment_account")
+    //private PaymentAccount paymentAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contractor contractor;
+
+    @Column(name = "sum", columnDefinition = "Money default 0.0")
+    private BigDecimal sum;
+
+    @Column(name = "archive", columnDefinition = "boolean default false")
+    private boolean archive;
+
+    @Column(name = "comment")
+    private String comment;
+}
