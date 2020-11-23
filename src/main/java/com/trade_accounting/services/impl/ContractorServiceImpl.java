5123d4cf436bec5b635102083324753da0f124b1
Maslak <andrey.maslak@softwareone.com>
2020-11-23 16:17:42 +0500

    add annotation Repository and autowared

--- src/main/java/com/trade_accounting/services/impl/ContractorServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/ContractorServiceImpl.java
@@ -1,8 +1,19 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.repositories.ContractorRepository;
 import com.trade_accounting.services.interfaces.ContractorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ContractorServiceImpl implements ContractorService {
+
+    private ContractorRepository contractorRepository;
+
+    @Autowired
+    public void setContractorRepository(ContractorRepository contractorRepository) {
+        this.contractorRepository = contractorRepository;
+    }
 }
