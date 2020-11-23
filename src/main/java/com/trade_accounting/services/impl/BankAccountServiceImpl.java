e2cdc629e1349daf1891b201e485fbca6ec06ea9
Maslak <andrey.maslak@softwareone.com>
2020-11-23 16:21:50 +0500

    add annotation Repository and autowared

--- src/main/java/com/trade_accounting/services/impl/BankAccountServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/BankAccountServiceImpl.java
@@ -1,8 +1,19 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.repositories.BankAccountRepository;
 import com.trade_accounting.services.interfaces.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class BankAccountServiceImpl implements BankAccountService {
+
+    private BankAccountRepository bankAccountRepository;
+
+    @Autowired
+    public void setBankAccountRepository(BankAccountRepository bankAccountRepository) {
+        this.bankAccountRepository = bankAccountRepository;
+    }
 }
