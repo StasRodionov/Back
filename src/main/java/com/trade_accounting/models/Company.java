e88fa6bbfdc96a28c140b05e1f765aed8e69ea7e
Maslak <andrey.maslak@softwareone.com>
2020-11-23 13:10:22 +0500

    change Set to List

--- src/main/java/com/trade_accounting/models/Company.java
+++ src/main/java/com/trade_accounting/models/Company.java
@@ -10,10 +10,10 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.Set;
+import java.util.List;
+
 
 @Data
 @NoArgsConstructor
@@ -72,5 +72,5 @@ public class Company {
     private String stamp;
 
     @OneToMany(fetch = FetchType.LAZY)
-    private Set<BankAccount> bankAccounts;
+    private List<BankAccount> bankAccounts;
 }
