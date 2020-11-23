610400fc9164fc33c2abef845e2fd552705744b7
Maslak <andrey.maslak@softwareone.com>
2020-11-23 16:24:28 +0500

    add annotation Repository and autowared

--- src/main/java/com/trade_accounting/repositories/CompanyRepository.java
+++ src/main/java/com/trade_accounting/repositories/CompanyRepository.java
@@ -3,6 +3,8 @@ package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
--- src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
@@ -1,8 +1,19 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.repositories.CompanyRepository;
 import com.trade_accounting.services.interfaces.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CompanyServiceImpl implements CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    public void setCompanyRepository(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
 }
