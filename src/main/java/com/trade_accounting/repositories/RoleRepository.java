dc03199a8e98c781655af71a6a4853f5a31658d4
Maslak <andrey.maslak@softwareone.com>
2020-11-23 17:09:52 +0500

    add constructor in Service

---
+++ src/main/java/com/trade_accounting/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.trade_accounting.repositories;
+
+import com.trade_accounting.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
