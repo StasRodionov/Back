c7c6cf4b05faca16bec58b0aa0326b8002675062
Maslak <andrey.maslak@softwareone.com>
2020-11-23 16:36:26 +0500

    add constructor in Service

--- src/main/java/com/trade_accounting/services/impl/ContractorServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/ContractorServiceImpl.java
@@ -2,7 +2,6 @@ package com.trade_accounting.services.impl;
 
 import com.trade_accounting.repositories.ContractorRepository;
 import com.trade_accounting.services.interfaces.ContractorService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,10 +9,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class ContractorServiceImpl implements ContractorService {
 
-    private ContractorRepository contractorRepository;
+    private final ContractorRepository contractorRepository;
 
-    @Autowired
-    public void setContractorRepository(ContractorRepository contractorRepository) {
+    public ContractorServiceImpl(ContractorRepository contractorRepository) {
         this.contractorRepository = contractorRepository;
     }
 }
