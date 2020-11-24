dc59e0ecedb22d56dff14758b87436db25a2a240
Maslak <andrey.maslak@softwareone.com>
2020-11-25 00:10:43 +0500

    fix annotation Warehouse

--- src/main/java/com/trade_accounting/models/dto/WarehouseDto.java
+++ src/main/java/com/trade_accounting/models/dto/WarehouseDto.java
@@ -3,9 +3,7 @@ package com.trade_accounting.models.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
-@Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
