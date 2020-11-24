809054151b5d97799df5f0366d1590d277cbb544
zsalamandra <zaur121079@yandex.ru>
2020-11-24 21:52:00 +0300

    add methods in Company service

--- src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
@@ -1,9 +1,11 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.models.Company;
 import com.trade_accounting.repositories.CompanyRepository;
 import com.trade_accounting.services.interfaces.CompanyService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,5 +15,30 @@ public class CompanyServiceImpl implements CompanyService {
 
     public CompanyServiceImpl(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getAll() {
+        return companyRepository.findAll();
+    }
+
+    @Override
+    public Company getById(Long id) {
+        return companyRepository.getOne(id);
+    }
+
+    @Override
+    public Company getByEmail(String email) {
+        return companyRepository.findByEmail(email);
+    }
+
+    @Override
+    public void update() {
+        // todo дописать когда будет готов DTO
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        companyRepository.deleteById(id);
     }
 }
--- src/main/java/com/trade_accounting/services/interfaces/CompanyService.java
+++ src/main/java/com/trade_accounting/services/interfaces/CompanyService.java
@@ -1,4 +1,13 @@
 package com.trade_accounting.services.interfaces;
 
+import com.trade_accounting.models.Company;
+
+import java.util.List;
+
 public interface CompanyService {
+    List<Company> getAll();
+    Company getById(Long id);
+    Company getByEmail(String email);
+    void update(); //todo добавить в параметры CompanyDTO
+    void deleteById(Long id);
 }
