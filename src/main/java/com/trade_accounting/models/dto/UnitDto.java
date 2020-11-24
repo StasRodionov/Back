136912dc06761997a3d4cc9990f992883926fa50
Maslak <andrey.maslak@softwareone.com>
2020-11-25 00:12:30 +0500

    fix annotation UnitDto

--- src/main/java/com/trade_accounting/models/dto/UnitDto.java
+++ src/main/java/com/trade_accounting/models/dto/UnitDto.java
@@ -4,11 +4,9 @@ import com.trade_accounting.models.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
