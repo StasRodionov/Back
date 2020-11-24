1d551bf65c550f9d783a747251e87dba27bd3365
Maslak <andrey.maslak@softwareone.com>
2020-11-24 22:05:03 +0500

    add ProductGroupRestController

---
+++ src/main/java/com/trade_accounting/controllers/rest/ProductGroupRestController.java
@@ -0,0 +1,9 @@
+package com.trade_accounting.controllers.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/productgroup")
+public class ProductGroupRestController {
+}
