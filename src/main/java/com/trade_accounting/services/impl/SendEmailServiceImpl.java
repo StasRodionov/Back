141b54dbb42cf806b7c76567929367a255237425
Maslak <andrey.maslak@softwareone.com>
2020-11-24 23:56:41 +0500

    fix setFrom

--- src/main/java/com/trade_accounting/services/impl/SendEmailServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/SendEmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.trade_accounting.services.impl;
 
 import com.trade_accounting.services.interfaces.SendEmailService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -11,11 +12,14 @@ public class SendEmailServiceImpl implements SendEmailService {
 
     private JavaMailSender javaMailSender = new JavaMailSenderImpl();
 
+    @Value("${spring.mail.username}")
+    private String username;
+
     @Override
     public void sendEmail(String to, String body, String topic) {
         System.out.println("sending email");
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("trade_jm@mail.ru");
+        simpleMailMessage.setFrom(username);
         simpleMailMessage.setTo(to);
         simpleMailMessage.setSubject(topic);
         simpleMailMessage.setText(body);
