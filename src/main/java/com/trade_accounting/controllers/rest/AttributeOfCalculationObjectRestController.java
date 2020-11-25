f72548ef04b7a0de6dc000d14319662725739fff
Maslak <andrey.maslak@softwareone.com>
2020-11-25 13:46:55 +0500

    fix mapping AttributeOfCalculationObjectRestController

--- src/main/java/com/trade_accounting/controllers/rest/AttributeOfCalculationObjectRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/AttributeOfCalculationObjectRestController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/attributeofcalculationobject")
+@RequestMapping("/api/attribute/calculation/object")
 public class AttributeOfCalculationObjectRestController {
 }
