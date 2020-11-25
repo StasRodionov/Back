521e027d38db8aec5c3b5bed5a24cd62df1c3c1f
Maslak <andrey.maslak@softwareone.com>
2020-11-25 13:55:43 +0500

    delete free imports from Unit and UnitDto

--- src/main/java/com/trade_accounting/models/Unit.java
+++ src/main/java/com/trade_accounting/models/Unit.java
@@ -6,13 +6,10 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
--- src/main/java/com/trade_accounting/models/dto/UnitDto.java
+++ src/main/java/com/trade_accounting/models/dto/UnitDto.java
@@ -1,11 +1,8 @@
 package com.trade_accounting.models.dto;
 
-import com.trade_accounting.models.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @NoArgsConstructor
