582595acac5e15ccc3244bf63e9d72759196d1e5
Maslak <andrey.maslak@softwareone.com>
2020-11-25 15:09:04 +0500

    add log to send email

--- src/main/java/com/trade_accounting/services/impl/MailServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/MailServiceImpl.java
@@ -1,29 +1,34 @@
 package com.trade_accounting.services.impl;
 
-import com.trade_accounting.services.interfaces.SendEmailService;
+import com.trade_accounting.services.interfaces.MailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SendEmailServiceImpl implements SendEmailService {
+@Slf4j
+public class MailServiceImpl implements MailService {
 
-    private JavaMailSender javaMailSender = new JavaMailSenderImpl();
+    private final JavaMailSender javaMailSender;
+
+    public MailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     @Value("${spring.mail.username}")
     private String username;
 
     @Override
     public void sendEmail(String to, String body, String topic) {
-        System.out.println("sending email");
+        log.info("sending email");
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setFrom(username);
         simpleMailMessage.setTo(to);
         simpleMailMessage.setSubject(topic);
         simpleMailMessage.setText(body);
         javaMailSender.send(simpleMailMessage);
-        System.out.println("sent email...");
+        log.info("sent email...");
     }
 }
--- src/main/java/com/trade_accounting/services/interfaces/SendEmailService.java
+++
@@ -1,5 +0,0 @@
-package com.trade_accounting.services.interfaces;
-
-public interface SendEmailService {
-    public void sendEmail(String to, String body, String topic);
-}
