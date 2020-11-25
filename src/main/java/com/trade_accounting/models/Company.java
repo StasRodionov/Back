e617ea288766cce85c85222a840de8d55191e394
Maslak <andrey.maslak@softwareone.com>
2020-11-25 16:09:52 +0500

    add field LegalDetail in Company

--- src/main/java/com/trade_accounting/models/Company.java
+++ src/main/java/com/trade_accounting/models/Company.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import java.util.List;
 
@@ -71,4 +72,7 @@ public class Company {
 
     @Column(name = "stamp")
     private String stamp;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private LegalDetail legalDetail;
 }
