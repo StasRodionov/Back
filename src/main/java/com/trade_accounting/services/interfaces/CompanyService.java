c528c09bc24d40361a7d88ca601b0c5bddbbc7d8
zsalamandra <zaur121079@yandex.ru>
2020-11-25 17:37:01 +0300

    CS

--- src/main/java/com/trade_accounting/services/interfaces/CompanyService.java
+++ src/main/java/com/trade_accounting/services/interfaces/CompanyService.java
@@ -5,10 +5,16 @@ import com.trade_accounting.models.Company;
 import java.util.List;
 
 public interface CompanyService {
+
     List<Company> getAll();
+
     Company getById(Long id);
+
     Company getByEmail(String email);
+
     void create(); //todo добавить в параметры CompanyDTO
+
     void update(); //todo добавить в параметры CompanyDTO
+
     void deleteById(Long id);
 }
