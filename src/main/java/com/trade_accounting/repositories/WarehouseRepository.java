ca0b2198d950b8c74dbf3fb9bd953023744c0725
Maslak <andrey.maslak@softwareone.com>
2020-11-27 00:37:23 +0500

    add service for warehouse

--- src/main/java/com/trade_accounting/repositories/WarehouseRepository.java
+++ src/main/java/com/trade_accounting/repositories/WarehouseRepository.java
@@ -1,9 +1,23 @@
 package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.Warehouse;
+import com.trade_accounting.models.dto.WarehouseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
+
+    @Query("select new com.trade_accounting.models.dto.WarehouseDto(e.id, e.name, e.sortNumber, e.address, " +
+            "e.commentToAddress, e.comment) from warehouses as e")
+    List<WarehouseDto> getAll();
+
+    @Query("select new com.trade_accounting.models.dto.WarehouseDto(e.id, e.name, e.sortNumber, e.address, " +
+            "e.commentToAddress, e.comment) from warehouses as e where e.id =: id")
+    WarehouseDto getById(@Param("id") Long id);
+
 }
--- src/main/java/com/trade_accounting/services/impl/WarehouseServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/WarehouseServiceImpl.java
@@ -1,9 +1,13 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.models.Warehouse;
+import com.trade_accounting.models.dto.WarehouseDto;
 import com.trade_accounting.repositories.WarehouseRepository;
 import com.trade_accounting.services.interfaces.WarehouseService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,5 +17,34 @@ public class WarehouseServiceImpl implements WarehouseService {
 
     public WarehouseServiceImpl(WarehouseRepository warehouseRepository) {
         this.warehouseRepository = warehouseRepository;
+    }
+
+    @Override
+    public List<WarehouseDto> getAll() {
+        return warehouseRepository.getAll();
+    }
+
+    @Override
+    public WarehouseDto getById(Long id) {
+        return warehouseRepository.getById(id);
+    }
+
+    @Override
+    public void create(WarehouseDto warehouseDto) {
+        Warehouse warehouse = new Warehouse(warehouseDto.getId(), warehouseDto.getName(), warehouseDto.getSortNumber(),
+                warehouseDto.getAddress(), warehouseDto.getCommentToAddress(), warehouseDto.getComment());
+        warehouseRepository.save(warehouse);
+    }
+
+    @Override
+    public void update(WarehouseDto warehouseDto) {
+        Warehouse warehouse = new Warehouse(warehouseDto.getId(), warehouseDto.getName(), warehouseDto.getSortNumber(),
+                warehouseDto.getAddress(), warehouseDto.getCommentToAddress(), warehouseDto.getComment());
+        warehouseRepository.save(warehouse);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        warehouseRepository.deleteById(id);
     }
 }
