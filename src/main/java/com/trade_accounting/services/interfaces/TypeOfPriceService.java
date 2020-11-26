3a068b93a1ea561a5d264fd462edab779248f411
Denis Pavlov <pavler495@gmail.com>
2020-11-26 19:24:35 +0300

    Refactored imports

--- src/main/java/com/trade_accounting/services/interfaces/TypeOfPriceService.java
+++ src/main/java/com/trade_accounting/services/interfaces/TypeOfPriceService.java
@@ -1,7 +1,6 @@
 package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.dto.TypeOfPriceDto;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
