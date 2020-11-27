251a705dbb82aee8b61e6be4f70ec38e72f360aa
zsalamandra <zaur121079@yandex.ru>
2020-11-27 12:01:35 +0300

    add method

--- src/main/java/com/trade_accounting/repositories/BankAccountRepository.java
+++ src/main/java/com/trade_accounting/repositories/BankAccountRepository.java
@@ -1,9 +1,37 @@
 package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.BankAccount;
+import com.trade_accounting.models.dto.BankAccountDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+
+    @Query("select new com.trade_accounting.models.dto.BankAccountDto(" +
+            "ba.id, " +
+            "ba.rcbic, " +
+            "ba.bank, " +
+            "ba.address, " +
+            "ba.correspondentAccount, " +
+            "ba.account, " +
+            "ba.mainAccount, " +
+            "ba.sortNumber) from BankAccount ba")
+    List<BankAccountDto> getAll();
+
+    @Query("select new com.trade_accounting.models.dto.BankAccountDto(" +
+            "ba.id, " +
+            "ba.rcbic, " +
+            "ba.bank, " +
+            "ba.address, " +
+            "ba.correspondentAccount, " +
+            "ba.account, " +
+            "ba.mainAccount, " +
+            "ba.sortNumber) from BankAccount ba " +
+            "where ba.id = :id")
+    BankAccountDto getById(@Param("id") Long id);
 }
--- src/main/java/com/trade_accounting/services/interfaces/BankAccountService.java
+++ src/main/java/com/trade_accounting/services/interfaces/BankAccountService.java
@@ -1,4 +1,19 @@
 package com.trade_accounting.services.interfaces;
 
+import com.trade_accounting.models.dto.BankAccountDto;
+
+import java.util.List;
+
 public interface BankAccountService {
+
+    List<BankAccountDto> getAll();
+
+    BankAccountDto getById(Long id);
+
+    void create(BankAccountDto dto);
+
+    void update(BankAccountDto dto);
+
+    void deleteById(Long id);
 }
+
