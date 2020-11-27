1643e4ba9ab916ee420b5d005ae906b70a5be4b1
Denis Pavlov <pavler495@gmail.com>
2020-11-27 13:58:36 +0300

    fix init

--- src/main/java/com/trade_accounting/config/DataInitializer.java
+++ src/main/java/com/trade_accounting/config/DataInitializer.java
@@ -1,6 +1,7 @@
 package com.trade_accounting.config;
 
 import com.trade_accounting.models.TypeOfPrice;
+import com.trade_accounting.repositories.TypeOfPriceRepository;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,13 +9,15 @@ import javax.annotation.PostConstruct;
 @Component
 public class DataInitializer {
 
+    private TypeOfPriceRepository typeOfPriceRepository;
+
     @PostConstruct
     public void init() {
         initTypeOfPrices();
     }
 
     private void initTypeOfPrices() {
-        new TypeOfPrice("Оптовая цена","1");
-        new TypeOfPrice("Розничная цена", "2");
+        typeOfPriceRepository.save(new TypeOfPrice("Оптовая цена","1"));
+        typeOfPriceRepository.save(new TypeOfPrice("Розничная цена", "2"));
     }
 }
