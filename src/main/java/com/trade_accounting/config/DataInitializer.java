15083b8abefd1c55097b93cee2c73cd3a1e37fa4
Sidorenkomv <sir.sidorenkomv@yandex.ru>
2020-11-27 12:17:14 +0000

    Update src/main/java/com/trade_accounting/config/DataInitializer.java

--- src/main/java/com/trade_accounting/config/DataInitializer.java
+++ src/main/java/com/trade_accounting/config/DataInitializer.java
@@ -21,7 +21,7 @@ public class DataInitializer {
     }
 
     private void initTypeOfPrices() {
-        typeOfPriceService.create(new TypeOfPriceDto("Оптовая цена","1"));
+        typeOfPriceService.create(new TypeOfPriceDto("Оптовая цена", "1"));
         typeOfPriceService.create(new TypeOfPriceDto("Розничная цена", "2"));
     }
 }
