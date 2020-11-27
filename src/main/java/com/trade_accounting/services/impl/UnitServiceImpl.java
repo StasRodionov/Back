c13855fab84069f8fd58b56c4d52c3db9f70bd83
Maslak <andrey.maslak@softwareone.com>
2020-11-27 09:51:16 +0500

    add service Unit

--- src/main/java/com/trade_accounting/services/impl/UnitServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/UnitServiceImpl.java
@@ -1,9 +1,13 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.models.Unit;
+import com.trade_accounting.models.dto.UnitDto;
 import com.trade_accounting.repositories.UnitRepository;
 import com.trade_accounting.services.interfaces.UnitService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,5 +17,35 @@ public class UnitServiceImpl implements UnitService {
 
     public UnitServiceImpl(UnitRepository unitRepository) {
         this.unitRepository = unitRepository;
+    }
+
+    @Override
+    public List<UnitDto> getAll() {
+        return unitRepository.getAll();
+    }
+
+    @Override
+    public UnitDto getById(Long id) {
+        return unitRepository.getById(id);
+    }
+
+    @Override
+    public void create(UnitDto unitDto) {
+        Unit unit = new Unit(unitDto.getId(), unitDto.getShortName(),
+                unitDto.getFullName(), unitDto.getSortNumber());
+        unitRepository.save(unit);
+    }
+
+    @Override
+    public void update(UnitDto unitDto) {
+        Unit unit = new Unit(unitDto.getId(), unitDto.getShortName(),
+                unitDto.getFullName(), unitDto.getSortNumber());
+        unitRepository.save(unit);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        unitRepository.deleteById(id);
     }
 }
