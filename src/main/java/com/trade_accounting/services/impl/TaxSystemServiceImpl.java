3c5483eed2d145eecf49610f5f9172732db9b6ff
zsalamandra <zaur121079@yandex.ru>
2020-11-27 12:36:50 +0300

    add method to TaxSystemRepository

--- src/main/java/com/trade_accounting/services/impl/TaxSystemServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/TaxSystemServiceImpl.java
@@ -1,9 +1,13 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.models.TaxSystem;
+import com.trade_accounting.models.dto.TaxSystemDto;
 import com.trade_accounting.repositories.TaxSystemRepository;
 import com.trade_accounting.services.interfaces.TaxSystemService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -14,5 +18,40 @@ public class TaxSystemServiceImpl implements TaxSystemService {
 
     public TaxSystemServiceImpl(TaxSystemRepository taxSystemRepository) {
         this.taxSystemRepository = taxSystemRepository;
+    }
+
+    @Override
+    public List<TaxSystemDto> getAll() {
+        return taxSystemRepository.getAll();
+    }
+
+    @Override
+    public TaxSystemDto getById(Long id) {
+        return taxSystemRepository.getById(id);
+    }
+
+    @Override
+    public void create(TaxSystemDto dto) {
+        TaxSystem taxSystem = new TaxSystem(
+                null,
+                dto.getName(),
+                dto.getSortNumber()
+        );
+        taxSystemRepository.save(taxSystem);
+    }
+
+    @Override
+    public void update(TaxSystemDto dto) {
+        TaxSystem taxSystem = new TaxSystem(
+                dto.getId(),
+                dto.getName(),
+                dto.getSortNumber()
+        );
+        taxSystemRepository.save(taxSystem);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        taxSystemRepository.deleteById(id);
     }
 }
--- src/main/java/com/trade_accounting/services/interfaces/TaxSystemService.java
+++ src/main/java/com/trade_accounting/services/interfaces/TaxSystemService.java
@@ -1,4 +1,19 @@
 package com.trade_accounting.services.interfaces;
 
+import com.trade_accounting.models.dto.TaxSystemDto;
+
+import java.util.List;
+
 public interface TaxSystemService {
+
+    List<TaxSystemDto> getAll();
+
+    TaxSystemDto getById(Long id);
+
+    void create(TaxSystemDto dto);
+
+    void update(TaxSystemDto dto);
+
+    void deleteById(Long id);
+
 }
