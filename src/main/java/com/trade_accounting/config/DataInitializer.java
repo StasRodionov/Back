8bba788426ab6a1024a8439f80895c126f2ba6a1
Mrvelik <alihopoi@yandex.ru>
2020-11-27 19:01:14 +0500

    add_method_initWarehouses

--- src/main/java/com/trade_accounting/config/DataInitializer.java
+++ src/main/java/com/trade_accounting/config/DataInitializer.java
@@ -2,8 +2,10 @@ package com.trade_accounting.config;
 
 import com.trade_accounting.models.dto.RoleDto;
 import com.trade_accounting.models.dto.TypeOfPriceDto;
+import com.trade_accounting.models.dto.WarehouseDto;
 import com.trade_accounting.services.interfaces.RoleService;
 import com.trade_accounting.services.interfaces.TypeOfPriceService;
+import com.trade_accounting.services.interfaces.WarehouseService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,17 +15,21 @@ public class DataInitializer {
 
     private final TypeOfPriceService typeOfPriceService;
     private final RoleService roleService;
+    private final WarehouseService warehouseService;
 
     public DataInitializer(TypeOfPriceService typeOfPriceService,
-                           RoleService roleService) {
+                           RoleService roleService,
+                           WarehouseService warehouseService) {
         this.typeOfPriceService = typeOfPriceService;
         this.roleService = roleService;
+        this.warehouseService = warehouseService;
     }
 
     @PostConstruct
     public void init() {
         initTypeOfPrices();
         initRoles();
+        initWarehouses();
     }
 
     private void initTypeOfPrices() {
@@ -34,5 +40,9 @@ public class DataInitializer {
     private void initRoles() {
         roleService.create(new RoleDto("admin", "1"));
         roleService.create(new RoleDto("user", "2"));
+    }
+
+    private void initWarehouses(){
+        warehouseService.create(new WarehouseDto("Основной склад", "1"));
     }
 }
