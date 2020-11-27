a5e2cffb9a40a601add9312ef91183866964ee0a
DaniilNov <xa86@mail.ru>
2020-11-27 19:00:32 +0300

    Создание методов в TypeOfContractorService

--- src/main/java/com/trade_accounting/services/impl/TypeOfContractorServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/TypeOfContractorServiceImpl.java
@@ -1,9 +1,13 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.models.TypeOfContractor;
+import com.trade_accounting.models.dto.TypeOfContractorDto;
 import com.trade_accounting.repositories.TypeOfContractorRepository;
 import com.trade_accounting.services.interfaces.TypeOfContractorService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -13,5 +17,33 @@ public class TypeOfContractorServiceImpl implements TypeOfContractorService {
 
     public TypeOfContractorServiceImpl(TypeOfContractorRepository typeOfContractorRepository) {
         this.typeOfContractorRepository = typeOfContractorRepository;
+    }
+
+    public List<TypeOfContractorDto> getAll() {
+        return typeOfContractorRepository.getAll();
+    }
+
+    public TypeOfContractorDto getById(Long id) {
+        return typeOfContractorRepository.getById(id);
+    }
+
+    public void create(TypeOfContractorDto typeOfContractorDto) {
+
+        typeOfContractorRepository.save(
+                new TypeOfContractor(
+                        typeOfContractorDto.getName(),
+                        typeOfContractorDto.getSortNumber()));
+    }
+
+    public void update(TypeOfContractorDto typeOfContractorDto) {
+        typeOfContractorRepository.save(
+                new TypeOfContractor(
+                        typeOfContractorDto.getId(),
+                        typeOfContractorDto.getName(),
+                        typeOfContractorDto.getSortNumber()));
+    }
+
+    public void deleteById(Long id) {
+        typeOfContractorRepository.deleteById(id);
     }
 }
