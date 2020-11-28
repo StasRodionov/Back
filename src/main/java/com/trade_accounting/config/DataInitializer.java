f7e10b31c348f1d0b5bc971cf63d5185b6479ebc
zsalamandra <zaur121079@yandex.ru>
2020-11-28 18:15:12 +0300

    add units

--- src/main/java/com/trade_accounting/config/DataInitializer.java
+++ src/main/java/com/trade_accounting/config/DataInitializer.java
@@ -2,8 +2,10 @@ package com.trade_accounting.config;
 
 import com.trade_accounting.models.dto.RoleDto;
 import com.trade_accounting.models.dto.TypeOfPriceDto;
+import com.trade_accounting.models.dto.UnitDto;
 import com.trade_accounting.services.interfaces.RoleService;
 import com.trade_accounting.services.interfaces.TypeOfPriceService;
+import com.trade_accounting.services.interfaces.UnitService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -13,17 +15,20 @@ public class DataInitializer {
 
     private final TypeOfPriceService typeOfPriceService;
     private final RoleService roleService;
+    private final UnitService unitService;
 
     public DataInitializer(TypeOfPriceService typeOfPriceService,
-                           RoleService roleService) {
+                           RoleService roleService, UnitService unitService) {
         this.typeOfPriceService = typeOfPriceService;
         this.roleService = roleService;
+        this.unitService = unitService;
     }
 
     @PostConstruct
     public void init() {
         initTypeOfPrices();
         initRoles();
+        initUnits();
     }
 
     private void initTypeOfPrices() {
@@ -35,4 +40,67 @@ public class DataInitializer {
         roleService.create(new RoleDto("admin", "1"));
         roleService.create(new RoleDto("user", "2"));
     }
+
+    private void initUnits(){
+        unitService.create(new UnitDto("мм", "Миллиметр", "1"));
+        unitService.create(new UnitDto("см", "Сантиметр", "2"));
+        unitService.create(new UnitDto("дм", "Дециметр", "3"));
+        unitService.create(new UnitDto("м", "Метр", "4"));
+        unitService.create(new UnitDto("км; 10^3 м", "Километр; тысяча метров", "5"));
+        unitService.create(new UnitDto("Мм; 10^6 м", "Мегаметр; миллион метров", "6"));
+        unitService.create(new UnitDto("пог. м", "Погонный метр", "7"));
+        unitService.create(new UnitDto("дюйм", "Дюйм (25,4 мм)", "8"));
+        unitService.create(new UnitDto("фут", "Фут (0,3048 м)", "9"));
+        unitService.create(new UnitDto("ярд", "Ярд (0,9144 м)", "10"));
+        unitService.create(new UnitDto("миля", "Морская миля (1852 м)", "11"));
+        unitService.create(new UnitDto("мм2", "Квадратный миллиметр", "12"));
+        unitService.create(new UnitDto("см2", "Квадратный сантиметр", "13"));
+        unitService.create(new UnitDto("дм2", "Квадратный дециметр", "14"));
+        unitService.create(new UnitDto("м2", "Квадратный метр", "15"));
+        unitService.create(new UnitDto("10^3 м^2", "Тысяча квадратных метров", "16"));
+        unitService.create(new UnitDto("га", "Гектар", "17"));
+        unitService.create(new UnitDto("км2", "Квадратный километр", "18"));
+        unitService.create(new UnitDto("дюйм2", "Квадратный дюйм (645,16 мм2)", "19"));
+        unitService.create(new UnitDto("фут2", "Квадратный фут (0,092903 м2)", "20"));
+        unitService.create(new UnitDto("ярд2", "Квадратный ярд (0,8361274 м2)", "21"));
+        unitService.create(new UnitDto("а", "Ар (100 м2)", "22"));
+        unitService.create(new UnitDto("мм3", "Кубический миллиметр", "23"));
+        unitService.create(new UnitDto("см3; мл", "Кубический сантиметр; миллилитр", "24"));
+        unitService.create(new UnitDto("л; дм3", "Литр; кубический дециметр", "25"));
+        unitService.create(new UnitDto("м3", "Кубический метр", "26"));
+        unitService.create(new UnitDto("дл", "Децилитр", "27"));
+        unitService.create(new UnitDto("гл", "Гектолитр", "28"));
+        unitService.create(new UnitDto("Мл", "Мегалитр", "29"));
+        unitService.create(new UnitDto("дюйм3", "Кубический дюйм (16387,1 мм3)", "30"));
+        unitService.create(new UnitDto("фут3", "Кубический фут (0,02831685 м3)", "31"));
+        unitService.create(new UnitDto("ярд3", "Кубический ярд (0,764555 м3)", "32"));
+        unitService.create(new UnitDto("10^6 м3", "Миллион кубических метров", "33"));
+        unitService.create(new UnitDto("гг", "Гектограмм", "34"));
+        unitService.create(new UnitDto("мг", "Миллиграмм", "35"));
+        unitService.create(new UnitDto("кар", "Метрический карат", "36"));
+        unitService.create(new UnitDto("г", "Грамм", "37"));
+        unitService.create(new UnitDto("кг", "Килограмм", "38"));
+        unitService.create(new UnitDto("т", "Тонна; метрическая тонна (1000 кг)", "39"));
+        unitService.create(new UnitDto("10^3 т", "Килотонна", "40"));
+        unitService.create(new UnitDto("сг", "Сантиграмм", "41"));
+        unitService.create(new UnitDto("БРТ", "Брутто-регистровая тонна (2,8316 м3)", "42"));
+        unitService.create(new UnitDto("т грп", "Грузоподъемность в метрических тоннах", "43"));
+        unitService.create(new UnitDto("ц", "Центнер (метрический) (100 кг); гектокилограмм; квинтал1 (метрический); децитонна", "44"));
+        unitService.create(new UnitDto("с", "Секунда", "45"));
+        unitService.create(new UnitDto("мин", "Минута", "46"));
+        unitService.create(new UnitDto("ч", "Час", "47"));
+        unitService.create(new UnitDto("сут; дн", "Сутки", "48"));
+        unitService.create(new UnitDto("нед", "Неделя", "49"));
+        unitService.create(new UnitDto("дек", "Декада", "50"));
+        unitService.create(new UnitDto("мес", "Месяц", "51"));
+        unitService.create(new UnitDto("кварт", "Квартал", "52"));
+        unitService.create(new UnitDto("полгода", "Полугодие", "53"));
+        unitService.create(new UnitDto("г; лет", "Год", "54"));
+        unitService.create(new UnitDto("деслет", "Десятилетие", "55"));
+        unitService.create(new UnitDto("рулон", "Рулон", "56"));
+        unitService.create(new UnitDto("шт", "Штука", "57"));
+        unitService.create(new UnitDto("ящ", "Ящик", "58"));
+        unitService.create(new UnitDto("блок", "Блок сигарет", "59"));
+    }
+
 }
