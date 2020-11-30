1714f0ee5dad817c8c0ec21cff70133bc8148d45
zsalamandra <zaur121079@yandex.ru>
2020-11-30 22:14:11 +0300

    русский лог!

--- src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
@@ -29,34 +29,36 @@ public class WarehouseRestController {
 
     @GetMapping
     public ResponseEntity<List<WarehouseDto>> getAll() {
-        log.info("returned all objects");
-        return ResponseEntity.ok(warehouseService.getAll());
+        List<WarehouseDto> warehouseDtos = warehouseService.getAll();
+        log.info("Запрошен список WarehouseDto");
+        return ResponseEntity.ok(warehouseDtos);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<WarehouseDto> getById(@PathVariable("id") Long id) {
-        log.info("returned object by id - {}", id);
-        return ResponseEntity.ok(warehouseService.getById(id));
+        WarehouseDto warehouseDto = warehouseService.getById(id);
+        log.info("Запрошен экземпляр WarehouseDto с id = {}", id);
+        return ResponseEntity.ok(warehouseDto);
     }
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody WarehouseDto warehouseDto) {
         warehouseService.create(warehouseDto);
-        log.info("created object: {}", warehouseDto.toString());
+        log.info("Записан новый экземпляр WarehouseDto");
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody WarehouseDto warehouseDto) {
         warehouseService.update(warehouseDto);
-        log.info("updated object: {}", warehouseDto.toString());
+        log.info("Обновлен экземпляр WarehouseDto");
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
         warehouseService.deleteById(id);
-        log.info("deleted object by id - {}", id);
+        log.info("Удален экземпляр с id - {}", id);
         return ResponseEntity.ok().build();
     }
 }
