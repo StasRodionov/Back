77a317b4b30dceaa437ca8ee3b1d53cb71f7974b
zsalamandra <zaur121079@yandex.ru>
2020-11-30 15:40:05 +0300

    add log messages

--- src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/WarehouseRestController.java
@@ -29,29 +29,34 @@ public class WarehouseRestController {
 
     @GetMapping
     public ResponseEntity<List<WarehouseDto>> getAll() {
+        log.info("returned all objects");
         return ResponseEntity.ok(warehouseService.getAll());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<WarehouseDto> getById(@PathVariable("id") Long id) {
+        log.info("returned object byId - %d", id);
         return ResponseEntity.ok(warehouseService.getById(id));
     }
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody WarehouseDto warehouseDto) {
         warehouseService.create(warehouseDto);
+        log.info("created object: " + warehouseDto.toString());
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody WarehouseDto warehouseDto) {
         warehouseService.update(warehouseDto);
+        log.info("updated object: " + warehouseDto.toString());
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
         warehouseService.deleteById(id);
+        log.info("deleted object by id - ", id);
         return ResponseEntity.ok().build();
     }
 }
