67d7360d46700685b203e47db9c86c83adc67321
DaniilNov <xa86@mail.ru>
2020-11-30 17:08:04 +0300

    fix

--- src/main/java/com/trade_accounting/repositories/TypeOfPriceRepository.java
+++ src/main/java/com/trade_accounting/repositories/TypeOfPriceRepository.java
@@ -12,10 +12,21 @@ import java.util.List;
 @Repository
 public interface TypeOfPriceRepository extends JpaRepository<TypeOfPrice, Long> {
 
-    @Query("select new com.trade_accounting.models.dto.TypeOfPriceDto(t.id, t.name, t.sortNumber) from TypeOfPrice t")
+    @Query("select new com.trade_accounting.models.dto.TypeOfPriceDto(" +
+            "t.id, " +
+            "t.name, " +
+            "t.sortNumber" +
+            ") " +
+            "from TypeOfPrice t")
     List<TypeOfPriceDto> getAll();
 
-    @Query("select new com.trade_accounting.models.dto.TypeOfPriceDto(t.id, t.name, t.sortNumber) from TypeOfPrice t where t.id = :id")
+    @Query("select new com.trade_accounting.models.dto.TypeOfPriceDto(" +
+            "t.id, " +
+            "t.name, " +
+            "t.sortNumber" +
+            ") " +
+            "from TypeOfPrice t " +
+            "where t.id = :id")
     TypeOfPriceDto getById(@Param("id") Long id);
 
 }
