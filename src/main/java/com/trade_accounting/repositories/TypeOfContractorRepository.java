0ed7f116c698014bec1d2268daf68d5cfecb468e
Maslak <andrey.maslak@softwareone.com>
2020-11-30 15:39:59 +0500

    fix create in LegalDetailService

--- src/main/java/com/trade_accounting/repositories/TypeOfContractorRepository.java
+++ src/main/java/com/trade_accounting/repositories/TypeOfContractorRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TypeOfContractorRepository extends JpaRepository<TypeOfContractor, Long> {
+
+    TypeOfContractor findByName(String name);
 }
