291e947b2b1185fd969eab6b2ee9fe4744e131d3
zsalamandra <zaur121079@yandex.ru>
2020-11-30 11:35:11 +0300

    added_methods_to_TypeOfRestController

--- src/main/java/com/trade_accounting/controllers/rest/TypeOfPriceRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/TypeOfPriceRestController.java
@@ -1,9 +1,54 @@
 package com.trade_accounting.controllers.rest;
 
+import com.trade_accounting.models.dto.TypeOfPriceDto;
+import com.trade_accounting.services.interfaces.TypeOfPriceService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/typeofprice")
 public class TypeOfPriceRestController {
+
+    private final TypeOfPriceService typeOfPriceService;
+
+    public TypeOfPriceRestController(TypeOfPriceService service) {
+        this.typeOfPriceService = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TypeOfPriceDto>> getAll() {
+        return ResponseEntity.ok(typeOfPriceService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TypeOfPriceDto> getById(@PathVariable(name = "id") Long id) {
+        return ResponseEntity.ok(typeOfPriceService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody TypeOfPriceDto typeOfPriceDto) {
+        typeOfPriceService.create(typeOfPriceDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody TypeOfPriceDto typeOfPriceDto) {
+        typeOfPriceService.update(typeOfPriceDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable(name = "id") Long id) {
+        typeOfPriceService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
