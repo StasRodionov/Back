81ce6b040ad53bed7d6177be6a34d105c2d2fbcc
Vasilich <126as500@gmail.com>
2020-12-01 12:13:23 +0300

    add RoleRestController

--- src/main/java/com/trade_accounting/controllers/rest/RoleRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/RoleRestController.java
@@ -1,11 +1,55 @@
 package com.trade_accounting.controllers.rest;
 
+import com.trade_accounting.models.dto.RoleDto;
+import com.trade_accounting.services.interfaces.RoleService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/role")
 public class RoleRestController {
+
+    private final RoleService roleService;
+
+    public RoleRestController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<RoleDto>> getAll() {
+        return ResponseEntity.ok(roleService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RoleDto> getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(roleService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody RoleDto roleDto) {
+        roleService.create(roleDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping()
+    public ResponseEntity<?> update(@RequestBody RoleDto roleDto) {
+        roleService.update(roleDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        roleService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
