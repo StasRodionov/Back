fbfca1aa4cbb445eaf1693082ea9e6b2c9bdf8a8
zsalamandra <zaur121079@yandex.ru>
2020-12-01 12:11:00 +0300

    add methods to ContractorGroupService

--- src/main/java/com/trade_accounting/repositories/ContractorGroupRepository.java
+++ src/main/java/com/trade_accounting/repositories/ContractorGroupRepository.java
@@ -1,9 +1,27 @@
 package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.ContractorGroup;
+import com.trade_accounting.models.dto.ContractorGroupDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ContractorGroupRepository extends JpaRepository<ContractorGroup, Long> {
+
+    @Query("select new com.trade_accounting.models.dto.ContractorGroupDto(" +
+            "cg.id, " +
+            "cg.name, " +
+            "cg.sortNumber" +
+            ") from ContractorGroup cg")
+    List<ContractorGroupDto> getAll();
+
+    @Query("select new com.trade_accounting.models.dto.ContractorGroupDto(" +
+            "cg.id, " +
+            "cg.name, " +
+            "cg.sortNumber) from ContractorGroup cg where cg.id = :id")
+    ContractorGroupDto getById(@Param("id") Long id);
 }
