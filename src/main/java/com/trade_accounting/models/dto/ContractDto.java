126641a16ef901b73891c159736cfa103ea45aa6
AnnaLeo97 <30714027+AnnaLeo97@users.noreply.github.com>
2020-12-02 11:48:43 +0500

    Исправила ContractDto

---
+++ src/main/java/com/trade_accounting/models/dto/ContractDto.java
@@ -0,0 +1,38 @@
+package com.trade_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContractDto {
+
+    private Long id;
+
+    private String number;
+
+    private LocalDate contractDate;
+
+    @NotNull
+    private CompanyDto companyDto;
+
+    private BankAccountDto bankAccountDto;
+
+    @NotNull
+    private ContractorDto contractorDto;
+
+    private BigDecimal amount;
+
+    private Boolean archive;
+
+    private String comment;
+
+    private LegalDetailDto legalDetailDto;
+}
