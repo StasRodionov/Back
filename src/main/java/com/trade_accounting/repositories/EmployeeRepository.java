40c0bc6fec2ead5a00d6ed4900df96c2ab042ad3
Juliet <juliet_v17@mail.ru>
2020-12-04 23:57:50 +0300

    Added methods in EmployeeService, EmployeeRepository & EmployeeServiceImpl

--- src/main/java/com/trade_accounting/repositories/EmployeeRepository.java
+++ src/main/java/com/trade_accounting/repositories/EmployeeRepository.java
@@ -1,9 +1,49 @@
 package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.Employee;
+import com.trade_accounting.models.dto.EmployeeDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @Query("select new com.trade_accounting.models.dto.EmployeeDto(" +
+            "em.id, " +
+            "em.lastName, " +
+            "em.firstName, " +
+            "em.middleName, " +
+            "em.sortNumber, " +
+            "em.phone, " +
+            "em.inn, " +
+            "em.description, " +
+            "em.email," +
+            "em.password," +
+            "em.department.id," +
+            "em.position.id," +
+            "em.roles.id," +
+            "em.image.id) from Employee em")
+    List<EmployeeDto> getAll();
+
+    @Query("select new com.trade_accounting.models.dto.EmployeeDto(" +
+            "em.id, " +
+            "em.lastName, " +
+            "em.firstName, " +
+            "em.middleName, " +
+            "em.sortNumber, " +
+            "em.phone, " +
+            "em.inn, " +
+            "em.description, " +
+            "em.email," +
+            "em.password," +
+            "em.department.id," +
+            "em.position.id," +
+            "em.roles.id," +
+            "em.image.id) from Employee em " +
+            "where em.id = :id")
+    EmployeeDto getById(@Param("id") Long id);
 }
