d8a061add6492f835cf73dc19ae3b375267b67f5
Anna <maksimova2800@gmail.com>
2020-12-15 13:33:34 +0300

    bug fix initCompanies()

--- src/main/java/com/trade_accounting/services/impl/ContractServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/ContractServiceImpl.java
@@ -45,9 +45,9 @@ public class ContractServiceImpl implements ContractService {
             contractDto.setBankAccountDto(
                     bankAccountRepository.getById(contractDto.getBankAccountDto().getId())
             );
-//            contractDto.setContractorDto(
-//                    contractorRepository.getById(contractDto.getContractorDto().getId())
-//            );
+            contractDto.setContractorDto(
+                    contractorRepository.getById(contractDto.getContractorDto().getId())
+            );
             contractDto.setLegalDetailDto(
                     legalDetailRepository.getById(contractDto.getLegalDetailDto().getId())
             );
