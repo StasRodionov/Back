cda2b032c4b2683fe1865cf6968c042c688c1f56
Nikolay Nikolaev <kumich72@gmail.com>
2021-01-12 22:04:38 +0500

    add swagger info for ImageRestController

--- src/main/java/com/trade_accounting/controllers/rest/ImageRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/ImageRestController.java
@@ -2,6 +2,11 @@ package com.trade_accounting.controllers.rest;
 
 import com.trade_accounting.models.dto.ImageDto;
 import com.trade_accounting.services.interfaces.ImageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +22,8 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@Tag(name = "Image Rest Controller", description = "CRUD операции с фото")
+@Api(tags = "Image Rest Controller")
 @RequestMapping("/api/image")
 public class ImageRestController {
 
@@ -26,35 +33,73 @@ public class ImageRestController {
         this.imageService = imageService;
     }
 
+    @ApiOperation(value = "getAll", notes = "Возвращает список всех фото")
     @GetMapping
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешное получение списка всех фото"),
+            @ApiResponse(code = 404, message = "Данный контролер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<List<ImageDto>> getAll() {
         List<ImageDto> images = imageService.getAll();
         log.info("Запрошен список Image");
         return ResponseEntity.ok(images);
     }
 
+    @ApiOperation(value = "getById", notes = "Возвращает определенное фото по Id")
     @GetMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Фото найдено"),
+            @ApiResponse(code = 404, message = "Данный контролер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<ImageDto> getById(@PathVariable(name = "id") Long id) {
         ImageDto image = imageService.getById(id);
         log.info("Запрошен экземпляр Image с id= {}", id);
         return ResponseEntity.ok(image);
     }
 
+    @ApiOperation(value = "create", notes = "Добавляет фото на основе переданных данных")
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Фото успешно добавлено"),
+            @ApiResponse(code = 201, message = "Запрос принят и данные созданы"),
+            @ApiResponse(code = 404, message = "Данный контролер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<?> create(@RequestBody ImageDto imageDto) {
         imageService.create(imageDto);
         log.info("Записан новый экземпляр Image c id= {}", imageDto.getId());
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "update", notes = "Обновляет фото на основе переданных данных")
     @PutMapping
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Фото успешно обновлено"),
+            @ApiResponse(code = 201, message = "Запрос принят и данные обновлены"),
+            @ApiResponse(code = 404, message = "Данный контролер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<?> update(@RequestBody ImageDto imageDto) {
         imageService.update(imageDto);
         log.info("Обновлен экземпляр Image с id= {}", imageDto.getId());
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "deleteById", notes = "Удаляет фото на основе переданного ID")
     @DeleteMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Фото успешно удалено"),
+            @ApiResponse(code = 204, message = "Запрос получен и обработан, данных для возврата нет"),
+            @ApiResponse(code = 404, message = "Данный контролер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<?> deleteById(@PathVariable(name = "id") Long id) {
         imageService.deleteById(id);
         log.info("Удален экземпляр Image с id= {}", id);
