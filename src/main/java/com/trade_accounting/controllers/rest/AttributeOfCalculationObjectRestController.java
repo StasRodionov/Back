7d5cde8f8639d64b3cbfa4822ab7ecbeb853900c
Leonid Volobuev <14leonidas88@gmail.com>
2021-01-16 16:28:51 +0500

    Adding swagger AttributeOfCalculationObjectRestController

--- src/main/java/com/trade_accounting/controllers/rest/AttributeOfCalculationObjectRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/AttributeOfCalculationObjectRestController.java
@@ -2,6 +2,14 @@ package com.trade_accounting.controllers.rest;
 
 import com.trade_accounting.models.dto.AttributeOfCalculationObjectDto;
 import com.trade_accounting.services.interfaces.AttributeOfCalculationObjectService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +25,8 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@Tag(name = "Attribute Of Calculation Object Rest Controller", description = "CRUD операции с объектами")
+@Api(tags = "Attribute Of Calculation Object Rest Controller")
 @RequestMapping("/api/attribute/calculation/object")
 public class AttributeOfCalculationObjectRestController {
 
@@ -28,6 +38,13 @@ public class AttributeOfCalculationObjectRestController {
     }
 
     @GetMapping
+    @ApiOperation(value = "getAll", notes = "Получение списка всех объектов")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешное получение списка объектов"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<List<AttributeOfCalculationObjectDto>> getAll(){
         List<AttributeOfCalculationObjectDto> attributeOfCalculationObjectDtos = attributeOfCalculationObjectService.getAll();
         log.info("Запрошен список AttributeOfCalculationObjectDto");
@@ -35,6 +52,13 @@ public class AttributeOfCalculationObjectRestController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "getById", notes = "Получение объекта по id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Объект найдена"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<AttributeOfCalculationObjectDto> getById(@PathVariable("id") Long id) {
         AttributeOfCalculationObjectDto attributeOfCalculationObjectDto = attributeOfCalculationObjectService.getById(id);
         log.info("Запрошен экземпляр AttributeOfCalculationObjectDto с id = {}", id);
@@ -42,6 +66,14 @@ public class AttributeOfCalculationObjectRestController {
     }
 
     @PostMapping
+    @ApiOperation(value = "create", notes = "Внесение нового объекта")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Объект создан"),
+            @ApiResponse(code = 201, message = "Запрос принят и объект добавлен"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<?> create(@RequestBody AttributeOfCalculationObjectDto attributeOfCalculationObjectDto){
         attributeOfCalculationObjectService.create(attributeOfCalculationObjectDto);
         log.info("Записан новый экземпляр AttributeOfCalculationObjectDto");
@@ -49,6 +81,14 @@ public class AttributeOfCalculationObjectRestController {
     }
 
     @PutMapping
+    @ApiOperation(value = "update", notes = "Изменение информации об объекте")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Информация о объекте обновлена"),
+            @ApiResponse(code = 201, message = "Запрос принят и данные об объекте обновлены"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<?> update(@RequestBody AttributeOfCalculationObjectDto attributeOfCalculationObjectDto){
         attributeOfCalculationObjectService.update(attributeOfCalculationObjectDto);
         log.info("Обновлен экземплярAttributeOfCalculationObjectDto");
@@ -56,6 +96,14 @@ public class AttributeOfCalculationObjectRestController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "deleteById", notes = "Удаление объекта по id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Объект удален"),
+            @ApiResponse(code = 204, message = "Запрос получен и обработан, данных для возврата нет"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<?> deleteById(@PathVariable("id") Long id){
         attributeOfCalculationObjectService.deleteById(id);
         log.info("Удален экземпляр с id = {}", id);
