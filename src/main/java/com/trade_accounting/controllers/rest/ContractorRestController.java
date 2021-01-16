a2810b707b0586d35ddb26596359eca7671a0660
Leonid Volobuev <14leonidas88@gmail.com>
2021-01-16 16:52:43 +0500

    adding swagger to contractor rest controller

--- src/main/java/com/trade_accounting/controllers/rest/ContractorRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/ContractorRestController.java
@@ -4,6 +4,14 @@ import com.trade_accounting.models.dto.ContractorDto;
 import com.trade_accounting.services.interfaces.ContractorService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +25,8 @@ import java.util.List;
 
 @Slf4j
 @RestController
+@Tag(name = "Contractor Rest Controller", description = "CRUD  операции с контрагентами")
+@Api(tags = "Contractor Rest Controller")
 @RequestMapping("/api/contractor")
 public class ContractorRestController {
 
@@ -27,6 +37,13 @@ public class ContractorRestController {
     }
 
     @GetMapping
+    @ApiOperation(value = "getAll", notes = "Получение списка всех контрагентов")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешное получение списка контрагентов"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<List<ContractorDto>> getAll() {
         List<ContractorDto> contractorDtoList = contractorService.getAll();
         log.info("Запрошен список ContractorDto");
@@ -34,6 +51,13 @@ public class ContractorRestController {
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "getById", notes = "Получение контрагента по id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Контрагент найден"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<ContractorDto> getById(@PathVariable("id") Long id) {
         ContractorDto contractorDto = contractorService.getById(id);
         log.info("Запрошен экземпляр ContractorDto с id= {}", id);
@@ -41,6 +65,14 @@ public class ContractorRestController {
     }
 
     @PostMapping
+    @ApiOperation(value = "create", notes = "Внесение нового контрагента")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Контрагент создан"),
+            @ApiResponse(code = 201, message = "Запрос принят и контрагент добавлен"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<ContractorDto> create(@RequestBody ContractorDto contractorDto) {
         contractorService.create(contractorDto);
         log.info("Записан новый экземпляр {}", contractorDto.toString());
@@ -48,6 +80,14 @@ public class ContractorRestController {
     }
 
     @PutMapping
+    @ApiOperation(value = "update", notes = "Изменение информации о контрагенте")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Информация о контрагенте обновлена"),
+            @ApiResponse(code = 201, message = "Запрос принят и данные о контрагенте обновлены"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<ContractorDto> update(@RequestBody ContractorDto contractorDto) {
         contractorService.update(contractorDto);
         log.info("Обновлен экземпляр ContractorDto с id= {}", contractorDto.getId());
@@ -55,6 +95,14 @@ public class ContractorRestController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation(value = "deleteById", notes = "Удаление компании по ее id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Аккаунт компании удален"),
+            @ApiResponse(code = 204, message = "Запрос получен и обработан, данных для возврата нет"),
+            @ApiResponse(code = 404, message = "Данный контроллер не найден"),
+            @ApiResponse(code = 403, message = "Операция запрещена"),
+            @ApiResponse(code = 401, message = "Нет доступа к данной операции")}
+    )
     public ResponseEntity<ContractorDto> deleteById(@PathVariable("id") Long id) {
         contractorService.deleteById(id);
         log.info("Удален экземпляр ContractorDto с id= {}", id);
