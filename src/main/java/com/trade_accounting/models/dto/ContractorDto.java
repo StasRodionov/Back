1313799eb917d9d8faa34eacc501aafa8e5b711b
Sergei <prussaq@gmail.com>
2021-01-25 16:56:01 +0200

    DataInitializer initContractors added

--- src/main/java/com/trade_accounting/models/dto/ContractorDto.java
+++ src/main/java/com/trade_accounting/models/dto/ContractorDto.java
@@ -3,6 +3,7 @@ package com.trade_accounting.models.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
@@ -42,26 +43,6 @@ public class ContractorDto {
 
     private LegalDetailDto legalDetailDto;
 
-    public ContractorDto(String name,
-                         @Pattern(regexp = "^([0-9]{10}|[0-9]{12})$") String inn,
-                         String sortNumber,
-                         String phone,
-                         String fax,
-                         String email,
-                         String address,
-                         String commentToAddress,
-                         String comment) {
-        this.name = name;
-        this.inn = inn;
-        this.sortNumber = sortNumber;
-        this.phone = phone;
-        this.fax = fax;
-        this.email = email;
-        this.address = address;
-        this.commentToAddress = commentToAddress;
-        this.comment = comment;
-    }
-
     public ContractorDto(Long id,
                          String name,
                          @Pattern(regexp = "^([0-9]{10}|[0-9]{12})$") String inn,
@@ -72,7 +53,15 @@ public class ContractorDto {
                          String address,
                          String commentToAddress,
                          String comment) {
-        this(name, inn, sortNumber, phone, fax, email, address, commentToAddress, comment);
         this.id = id;
+        this.name = name;
+        this.inn = inn;
+        this.sortNumber = sortNumber;
+        this.phone = phone;
+        this.fax = fax;
+        this.email = email;
+        this.address = address;
+        this.commentToAddress = commentToAddress;
+        this.comment = comment;
     }
 }
