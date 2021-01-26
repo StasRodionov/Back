65a239c523941a4d6e7be8724d7d26edcc187646
Leonid Volobuev <14leonidas88@gmail.com>
2021-01-26 20:19:49 +0500

    InvoiceProductService and InvoiceProductServiceImpl

---
+++ src/main/java/com/trade_accounting/services/impl/InvoiceProductServiceImpl.java
@@ -0,0 +1,71 @@
+package com.trade_accounting.services.impl;
+
+import com.trade_accounting.models.InvoiceProduct;
+import com.trade_accounting.models.dto.InvoiceProductDto;
+import com.trade_accounting.repositories.InvoiceProductRepository;
+import com.trade_accounting.repositories.InvoiceRepository;
+import com.trade_accounting.repositories.ProductRepository;
+import com.trade_accounting.services.interfaces.InvoiceProductService;
+
+import java.util.List;
+
+public class InvoiceProductServiceImpl implements InvoiceProductService {
+
+    private final InvoiceRepository invoiceRepository;
+    private final ProductRepository productRepository;
+    private final InvoiceProductRepository invoiceProductRepository;
+
+    public InvoiceProductServiceImpl(InvoiceRepository invoiceRepository,
+                                     ProductRepository productRepository,
+                                     InvoiceProductRepository invoiceProductRepository) {
+        this.invoiceRepository = invoiceRepository;
+        this.productRepository = productRepository;
+        this.invoiceProductRepository = invoiceProductRepository;
+    }
+
+    @Override
+    public List<InvoiceProductDto> getAll() {
+
+        List<InvoiceProductDto> listInvoiceProductDto = invoiceProductRepository.getAll();
+        for (InvoiceProductDto invoiceProductDto: listInvoiceProductDto){
+            invoiceProductDto.setInvoiceDto(invoiceRepository.getById(invoiceProductDto.getInvoiceDto().getId()));
+            invoiceProductDto.setProductDto(productRepository.getById(invoiceProductDto.getProductDto().getId()));
+        }
+        return listInvoiceProductDto;
+    }
+
+    @Override
+    public InvoiceProductDto getById(Long id) {
+        InvoiceProductDto invoiceProductDto = invoiceProductRepository.getById(id);
+        invoiceProductDto.setInvoiceDto(invoiceRepository.getById(invoiceProductDto.getInvoiceDto().getId()));
+        invoiceProductDto.setProductDto(productRepository.getById(invoiceProductDto.getProductDto().getId()));
+        return invoiceProductDto;
+    }
+
+    @Override
+    public void create(InvoiceProductDto invoiceProductDto) {
+        invoiceProductRepository.save(new InvoiceProduct(
+                invoiceRepository.getOne(invoiceProductDto.getInvoiceDto().getId()),
+                productRepository.getOne(invoiceProductDto.getProductDto().getId()),
+                invoiceProductDto.getAmount(),
+                invoiceProductDto.getPrice()
+        ));
+    }
+
+    @Override
+    public void update(InvoiceProductDto invoiceProductDto) {
+        invoiceProductRepository.save(new InvoiceProduct(
+                invoiceProductDto.getId(),
+                invoiceRepository.getOne(invoiceProductDto.getInvoiceDto().getId()),
+                productRepository.getOne(invoiceProductDto.getProductDto().getId()),
+                invoiceProductDto.getAmount(),
+                invoiceProductDto.getPrice()
+        ));
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        invoiceProductRepository.deleteById(id);
+    }
+}
