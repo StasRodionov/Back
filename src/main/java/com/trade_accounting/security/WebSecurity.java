e529ab7744243bc7dff91e592ae519d744164e1b
it-mikhail <mikhail.tupitsyn@gmail.com>
2021-02-05 15:49:03 +0300

    added authentification (added spring security classes)

---
+++ src/main/java/com/trade_accounting/security/WebSecurity.java
@@ -0,0 +1,72 @@
+package com.trade_accounting.security;
+
+import com.trade_accounting.services.impl.EmployeeDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static com.trade_accounting.config.SecurityConstants.SIGN_UP_URL;
+
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    private EmployeeDetailsServiceImpl userDetailsService;
+    // private BCryptPasswordEncoder bCryptPasswordEncoder; // #bookmark #encrypt.password
+
+    // public WebSecurity(UserDetailsServiceImpl userService, BCryptPasswordEncoder bCryptPasswordEncoder) { // #bookmark #encrypt.password
+    public WebSecurity(EmployeeDetailsServiceImpl userService) {
+        this.userDetailsService = userService;
+        // this.bCryptPasswordEncoder = bCryptPasswordEncoder; // #bookmark #encrypt.password
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .cors()
+                .and()
+                    .csrf().disable()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                    .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+}
