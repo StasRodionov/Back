d9045811d5f4a091b1bf0eebc1b54d825f2a7283
Nikolay Nikolaev <kumich72@gmail.com>
2021-02-12 16:14:38 +0500

    add filter and search for Currency

--- src/main/java/com/trade_accounting/services/impl/CurrencyServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/CurrencyServiceImpl.java
@@ -4,16 +4,20 @@ import com.trade_accounting.models.Currency;
 import com.trade_accounting.models.dto.CurrencyDto;
 import com.trade_accounting.repositories.CurrencyRepository;
 import com.trade_accounting.services.interfaces.CurrencyService;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class CurrencyServiceImpl implements CurrencyService {
 
     private final CurrencyRepository currencyRepository;
+    private final ModelMapper modelMapper = new ModelMapper();
 
     public CurrencyServiceImpl(CurrencyRepository currencyRepository) {
         this.currencyRepository = currencyRepository;
@@ -22,6 +26,12 @@ public class CurrencyServiceImpl implements CurrencyService {
     @Override
     public List<CurrencyDto> getAll() {
         return currencyRepository.getAll();
+    }
+
+    @Override
+    public List<CurrencyDto> search(Specification<Currency> spec) {
+        return currencyRepository.findAll(spec).stream()
+                .map(this::convertToDto).collect(Collectors.toList());
     }
 
     @Override
@@ -54,5 +64,10 @@ public class CurrencyServiceImpl implements CurrencyService {
     @Override
     public void deleteById(Long id) {
         currencyRepository.deleteById(id);
+    }
+
+    private CurrencyDto convertToDto(Currency currency) {
+        CurrencyDto currencyDto = modelMapper.map(currency, CurrencyDto.class);
+        return currencyDto;
     }
 }
