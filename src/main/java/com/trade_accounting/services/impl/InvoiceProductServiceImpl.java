35054290d6ef05aa238a3442b1b5b06bae3bfd0f
Ilya Shamaev <ishamaev.is@gmail.com>
2021-03-13 19:33:40 +0400

    fix adding UnitDto to ProductDto in List of InvoiceProductDto

--- src/main/java/com/trade_accounting/services/impl/InvoiceProductServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/InvoiceProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.trade_accounting.models.dto.InvoiceProductDto;
 import com.trade_accounting.repositories.InvoiceProductRepository;
 import com.trade_accounting.repositories.InvoiceRepository;
 import com.trade_accounting.repositories.ProductRepository;
+import com.trade_accounting.repositories.UnitRepository;
 import com.trade_accounting.services.interfaces.InvoiceProductService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,20 +20,22 @@ public class InvoiceProductServiceImpl implements InvoiceProductService {
     private final InvoiceRepository invoiceRepository;
     private final ProductRepository productRepository;
     private final InvoiceProductRepository invoiceProductRepository;
+    private final UnitRepository unitRepository;
 
     public InvoiceProductServiceImpl(InvoiceRepository invoiceRepository,
                                      ProductRepository productRepository,
-                                     InvoiceProductRepository invoiceProductRepository) {
+                                     InvoiceProductRepository invoiceProductRepository, UnitRepository unitRepository) {
         this.invoiceRepository = invoiceRepository;
         this.productRepository = productRepository;
         this.invoiceProductRepository = invoiceProductRepository;
+        this.unitRepository = unitRepository;
     }
 
     @Override
     public List<InvoiceProductDto> getAll() {
 
         List<InvoiceProductDto> listInvoiceProductDto = invoiceProductRepository.getAll();
-        for (InvoiceProductDto invoiceProductDto: listInvoiceProductDto){
+        for (InvoiceProductDto invoiceProductDto : listInvoiceProductDto) {
             invoiceProductDto.setInvoiceDto(invoiceRepository.getById(invoiceProductDto.getInvoiceDto().getId()));
             invoiceProductDto.setProductDto(productRepository.getById(invoiceProductDto.getProductDto().getId()));
         }
@@ -42,11 +45,11 @@ public class InvoiceProductServiceImpl implements InvoiceProductService {
     @Override
     public List<InvoiceProductDto> getByInvoiceId(Long id) {
         List<InvoiceProductDto> invoiceProductList = invoiceProductRepository.getByInvoiceId(id);
-        for (InvoiceProductDto invoiceProductDto: invoiceProductList){
+        for (InvoiceProductDto invoiceProductDto : invoiceProductList) {
             invoiceProductDto.setInvoiceDto(invoiceRepository.getById(invoiceProductDto.getInvoiceDto().getId()));
             invoiceProductDto.setProductDto(productRepository.getById(invoiceProductDto.getProductDto().getId()));
+            invoiceProductDto.getProductDto().setUnitDto(unitRepository.getUnitByProductId(invoiceProductDto.getProductDto().getId()));
         }
-        System.out.println(invoiceProductList);
         return invoiceProductList;
     }
 
