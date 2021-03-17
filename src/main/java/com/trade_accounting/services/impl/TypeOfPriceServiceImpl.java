7a3e2a712aa0d163c6a046f56a2b87140b15fe4b
Rodion Shuhart <Radr.shuhart@ya.ru>
2021-03-17 13:39:58 +0600

    internal commit

--- src/main/java/com/trade_accounting/services/impl/TypeOfPriceServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/TypeOfPriceServiceImpl.java
@@ -4,10 +4,12 @@ import com.trade_accounting.models.TypeOfPrice;
 import com.trade_accounting.models.dto.TypeOfPriceDto;
 import com.trade_accounting.repositories.TypeOfPriceRepository;
 import com.trade_accounting.services.interfaces.TypeOfPriceService;
+import com.trade_accounting.utils.DtoMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -15,13 +17,18 @@ public class TypeOfPriceServiceImpl implements TypeOfPriceService {
 
     private final TypeOfPriceRepository typeOfPriceRepository;
 
-    public TypeOfPriceServiceImpl(TypeOfPriceRepository typeOfPriceRepository) {
+    private final DtoMapper dtoMapper;
+
+    public TypeOfPriceServiceImpl(TypeOfPriceRepository typeOfPriceRepository, DtoMapper dtoMapper) {
         this.typeOfPriceRepository = typeOfPriceRepository;
+        this.dtoMapper = dtoMapper;
     }
 
     @Override
     public List<TypeOfPriceDto> getAll() {
-        return typeOfPriceRepository.getAll();
+        return typeOfPriceRepository.findAll().stream()
+                .map(dtoMapper::typeOfPriceToTypeOfPriceDto)
+                .collect(Collectors.toList());
     }
 
     @Override
