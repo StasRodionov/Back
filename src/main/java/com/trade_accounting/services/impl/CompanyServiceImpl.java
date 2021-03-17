5f0b3ffb12aa14e8d18164c56878daf317fa8c70
Rodion Shuhart <Radr.shuhart@ya.ru>
2021-03-17 16:55:01 +0600

    fixed bankAccount bug

--- src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/CompanyServiceImpl.java
@@ -5,9 +5,7 @@ import com.trade_accounting.models.dto.CompanyDto;
 import com.trade_accounting.repositories.BankAccountRepository;
 import com.trade_accounting.repositories.CompanyRepository;
 import com.trade_accounting.repositories.LegalDetailRepository;
-import com.trade_accounting.repositories.TypeOfContractorRepository;
 import com.trade_accounting.services.interfaces.CompanyService;
-import com.trade_accounting.services.interfaces.LegalDetailService;
 import com.trade_accounting.utils.DtoMapper;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -69,16 +67,18 @@ public class CompanyServiceImpl implements CompanyService {
         Company company = dtoMapper.companyDtoToCompany(companyDto);
 
         company.setLegalDetail(
-                legalDetailRepository.findById(companyDto.getLegalDetailDto().getId())
-                        .orElse(null)
+                legalDetailRepository.save(
+                    dtoMapper.legalDetailDtoToLegalDetail(
+                            companyDto.getLegalDetailDto()
+                    )
+                )
         );
 
         company.setBankAccounts(
                 companyDto.getBankAccountDto().stream()
                 .map(
                         bankAccount -> bankAccountRepository
-                        .findById(bankAccount.getId())
-                        .orElse(null)
+                        .save(dtoMapper.bankAccountDtoToBankAccount(bankAccount))
                 )
                 .collect(Collectors.toList())
         );
@@ -90,7 +90,27 @@ public class CompanyServiceImpl implements CompanyService {
 
     @Override
     public CompanyDto update(CompanyDto companyDto) {
-        return create(companyDto);
+        Company company = dtoMapper.companyDtoToCompany(companyDto);
+
+        company.setLegalDetail(
+                legalDetailRepository.findById(
+                        companyDto.getLegalDetailDto().getId()
+                ).orElse(null)
+        );
+
+        company.setBankAccounts(
+                companyDto.getBankAccountDto().stream()
+                        .map(
+                                bankAccount -> bankAccountRepository
+                                        .findById(bankAccount.getId())
+                                        .orElse(null)
+                        )
+                        .collect(Collectors.toList())
+        );
+
+        companyRepository.save(company);
+
+        return companyDto;
     }
 
     @Override
