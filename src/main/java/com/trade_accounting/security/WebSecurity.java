ed68d2943b85d3f745b3ecaa4fa7f20e4ef293fa
Ilya Shamaev <ishamaev.is@gmail.com>
2021-03-18 16:46:06 +0400

    refactor with dtoMapper for getting InvoiceProductDto by InvoiceId

--- src/main/java/com/trade_accounting/security/WebSecurity.java
+++ src/main/java/com/trade_accounting/security/WebSecurity.java
@@ -33,26 +33,26 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //Security disabled
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
-
-        //Security enabled
 //        http
-//                .cors()
-//                .and()
-//                    .csrf().disable()
-//                    .exceptionHandling()
-//                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-//                .and()
-//                    .authorizeRequests()
-//                    .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-//                    .anyRequest().authenticated()
-//                .and()
-//                    .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-//                    .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-//                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/**").permitAll();
+
+//        Security enabled
+        http
+                .cors()
+                .and()
+                    .csrf().disable()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                    .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
 
