16266f9a7e55f698095082ee3fb9a5569a7a9baf
DaryaBelyankina <polarangel@yandex.ru>
2021-03-22 16:49:24 +0300

    fixed mapping and tests of contracts

--- src/test/java/com/trade_accounting/services/impl/DtoStubs.java
+++ src/test/java/com/trade_accounting/services/impl/DtoStubs.java
@@ -2,6 +2,7 @@ package com.trade_accounting.services.impl;
 
 import com.trade_accounting.models.dto.AttributeOfCalculationObjectDto;
 import com.trade_accounting.models.dto.CompanyDto;
+import com.trade_accounting.models.dto.ContractDto;
 import com.trade_accounting.models.dto.TypeOfContractorDto;
 import com.trade_accounting.utils.DtoMapper;
 import org.mapstruct.factory.Mappers;
@@ -25,5 +26,9 @@ public class DtoStubs {
         return dtoMapper.attributeOfCalculationObjectToAttributeOfCalculationObjectDto(
                 ModelStubs.getAttributeOfCalculationObject(id)
         );
+    }
+
+    public static ContractDto getContractDto(Long id){
+        return dtoMapper.contractToContractDto(ModelStubs.getContract(id));
     }
 }
