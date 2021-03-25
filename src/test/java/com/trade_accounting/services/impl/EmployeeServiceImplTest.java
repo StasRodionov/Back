ba71c28b47d007e060bb9d6d3399db9fbfdf2c98
ShaipovZA <z.shaipov@id-mt.ru>
2021-03-25 17:57:00 +0300

    fixed employee image

--- src/test/java/com/trade_accounting/services/impl/EmployeeServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/EmployeeServiceImplTest.java
@@ -135,7 +135,7 @@ class EmployeeServiceImplTest {
 
     @Test
     void create_shouldPassInstructionsSuccessfulCreate() {
-        employeeService.create(
+        employeeService.save(
             DtoStubs.getEmployeeDto(1L)
         );
 
@@ -148,7 +148,7 @@ class EmployeeServiceImplTest {
 
     @Test
     void update_shouldPassInstructionsSuccessfulUpdate() {
-        employeeService.update(
+        employeeService.save(
             DtoStubs.getEmployeeDto(1L)
         );
 
