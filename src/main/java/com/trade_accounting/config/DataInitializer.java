f250814e6094533231c740757739aebfd24dbaca
Alex Alex <lex156166@gmail.com>
2021-03-30 19:32:51 +0400

    Change DataInitializer

--- src/main/java/com/trade_accounting/config/DataInitializer.java
+++ src/main/java/com/trade_accounting/config/DataInitializer.java
@@ -12,6 +12,7 @@ import com.trade_accounting.models.dto.ContractorGroupDto;
 import com.trade_accounting.models.dto.CurrencyDto;
 import com.trade_accounting.models.dto.DepartmentDto;
 import com.trade_accounting.models.dto.EmployeeDto;
+import com.trade_accounting.models.dto.ImageDto;
 import com.trade_accounting.models.dto.InvoiceDto;
 import com.trade_accounting.models.dto.LegalDetailDto;
 import com.trade_accounting.models.dto.PaymentDto;
@@ -162,6 +163,8 @@ public class DataInitializer {
         initTaxSystems();
         initProductGroups();
         initCurrency();
+        initImage();
+        initTypeOfPrices();
 
         initLegalDetails();
         initCompanies();
@@ -437,6 +440,14 @@ public class DataInitializer {
         currencyService.create(new CurrencyDto(null, "rubles", "Russian Rubles", "25", "rub", "1"));
         currencyService.create(new CurrencyDto(null, "bel rubles", "Bellarusian Rubles", "25", "belrub", "2"));
         currencyService.create(new CurrencyDto(null, "eng dollar", "USA Dollars ", "25", "dol", "3"));
+    }
+
+    private void initImage() {
+        imageService.create(new ImageDto(null,  null, "1"), "employees");
+        imageService.create(new ImageDto(null,  null, "2"), "employees");
+        imageService.create(new ImageDto(null,  null, "3"), "employees");
+        imageService.create(new ImageDto(null,  null, "4"), "employees");
+        imageService.create(new ImageDto(null,  null, "5"), "employees");
     }
 
     private void initLegalDetails() {
