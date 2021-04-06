372377be5c10b70bc5bf7a5cf925538428e29237
Andrew_Zh <fjf3891@gmail.com>
2021-04-06 17:54:26 +0300

    getAll() and getById() pointcuts implemented

--- src/main/java/com/trade_accounting/aspects/LoggingPointcuts.java
+++ src/main/java/com/trade_accounting/aspects/LoggingPointcuts.java
@@ -1,6 +1,7 @@
 package com.trade_accounting.aspects;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -14,12 +15,24 @@ public class LoggingPointcuts {
         @Pointcut("within(com.trade_accounting.services..*)")
         public void inServiceLayer() {}
 
-        @Pointcut("execution(* getAll(..))")
+        @Pointcut("execution(* getAll())")
         public void getAllExecution() {}
 
+        @Pointcut("execution(* getById(..))")
+        public void getByIdExecution() {}
+
         @Before("inServiceLayer() && getAllExecution()")
-        public void beforeAnyServiceMethod() {
-            log.info("Hello from aspect");
+        public void getAllMethod(JoinPoint joinPoint) {
+                var serviceName = joinPoint.getTarget().getClass().getSimpleName();
+                var entityTypeName = serviceName.replaceFirst("ServiceImpl", "") + "Dto";
+                log.info("Запрошен список {}", entityTypeName);
+        }
+
+        @Before(value = "inServiceLayer() && getByIdExecution() && args(id)")
+        public void getByIdMethod(JoinPoint joinPoint, Long id) {
+                var serviceName = joinPoint.getTarget().getClass().getSimpleName();
+                var entityTypeName = serviceName.replaceFirst("ServiceImpl", "") + "Dto";
+                log.info("Запрошен экземпляр {} с id = {}", entityTypeName, id);
         }
 
 }
