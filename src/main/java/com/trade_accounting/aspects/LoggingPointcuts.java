e070e7a44eea2cd95b8b3eb88d2e4c142dfa7d86
Andrew_Zh <fjf3891@gmail.com>
2021-04-07 14:13:03 +0300

    basic logging for crud operations

--- src/main/java/com/trade_accounting/aspects/LoggingPointcuts.java
+++ src/main/java/com/trade_accounting/aspects/LoggingPointcuts.java
@@ -1,5 +1,6 @@
 package com.trade_accounting.aspects;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
@@ -21,18 +22,48 @@ public class LoggingPointcuts {
         @Pointcut("execution(* getById(..))")
         public void getByIdExecution() {}
 
+        @Pointcut("execution(* create(..))")
+        public void createExecution() {}
+
+        @Pointcut("execution(* update(..))")
+        public void updateExecution() {}
+
+        @Pointcut("execution(* deleteById(..))")
+        public void deleteExecution() {}
+
         @Before("inServiceLayer() && getAllExecution()")
         public void getAllMethod(JoinPoint joinPoint) {
-                var serviceName = joinPoint.getTarget().getClass().getSimpleName();
-                var entityTypeName = serviceName.replaceFirst("ServiceImpl", "") + "Dto";
-                log.info("Запрошен список {}", entityTypeName);
+                log.info("Запрошен список {}", getDtoName(joinPoint));
         }
 
         @Before(value = "inServiceLayer() && getByIdExecution() && args(id)")
-        public void getByIdMethod(JoinPoint joinPoint, Long id) {
-                var serviceName = joinPoint.getTarget().getClass().getSimpleName();
-                var entityTypeName = serviceName.replaceFirst("ServiceImpl", "") + "Dto";
-                log.info("Запрошен экземпляр {} с id = {}", entityTypeName, id);
+        public void logGetById(JoinPoint joinPoint, Long id) {
+                log.info("Запрошен экземпляр {} с id={}", getDtoName(joinPoint), id);
         }
 
+        @Before(value = "inServiceLayer() && createExecution() && args(dto)")
+        public void createMethod(JoinPoint joinPoint, Object dto) {
+                log.info("Создан экземпляр {}: {}", getDtoName(joinPoint), dto);
+        }
+
+        @Before(value = "inServiceLayer() && updateExecution() && args(dto)")
+        public void logUpdate(JoinPoint joinPoint, Object dto) {
+                log.info("Обновлен экземпляр {} с id={}", getDtoName(joinPoint), getId(dto));
+        }
+
+        @Before(value = "inServiceLayer() && deleteExecution() && args(id)")
+        public void logDelete(JoinPoint joinPoint, Long id) {
+                log.info("Удален экземпляр {} с id={}", getDtoName(joinPoint), id);
+        }
+
+        private String getDtoName(JoinPoint joinPoint) {
+                var serviceName = joinPoint.getTarget().getClass().getSimpleName();
+                return serviceName.replaceFirst("ServiceImpl", "Dto") ;
+        }
+
+        @SneakyThrows
+        private Long getId(Object dto) {
+                return (Long) dto.getClass().getMethod("getId", new Class<?>[]{})
+                                .invoke(dto, new Object[]{});
+        }
 }
