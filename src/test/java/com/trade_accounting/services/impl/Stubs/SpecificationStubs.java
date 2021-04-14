dfa2d4b8c56cbc9a47bc892274d9b9d15cd6f304
Roman Vokhmin <vohmin77@gmail.com>
2021-04-14 04:50:28 +0000

    Убран DTO из репозитория, сервис покрыт тестами, в сервисе сделан маппинг

--- src/test/java/com/trade_accounting/services/impl/Stubs/SpecificationStubs.java
+++ src/test/java/com/trade_accounting/services/impl/Stubs/SpecificationStubs.java
@@ -4,12 +4,8 @@ import com.trade_accounting.models.Company;
 import com.trade_accounting.models.Contract;
 import com.trade_accounting.models.Currency;
 import com.trade_accounting.models.Employee;
+import com.trade_accounting.models.Product;
 import org.springframework.data.jpa.domain.Specification;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 
 public class SpecificationStubs {
     public static Specification<Employee> getEmployeeSpecificationStub() {
@@ -25,6 +21,9 @@ public class SpecificationStubs {
     }
 
     public static Specification<Currency> getCurrencySpecificationStub() {
+        return (root, criteriaQuery, criteriaBuilder) -> null;
+    }
+    public static Specification<Product> getProductSpecificationStub() {
         return (root, criteriaQuery, criteriaBuilder) -> null;
     }
 }
