cae01d52a0afd285175f8b961438f17ffbc6485a
Gauss-BRO <novel764@gmail.com>
2021-04-14 22:46:18 +0700

    "add ignore case"

--- src/main/java/com/trade_accounting/repositories/EmployeeRepository.java
+++ src/main/java/com/trade_accounting/repositories/EmployeeRepository.java
@@ -59,7 +59,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSp
     Optional<Employee> findByEmail(String email);
 
     @Query("from Employee e " +
-            "where concat(e.firstName, ' ', e.middleName, ' ', e.lastName , ' ', e.email, ' ', e.phone) " +
+            "where concat(lower(e.firstName), ' ', lower(e.middleName), ' ', lower(e.lastName), ' ', lower(e.email)" +
+            ", ' ',e.phone) " +
             "like concat('%', :symbols, '%')")
     List<Employee> getBySymbols(@Param("symbols") String symbols);
 
