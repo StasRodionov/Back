6b89223da46a18ea48f3f06b921380adac8070e7
Andrew_Zh <fjf3891@gmail.com>
2021-04-19 12:54:02 +0300

    all advices changed to AfterReturning

--- src/main/java/com/trade_accounting/aspects/LoggingPointcuts.java
+++ src/main/java/com/trade_accounting/aspects/LoggingPointcuts.java
@@ -38,12 +38,12 @@ public class LoggingPointcuts {
         @Pointcut("execution(* search(..))")
         public void searchExecution() {}
 
-        @After("inServiceLayer() && getAllExecution()")
+        @AfterReturning("inServiceLayer() && getAllExecution()")
         public void logGetAll(JoinPoint joinPoint) {
                 log.info("Запрошен список {}", getDtoName(joinPoint));
         }
 
-        @After(value = "inServiceLayer() && getByIdExecution() && args(id)")
+        @AfterReturning(value = "inServiceLayer() && getByIdExecution() && args(id)")
         public void logGetById(JoinPoint joinPoint, Long id) {
                 log.info("Запрошен экземпляр {} с id={}", getDtoName(joinPoint), id);
         }
@@ -55,33 +55,28 @@ public class LoggingPointcuts {
                 } else {
                         log.info("Создан экземпляр {}: {}", dto.getClass().getSimpleName(), dto);
                 }
-
         }
 
-        @After(value = "inServiceLayer() && voidCreateExecution() && args(dto)")
+        @AfterReturning(value = "inServiceLayer() && voidCreateExecution() && args(dto)")
         public void logVoidCreate(Object dto) {
                 log.info("Создан экземпляр {}", dto.getClass().getSimpleName());
         }
 
-        @After(value = "inServiceLayer() && updateExecution() && args(dto)")
+        @AfterReturning(value = "inServiceLayer() && updateExecution() && args(dto)")
         public void logUpdate(Object dto) {
                 log.info("Обновлен экземпляр {} с id={}", dto.getClass().getSimpleName(), getId(dto));
         }
 
-        @After(value = "inServiceLayer() && deleteExecution() && args(id)")
+        @AfterReturning(value = "inServiceLayer() && deleteExecution() && args(id)")
         public void logDelete(JoinPoint joinPoint, Long id) {
                 log.info("Удален экземпляр {} с id={}", getDtoName(joinPoint), id);
         }
 
         @AfterReturning(value = "inServiceLayer() && searchExecution()",  returning = "result")
         public void logSearch(JoinPoint joinPoint, Object result) {
-                if(result == null) {
-                        // search method is void
-                } else {
-                        log.info("Найдены экземпляры {}: {}", getDtoName(joinPoint), result);
-                }
-
+                log.info("Найдены экземпляры {}: {}", getDtoName(joinPoint), result);
         }
+
         private String getDtoName(JoinPoint joinPoint) {
                 var serviceName = joinPoint.getTarget().getClass().getSimpleName();
                 return serviceName.replaceFirst("ServiceImpl", "Dto") ;
