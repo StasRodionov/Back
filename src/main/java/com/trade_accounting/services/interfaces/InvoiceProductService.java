36f18d54b8143c57a97a8720ec6383c5f57f3053
Andrew_Zh <fjf3891@gmail.com>
2021-04-24 18:59:48 +0300

    Transactional annotation added to default methods

--- src/main/java/com/trade_accounting/services/interfaces/InvoiceProductService.java
+++ src/main/java/com/trade_accounting/services/interfaces/InvoiceProductService.java
@@ -3,13 +3,15 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.InvoiceProduct;
 import com.trade_accounting.models.dto.InvoiceProductDto;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface InvoiceProductService extends AbstractService<InvoiceProductDto>,
         SearchableService<InvoiceProduct, InvoiceProductDto> {
 
-   default List<InvoiceProductDto> searchByInvoiceId(Long id) {
+    @Transactional
+    default List<InvoiceProductDto> searchByInvoiceId(Long id) {
         return search((root, query, builder) ->
                 builder.equal(root.get("invoice").get("id"), id));
     }
