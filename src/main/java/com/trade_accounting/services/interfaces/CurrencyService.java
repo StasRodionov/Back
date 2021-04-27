f319e9094465b5029ebbf9e2b07ef1f92002880c
Andrew_Zh <fjf3891@gmail.com>
2021-04-27 17:12:59 +0300

    search method refactored

--- src/main/java/com/trade_accounting/services/interfaces/CurrencyService.java
+++ src/main/java/com/trade_accounting/services/interfaces/CurrencyService.java
@@ -2,11 +2,7 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.Currency;
 import com.trade_accounting.models.dto.CurrencyDto;
-import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
+public interface CurrencyService extends AbstractService<CurrencyDto>, SearchableService<Currency, CurrencyDto> {
 
-public interface CurrencyService extends AbstractService<CurrencyDto> {
-
-    List<CurrencyDto> search(Specification<Currency> specification);
 }
