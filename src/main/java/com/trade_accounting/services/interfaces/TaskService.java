2be0a64ce227623cdffd681153b7e3d9e8945b97
Andrew_Zh <fjf3891@gmail.com>
2021-04-27 18:53:30 +0300

    extends SearchableService

--- src/main/java/com/trade_accounting/services/interfaces/TaskService.java
+++ src/main/java/com/trade_accounting/services/interfaces/TaskService.java
@@ -2,14 +2,11 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.Task;
 import com.trade_accounting.models.dto.TaskDto;
-import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
-public interface TaskService extends AbstractService<TaskDto>{
+public interface TaskService extends AbstractService<TaskDto>, SearchableService<Task, TaskDto>{
 
     void createAll(List<TaskDto> dtos);
-
-    List<TaskDto> search(Specification<Task> specification);
 
 }
