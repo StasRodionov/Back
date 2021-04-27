1c692d858ac648e81531cf40aed39afbdc78b965
Andrew_Zh <fjf3891@gmail.com>
2021-04-27 17:00:31 +0300

    search method refactored

--- src/main/java/com/trade_accounting/services/interfaces/CompanyService.java
+++ src/main/java/com/trade_accounting/services/interfaces/CompanyService.java
@@ -2,13 +2,8 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.Company;
 import com.trade_accounting.models.dto.CompanyDto;
-import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
-
-public interface CompanyService extends AbstractService<CompanyDto>   {
-
-    List<CompanyDto> search(Specification<Company> specification);
+public interface CompanyService extends AbstractService<CompanyDto>, SearchableService<Company, CompanyDto> {
 
     CompanyDto getByEmail(String email);
 }
