ac826d2ee57d514959d3ceda6a7ab842f21c56f4
Andrew_Zh <fjf3891@gmail.com>
2021-04-28 15:40:39 +0300

    getByInvoiceId test removed

--- src/test/java/com/trade_accounting/services/impl/InvoiceProductServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/InvoiceProductServiceImplTest.java
@@ -98,31 +98,6 @@ class InvoiceProductServiceImplTest {
         invoiceProductListDtoIsCorrectlyInited(invoiceProductDto);
     }
 
-   /* @Test
-    void getByInvoiceId_shouldReturnListOfInvoiceProduct() {
-        when(invoiceProductRepository.getByInvoiceId(anyLong()))
-                .thenReturn(
-                        Stream.of(
-                                ModelStubs.getInvoiceProduct(1L),
-                                ModelStubs.getInvoiceProduct(2L),
-                                ModelStubs.getInvoiceProduct(3L)
-                        ).collect(Collectors.toList())
-                );
-        List<InvoiceProductDto> invoiceProductDtoList = invoiceProductService.searchByInvoiceId(1L);
-        assertNotNull(
-                invoiceProductDtoList,
-                "failure - expected that a list of invoiceProductDto not null"
-        );
-        assertTrue(
-                invoiceProductDtoList.size() > 0,
-                "failure - expected that a list of invoiceProductDto grater than 0"
-        );
-        for (InvoiceProductDto invoiceProductDto : invoiceProductDtoList) {
-            invoiceProductListDtoIsCorrectlyInited(invoiceProductDto);
-        }
-
-    }
-*/
     @Test
     void create_shouldPassInstructionsSuccessfulCreate() {
         invoiceProductService.create(
