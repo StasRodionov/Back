b4d287c9f78c27083a6da24132929086fa5a6191
krasnovss91 <krasnovstanislav91@gmail.com>
2021-05-20 09:52:02 +0400

    fias_tests_1

---
+++ src/test/java/com/trade_accounting/services/impl/fias/StreetService.java
@@ -0,0 +1,9 @@
+package com.trade_accounting.services.impl.fias;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class StreetService {
+    
+}
