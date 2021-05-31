6722db8b0e5ecec7665d85bafa86bca165d3f21d
krasnovss91 <krasnovstanislav91@gmail.com>
2021-05-31 19:23:37 +0400

    ProductionRepository

---
+++ src/main/java/com/trade_accounting/repositories/ProductionRepository.java
@@ -0,0 +1,8 @@
+package com.trade_accounting.repositories;
+
+import com.trade_accounting.models.dto.ProductionDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductionRepository extends JpaRepository<ProductionDto, Long> {
+    
+}
