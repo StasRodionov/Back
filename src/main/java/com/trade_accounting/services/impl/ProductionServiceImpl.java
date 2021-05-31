2e3f26622bbb6162c74205f632df17dfe0bea19a
krasnovss91 <krasnovstanislav91@gmail.com>
2021-05-31 19:29:50 +0400

    ProductionServiceImpl_1

---
+++ src/main/java/com/trade_accounting/services/impl/ProductionServiceImpl.java
@@ -0,0 +1,56 @@
+package com.trade_accounting.services.impl;
+
+import com.trade_accounting.models.Product;
+import com.trade_accounting.models.dto.PageDto;
+import com.trade_accounting.models.dto.ProductDto;
+import com.trade_accounting.services.interfaces.ProductService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProductionServiceImpl implements ProductService {
+    @Override
+    public List<ProductDto> getAll() {
+        return null;
+    }
+
+    @Override
+    public ProductDto getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ProductDto create(ProductDto dto) {
+        return null;
+    }
+
+    @Override
+    public ProductDto update(ProductDto dto) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public PageDto<ProductDto> search(Specification<Product> specification, Pageable page) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> search(String value) {
+        return null;
+    }
+
+    @Override
+    public List<ProductDto> search(Specification<Product> spec) {
+        return null;
+    }
+}
