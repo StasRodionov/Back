1e6dc8555ade1c2bc3001ca1cc872c8eceeb8e1f
krasnovss91 <krasnovstanislav91@gmail.com>
2021-05-31 19:34:15 +0400

    ProductionServiceImpl_2

--- src/main/java/com/trade_accounting/services/impl/ProductionServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/ProductionServiceImpl.java
@@ -3,7 +3,9 @@ package com.trade_accounting.services.impl;
 import com.trade_accounting.models.Product;
 import com.trade_accounting.models.dto.PageDto;
 import com.trade_accounting.models.dto.ProductDto;
+import com.trade_accounting.repositories.ProductionRepository;
 import com.trade_accounting.services.interfaces.ProductService;
+import com.trade_accounting.utils.DtoMapper;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,16 @@ import java.util.List;
 @Service
 @Transactional
 public class ProductionServiceImpl implements ProductService {
+
+    private final ProductionRepository productionRepository;
+
+    private final DtoMapper dtoMapper;
+
+    public ProductionServiceImpl(ProductionRepository productionRepository, DtoMapper dtoMapper){
+        this.productionRepository = productionRepository;
+        this.dtoMapper = dtoMapper;
+    }
+
     @Override
     public List<ProductDto> getAll() {
         return null;
