818a23ad818ba9098d1f738c9bc26e295459896f
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-01 09:04:14 +0400

    ProductionServiceImpl_6

--- src/main/java/com/trade_accounting/services/impl/ProductionServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/ProductionServiceImpl.java
@@ -1,16 +1,10 @@
 package com.trade_accounting.services.impl;
 
-import com.trade_accounting.models.Product;
 import com.trade_accounting.models.Production;
-import com.trade_accounting.models.dto.PageDto;
-import com.trade_accounting.models.dto.ProductDto;
 import com.trade_accounting.models.dto.ProductionDto;
 import com.trade_accounting.repositories.ProductionRepository;
-import com.trade_accounting.services.interfaces.ProductService;
 import com.trade_accounting.services.interfaces.ProductionService;
 import com.trade_accounting.utils.DtoMapper;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,44 +19,32 @@ public class ProductionServiceImpl implements ProductionService {
 
     private final DtoMapper dtoMapper;
 
-    public ProductionServiceImpl(ProductionRepository productionRepository, DtoMapper dtoMapper){
+    public ProductionServiceImpl(ProductionRepository productionRepository, DtoMapper dtoMapper) {
         this.productionRepository = productionRepository;
         this.dtoMapper = dtoMapper;
     }
 
     @Override
     public List<ProductionDto> getAll() {
-        /*
-        final List<ProductionDto> collect = productionRepository.findAll().stream()
-                .map(dtoMapper::productionToProductionDto)
-                .collect(Collectors.toList());
-        return collect;
 
-         */
-        return null;
+       final List<ProductionDto> collect = productionRepository.findAll().stream()
+               .map(dtoMapper:: productionToProductionDto)
+               .collect(Collectors.toList());
+       return collect;
     }
 
 
     @Override
     public ProductionDto getById(Long id) {
-        /*
-                return dtoMapper.projectToProjectDto(
-                projectRepository.findById(id).orElse(new Project())
-        );
-         */
-       return null;
+        return dtoMapper.productionToProductionDto(productionRepository.findById(id).orElse(new Production()));
+
     }
 
     @Override
     public ProductionDto create(ProductionDto dto) {
-        /*
-
-        Production production = productionRepository.save(
-                dtoMapper.productionDtoToProduction(dto)
-        );
+        Production production = productionRepository.save(dtoMapper.productionDtoToProduction(dto));
         dto.setId(production.getId());
         return dtoMapper.productionToProductionDto(production);
-         */
         return null;
     }
 
