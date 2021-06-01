61d5693de65394455b3073260e0d8244aa707b58
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-01 10:02:59 +0400

    controller_1

--- src/main/java/com/trade_accounting/controllers/rest/ProductionRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/ProductionRestController.java
@@ -1,9 +1,20 @@
 package com.trade_accounting.controllers.rest;
 
+import com.trade_accounting.services.interfaces.ProductionService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Production Rest Controller", description = "CRUD операции с производством")
+@Api(tags = "Production Rest Controller")
 @RequestMapping("/api/production")
 public class ProductionRestController {
+
+    private final ProductionService productionService;
+
+    public ProductionRestController(ProductionService productionService){ this.productionService = productionService; }
+
+
 }
