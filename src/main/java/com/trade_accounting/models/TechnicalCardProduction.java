c0d79abb8fffb5be5b6cd6f45754aa4509f4968c
Evgeniy Filatov <filatov.evgeny13@mail.ru>
2021-06-01 12:20:23 +0500

    Technical card model

---
+++ src/main/java/com/trade_accounting/models/TechnicalCardProduction.java
@@ -0,0 +1,32 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "technical_card_productions")
+public class TechnicalCardProduction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Product product;
+}
