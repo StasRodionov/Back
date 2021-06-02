3a9989e98002784570816bb050593dd6324b07f9
Evgeniy Filatov <filatov.evgeny13@mail.ru>
2021-06-02 14:18:34 +0500

    technical card repo and service

---
+++ src/main/java/com/trade_accounting/services/impl/TechnicalCardGroupServiceImpl.java
@@ -0,0 +1,57 @@
+package com.trade_accounting.services.impl;
+
+import com.trade_accounting.models.TechnicalCardGroup;
+import com.trade_accounting.models.dto.TechnicalCardGroupDto;
+import com.trade_accounting.repositories.TechnicalCardGroupRepository;
+import com.trade_accounting.services.interfaces.TechnicalCardGroupService;
+import com.trade_accounting.utils.DtoMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class TechnicalCardGroupServiceImpl implements TechnicalCardGroupService {
+
+    private final TechnicalCardGroupRepository technicalCardGroupRepository;
+
+    private final DtoMapper dtoMapper;
+
+    public TechnicalCardGroupServiceImpl(TechnicalCardGroupRepository technicalCardGroupRepository, DtoMapper dtoMapper) {
+        this.technicalCardGroupRepository = technicalCardGroupRepository;
+        this.dtoMapper = dtoMapper;
+    }
+
+
+    @Override
+    public List<TechnicalCardGroupDto> getAll() {
+        return technicalCardGroupRepository.findAll().stream()
+                .map(dtoMapper::technicalCardGroupToTechnicalCardGroupDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TechnicalCardGroupDto getById(Long id) {
+        return dtoMapper.technicalCardGroupToTechnicalCardGroupDto(
+                technicalCardGroupRepository.findById(id).orElse(new TechnicalCardGroup())
+        );
+    }
+
+    @Override
+    public TechnicalCardGroupDto create(TechnicalCardGroupDto dto) {
+        return dtoMapper.technicalCardGroupToTechnicalCardGroupDto(technicalCardGroupRepository
+                .save(dtoMapper.technicalCardGroupDtoToTechnicalCardGroup(dto)));
+    }
+
+    @Override
+    public TechnicalCardGroupDto update(TechnicalCardGroupDto dto) {
+        return create(dto);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        technicalCardGroupRepository.deleteById(id);
+    }
+}
