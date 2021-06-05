5e24eb88de5e89d618af0d2788e6ad30f884186a
Nikolay Rednov <red.niko@yandex.ru>
2021-06-05 22:19:47 +0400

    создал модель и dto

---
+++ src/main/java/com/trade_accounting/models/InvoicesToCustomers.java
@@ -0,0 +1,60 @@
+package com.trade_accounting.models;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data  //Класс счета покупателям.
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "invoiceToCustomers")
+public class InvoicesToCustomers {
+
+    @Id
+    @NotNull
+    private Long id;
+
+    @NotNull
+    @Column(name = "data")
+    private LocalDateTime date;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Warehouse warehouse;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contractor contractor;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contract contract;
+
+    @Column(name = "is_Spend")
+    @ColumnDefault("false")
+    private boolean isSpend;
+
+    @Column(name = "comment")
+    private String comment;
+
+
+}
---
+++ src/main/java/com/trade_accounting/models/dto/InvoicesToCustomersDto.java
@@ -0,0 +1,49 @@
+package com.trade_accounting.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvoicesToCustomersDto {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String date;
+
+    @NotNull
+    private CompanyDto companyDto;
+
+    @NotNull
+    private WarehouseDto warehouseDto;
+
+    @NotNull
+    private ContractDto contractDto;
+
+    private boolean isSpend;
+
+    private String comment;
+
+    public InvoicesToCustomersDto(Long id, LocalDateTime date,
+                                  Long companyId, Long contractId,
+                                  Long warehouseId, boolean isSpend,
+                                  String comment) {
+        this.id = id;
+        this.date = date.toString();
+        this.companyDto = new CompanyDto();
+        this.companyDto.setId(companyId);
+        this.contractDto = new ContractDto();
+        this.contractDto.setId(contractId);
+        this.warehouseDto = new WarehouseDto();
+        this.warehouseDto.setId(warehouseId);
+        this.isSpend = isSpend;
+        this.comment = comment;
+    }
+}
