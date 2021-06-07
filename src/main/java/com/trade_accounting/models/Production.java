83472d37f405c86cc3fa76fe26c31565b3ad6c98
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-07 13:22:41 +0400

    model_productions

--- src/main/java/com/trade_accounting/models/Production.java
+++ src/main/java/com/trade_accounting/models/Production.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "production ")
+@Table(name = "productions")
 public class Production {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
