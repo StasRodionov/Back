836299d369270e1b8a81d580d021a6d385e04fb2
code-tzh <code.tzh@gmail.com>
2021-06-09 18:09:21 +0300

    Technical Operations model (edit)

--- src/main/java/com/trade_accounting/models/TechnicalOperations.java
+++ src/main/java/com/trade_accounting/models/TechnicalOperations.java
@@ -1,44 +1,47 @@
 package com.trade_accounting.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "tech_operations")
+@Builder
+@Table(name = "technical_operations")
 public class TechnicalOperations {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "number")
+    @Column
     @NotNull
     @ColumnDefault(value = "00001")
     private String number;
 
-    @Column(name = "time")
-    private LocalDateTime time;
+    @Column
+    private LocalDate dateOfTheOperation;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     private TechnicalCard technicalCard;
 
-    @Column(name = "volume")
-    private Integer volumeProduction;
+    @Column
+    private Integer volume;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     private Warehouse warehouse;
 }
