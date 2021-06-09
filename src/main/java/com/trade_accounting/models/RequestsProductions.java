c70f4de77d9955d20ee7358c6fe33fa40561ec30
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-09 13:55:15 +0400

    model_RP_3

--- src/main/java/com/trade_accounting/models/RequestsProductions.java
+++ src/main/java/com/trade_accounting/models/RequestsProductions.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -21,10 +23,17 @@ public class RequestsProductions {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-  //осталось добавить дату
+
+    @Column(name = "number_of_the_certificate")
+    private String numberOfTheCertificate;
+
+    @Column(name = "date_of_the_certificate")
+    private LocalDate dateOfTheCertificate;
+
     @OneToOne
     private TechnicalCard technicalCard;
 
+    @Column(name = "volume")
     private Integer volume;
 
     @OneToOne
