9728226b1e4ae8a679a8e474daf8c55fc4679793
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-12 13:12:47 +0400

    dto_2

--- src/main/java/com/trade_accounting/models/RequestsProductions.java
+++ src/main/java/com/trade_accounting/models/RequestsProductions.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
