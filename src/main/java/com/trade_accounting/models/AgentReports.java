28b834890d755a5f1209e3f59ee633824048007b
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-16 13:38:17 +0400

    model_AgentReports_3

--- src/main/java/com/trade_accounting/models/AgentReports.java
+++ src/main/java/com/trade_accounting/models/AgentReports.java
@@ -5,11 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -21,5 +26,20 @@ public class AgentReports {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String documentType;
+
+    @Column(name = "time")
+    private LocalDateTime time;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contractor contractor;
+
     
+
 }
