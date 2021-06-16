f91d7fa591177880ee0c06fe7839aff1061bc760
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-16 16:17:29 +0400

    service_3

--- src/main/java/com/trade_accounting/services/impl/AgentReportsServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/AgentReportsServiceImpl.java
@@ -1,11 +1,27 @@
 package com.trade_accounting.services.impl;
 
 import com.trade_accounting.models.dto.AgentReportsDto;
+import com.trade_accounting.repositories.AgentReportsRepository;
 import com.trade_accounting.services.interfaces.AgentReportsService;
+import com.trade_accounting.utils.DtoMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class AgentReportsServiceImpl implements AgentReportsService {
+
+    private final AgentReportsRepository agentReportsRepository;
+
+    private final DtoMapper dtoMapper;
+
+    public AgentReportsServiceImpl(AgentReportsRepository agentReportsRepository, DtoMapper dtoMapper){
+        this.agentReportsRepository = agentReportsRepository;
+        this.dtoMapper = dtoMapper;
+    }
+    
     @Override
     public List<AgentReportsDto> getAll() {
         return null;
