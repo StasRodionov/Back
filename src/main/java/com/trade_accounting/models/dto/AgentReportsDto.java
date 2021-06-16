1d79ec441a9dd4d2a2f5de625f6b21750335f91e
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-16 14:14:14 +0400

    dto_2

--- src/main/java/com/trade_accounting/models/dto/AgentReportsDto.java
+++ src/main/java/com/trade_accounting/models/dto/AgentReportsDto.java
@@ -5,10 +5,38 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AgentReportsDto {
-    
+
+    private Long id;
+
+    private String documentType;
+
+    private LocalDateTime time;
+
+    private CompanyDto companyDto;
+
+    private ContractorDto contractorDto;
+
+    private Long sum;
+
+    private Long remunirationSum;
+
+    private Long comitentSum;
+
+    private Long paid;
+
+    private String status;
+
+    private Long sent;
+
+    private Long printed;
+
+    private String commentary;
+
 }
