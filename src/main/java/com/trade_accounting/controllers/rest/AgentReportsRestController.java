c74a55605837f2b95f2541eeae577c79ae5e0c34
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-16 18:41:06 +0400

    controller_2

--- src/main/java/com/trade_accounting/controllers/rest/AgentReportsRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/AgentReportsRestController.java
@@ -1,4 +1,21 @@
 package com.trade_accounting.controllers.rest;
 
+import com.trade_accounting.services.interfaces.AgentReportsService;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "AgentReports Rest Controller", description = "CRUD операции с отчётами комиссионера")
+@Api(tags = "AgentReports Rest Controller")
+@RequestMapping("/api/agentReports")
 public class AgentReportsRestController {
+
+    private final AgentReportsService agentReportsService;
+
+    public AgentReportsRestController(AgentReportsService agentReportsService){
+        this.agentReportsService = agentReportsService;
+    }
+
 }
