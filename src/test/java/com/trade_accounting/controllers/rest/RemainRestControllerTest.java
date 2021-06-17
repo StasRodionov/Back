e19bce8a8559104d0745f53ffbc31f78384fcb7e
code-tzh <code.tzh@gmail.com>
2021-06-18 00:51:26 +0300

    Remain Rest Controller Test

--- src/test/java/com/trade_accounting/controllers/rest/RemainRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/RemainRestControllerTest.java
@@ -2,7 +2,6 @@ package com.trade_accounting.controllers.rest;
 
 import com.google.gson.Gson;
 import com.trade_accounting.models.dto.RemainDto;
-import com.trade_accounting.models.dto.WarehouseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
