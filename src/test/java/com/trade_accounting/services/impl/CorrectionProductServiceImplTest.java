5d02ca4cc65305c14c7b189ebba80755b418e323
RoGilgan <rogilgan@bk.ru>
2021-06-17 21:10:58 +0200

    Add test for service correction product

--- src/test/java/com/trade_accounting/services/impl/CorrectionProductServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/CorrectionProductServiceImplTest.java
@@ -1,13 +1,11 @@
 package com.trade_accounting.services.impl;
 
 import com.trade_accounting.models.CorrectionProduct;
-import com.trade_accounting.models.Product;
 import com.trade_accounting.models.dto.CorrectionProductDto;
 import com.trade_accounting.repositories.CorrectionProductRepository;
 import com.trade_accounting.repositories.ProductRepository;
 import com.trade_accounting.services.impl.Stubs.DtoStubs;
 import com.trade_accounting.services.impl.Stubs.ModelStubs;
-import com.trade_accounting.services.interfaces.CorrectionProductService;
 import com.trade_accounting.utils.DtoMapperImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,17 +14,14 @@ import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CorrectionProductServiceImplTest {
