68397dc92a06c015a2f16e1576fb8e3529f8c3d4
krasnovss91 <krasnovstanislav91@gmail.com>
2021-06-17 10:34:36 +0400

    service_4

--- src/main/java/com/trade_accounting/services/impl/PriceListServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/PriceListServiceImpl.java
@@ -1,11 +1,28 @@
 package com.trade_accounting.services.impl;
 
 import com.trade_accounting.models.dto.PriceListDto;
+import com.trade_accounting.repositories.PriceListRepository;
 import com.trade_accounting.services.interfaces.PriceListService;
+import com.trade_accounting.utils.DtoMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
+@Transactional
 public class PriceListServiceImpl implements PriceListService {
+
+
+    private final PriceListRepository priceListRepository;
+    private final DtoMapper dtoMapper;
+
+    public PriceListServiceImpl(PriceListRepository priceListRepository, DtoMapper dtoMapper) {
+        this.priceListRepository = priceListRepository;
+        this.dtoMapper = dtoMapper;
+    }
+
+
     @Override
     public List<PriceListDto> getAll() {
         return null;
