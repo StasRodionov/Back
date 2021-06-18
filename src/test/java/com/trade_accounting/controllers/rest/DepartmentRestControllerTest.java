e4bfb6f442ef733fe524b083f75bc836e458aa8b
Andrius007 <abramov-hills@mail.ru>
2021-06-18 10:46:14 +0300

    Тесты для DepartmentRestController

--- src/test/java/com/trade_accounting/controllers/rest/DepartmentRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/DepartmentRestControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -31,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     @AutoConfigureMockMvc
     @TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.yml"})
     @Sql(value = "/department-before.sql")
-    @WithUserDetails(value = "karimogon@mail.ru")
+    @WithMockUser
     public class DepartmentRestControllerTest {
 
     @Autowired
