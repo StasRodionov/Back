de7d9925cf1aa3d2ac0d6b90268912aeaf649e72
Dmitry Evstratov <evstratovdmitry1992@gmail.com>
2021-06-19 13:39:03 +0300

    created class model ReturnToSuppliers

---
+++ src/main/java/com/trade_accounting/models/ReturnToSuppliers.java
@@ -0,0 +1,64 @@
+package com.trade_accounting.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Класс-модель возвраты поставщикам
+ *
+ * @param id         - номер поставщика
+ * @param date       - время возврата
+ * @param warehouse  - возврат со склада
+ * @param company    - компания
+ * @param contractor - контрагент
+ * @param isSend     - отправлено
+ * @param isPrint    - напечатано
+ * @param comment    - комментарий
+ * @author Evstratov
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "returns_suppliers")
+public class ReturnToSuppliers {
+
+    @Id
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String date;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Warehouse warehouse;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Contractor contractor;
+
+    @ColumnDefault("false")
+    private Boolean isSend;
+
+    @ColumnDefault("false")
+    private Boolean isPrint;
+
+    @NotNull
+    private String comment;
+}
