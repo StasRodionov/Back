554fe36650045582bf8e67163bc3dc757fff12f6
Dmitry Evstratov <evstratovdmitry1992@gmail.com>
2021-06-29 16:20:59 +0300

    created file return_suppliers-before.sql

--- src/test/java/com/trade_accounting/services/impl/ReturnToSupplierServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/ReturnToSupplierServiceImplTest.java
@@ -1,4 +1,24 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.repositories.ReturnToSupplierRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class ReturnToSupplierServiceImplTest {
+
+    @Mock
+    ReturnToSupplierRepository returnToSupplierRepository;
+
+    @InjectMocks
+    ReturnToSupplierServiceImpl returnToSupplierService;
+
+    @Test
+    public void test(){
+
+    }
+
 }
