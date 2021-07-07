6eb8e2473acbc208fd2531e200b358e29dbf310c
mr-nicko89 <mr-nicko89@yandex.ru>
2021-07-07 14:44:37 +0300

    Частично сделал models: Movement, MovementProduct

---
+++ src/main/java/com/trade_accounting/models/Movement.java
@@ -0,0 +1,67 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "movement")
+public class Movement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "date")
+    private LocalDateTime date;
+
+//    Со склада
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Warehouse warehouseFrom;
+// На склад
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Warehouse warehouseTo;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    @Column(name = "is_sent")
+    @ColumnDefault("false")
+    private Boolean isSent = false;
+
+    @Column(name = "is_print")
+    @ColumnDefault("false")
+    private Boolean isPrint = false;
+
+//    @Column
+//    @ColumnDefault("false")
+//    private Boolean writeOffProduct = false;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<CorrectionProduct> correctionProducts;
+}
