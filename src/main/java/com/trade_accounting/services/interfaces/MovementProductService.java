65ce70c3b7400f79a78d73e259d97871d7f8de2f
Nickolay <mr-nicko89@yandex.ru>
2021-07-09 20:55:45 +0300

    Удалил лишнее

--- src/main/java/com/trade_accounting/services/interfaces/MovementProductService.java
+++
@@ -1,6 +0,0 @@
-package com.trade_accounting.services.interfaces;
-
-import com.trade_accounting.models.dto.MovementProductDto;
-
-public interface MovementProductService extends AbstractService<MovementProductDto>{
-}
--- src/main/java/com/trade_accounting/services/interfaces/MovementService.java
+++
@@ -1,6 +0,0 @@
-package com.trade_accounting.services.interfaces;
-
-import com.trade_accounting.models.dto.MovementDto;
-
-public interface MovementService extends AbstractService<MovementDto> {
-}
