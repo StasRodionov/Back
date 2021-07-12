8c43ae48c9f2703cb78ee3ffd981b3b14bd73caa
Dmitry Evstratov <evstratovdmitry1992@gmail.com>
2021-07-12 13:30:20 +0300

    first test created

--- src/test/java/com/trade_accounting/services/impl/AgentReportsServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/AgentReportsServiceImplTest.java
@@ -3,14 +3,17 @@ package com.trade_accounting.services.impl;
 
 import com.trade_accounting.repositories.AgentReportsRepository;
 import com.trade_accounting.utils.DtoMapperImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @ExtendWith(MockitoExtension.class)
-public class AgentReportsServiceImplTest {
+class AgentReportsServiceImplTest {
 
     @InjectMocks
     private AgentReportsServiceImpl service;
@@ -20,5 +23,10 @@ public class AgentReportsServiceImplTest {
 
     @Spy
     private DtoMapperImpl dtoMapper;
+
+    @Test
+    void repositoryIsNotNullTest() {
+        assertNotNull(repository, "Repository is not filled");
+    }
 
 }
