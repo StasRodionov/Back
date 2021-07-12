04edaa8803ca50c187fc3861a3ae7fcd2f3193d4
Dmitry Evstratov <evstratovdmitry1992@gmail.com>
2021-07-12 14:45:25 +0300

    integration tests for agentReports is started

---
+++ src/test/resources/agentReports-before.sql
@@ -0,0 +1,6 @@
+DELETE
+FROM agent_reports;
+INSERT INTO agent_reports(id, comitent_sum, commentary, document_type, number, paid, printed, remuniration_sum, sent,
+                          status, sum, time, company_id, contractor_id)
+VALUES (),
+       ();
