533630bb860d310df652feaeeab362f26c4e4ca5
Pavel Andrusov <pavel.andrusov.89@gmail.com>
2021-07-13 17:52:45 +0300

    task done

---
+++ src/main/java/com/trade_accounting/models/InternalOrder.java
@@ -0,0 +1,56 @@
+package com.trade_accounting.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Pavel Andrusov
+ * @version 1.0.0
+ * */
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "internal_order")
+public class InternalOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<InternalOrderProducts> internalOrderProducts;
+
+    @NotNull
+    private LocalDateTime date;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    @ColumnDefault("false")
+    private Boolean isSent = null;
+
+    @ColumnDefault("false")
+    private Boolean isPrint = null;
+
+    private String comment;
+}
