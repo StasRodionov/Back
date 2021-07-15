42ed04854fcdc86f2d1b197e4f899559165c9ec7
Alexey <slag88@mail.ru>
2021-07-15 21:06:16 +0300

    currency_rest_controller_test

--- src/test/resources/Currency-before.sql
+++ src/test/resources/Currency-before.sql
@@ -1,21 +1,9 @@
--- DELETE FROM inventarizations_inventarization_products;
 DELETE FROM currency;
 
 INSERT INTO currency (id, digital_code, full_name, letter_code, short_name, sort_number)
 VALUES (1, '25', 'Russian Rubles', 'rub', 'rubles', '1'),
        (2, '25', 'Bellarusian Rubles', 'belrub', 'bel rubles', '2'),
        (3, '25', 'USA Dollars', 'dol', 'eng dollar', '3');
-
--- INSERT INTO inventarizations_inventarization_products (inventarization_id, inventarization_products_id)
--- VALUES (1, 1),
---        (1, 2),
---        (1, 3),
---        (2, 4),
---        (2, 5),
---        (2, 6),
---        (3, 7),
---        (3, 8),
---        (3, 9);
 
 SELECT setval('currency_id_seq', max(id))
 FROM inventarizations;
