1adf5344e74b9b4789be6e954f1ef3be6f447b15
mr-nicko89 <mr-nicko89@yandex.ru>
2021-07-16 16:39:41 +0300

    fix MovementRestControllerTest

--- src/test/resources/Movement-before.sql
+++ src/test/resources/Movement-before.sql
@@ -2,7 +2,7 @@ DELETE FROM movement_movement_products;
 DELETE FROM movement;
 
 INSERT INTO movement (id, comment, date, is_print, is_sent, company_id, warehouse_from_id,warehouse_to_id)
-VALUES (1, 'Перемещение 1', '2021-07-16 15:10', false, false, 1, 1, 1),
+VALUES (1, 'Перемещение 1', '2021-07-16 15:10', false, false, 1, 1, 2),
        (2, 'Перемещение 2', '2021-07-16 15:10', false, false, 1, 2, 1),
        (3, 'Перемещение 3', '2021-07-16 15:10', false, false, 1, 1, 2);
 
