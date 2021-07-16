64ede2bd4e9578d07bd2ebe5f9704f205435bda5
Pavel Andrusov <pavel.andrusov.89@gmail.com>
2021-07-16 17:48:23 +0300

    в процессе

---
+++ src/main/java/com/trade_accounting/utils/mappers/InternalOrderMapper.java
@@ -0,0 +1,54 @@
+package com.trade_accounting.utils.mappers;
+
+import com.trade_accounting.models.InternalOrder;
+import com.trade_accounting.models.InternalOrderProduct;
+import com.trade_accounting.models.dto.InternalOrderDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
+
+/**
+ * @author Pavel Andrusov
+ */
+
+@Mapper(componentModel = "spring")
+public interface InternalOrderMapper {
+    /**
+     * @return InternalOrder
+     */
+    @Mappings({
+            @Mapping(source = "companyId", target = "company.id")
+    })
+    InternalOrder toModel(InternalOrderDto internalOrderDto);
+
+    /**
+     * @return InternalOrderDto
+     */
+    default InternalOrderDto toDto(InternalOrder internalOrder) {
+        InternalOrderDto internalOrderDto = new InternalOrderDto();
+        if (internalOrder == null) {
+            return null;
+        } else {
+            internalOrderDto.setId(internalOrder.getId());
+            internalOrderDto.setDate(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(internalOrder.getDate()));
+            internalOrderDto.setIsSent(internalOrder.getIsSent());
+            internalOrderDto.setIsPrint(internalOrder.getIsPrint());
+            internalOrderDto.setComment(internalOrder.getComment());
+            internalOrderDto.setInternalOrderProductsIds(
+                    internalOrder.getInternalOrderProducts().stream()
+                            .map(InternalOrderProduct::getId)
+                            .collect(Collectors.toList())
+            );
+
+            if (internalOrder.getCompany() == null) {
+                return null;
+            } else {
+                internalOrderDto.setCompanyId(internalOrder.getCompany().getId());
+                return internalOrderDto;
+            }
+        }
+    }
+}
