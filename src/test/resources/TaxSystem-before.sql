836f51135f3394ca4c48564942621fe070ed73ce
Alexey <slag88@mail.ru>
2021-07-18 17:05:00 +0300

    currency_rest_controller_test

---
+++ src/test/resources/TaxSystem-before.sql
@@ -0,0 +1,10 @@
+TRUNCATE products CASCADE;
+DELETE FROM tax_systems;
+
+INSERT INTO tax_systems (id, name, sort_number)
+VALUES (1, 'ОСН', '1'),
+       (2, 'УСН.Доход', '2'),
+       (3, 'УСН.Доход-Расход', '3');
+
+SELECT setval('tax_systems_id_seq', max(id))
+FROM tax_systems;
