31ecb7e0cf9d5cf19ba9a0b6f8f64ccbb5fd2dae
Nickolay <mr-nicko89@yandex.ru>
2021-07-18 16:14:03 +0300

    edit MovementProductDtoStubs

--- src/test/java/com/trade_accounting/services/impl/Stubs/dto/MovementProductDtoStubs.java
+++ src/test/java/com/trade_accounting/services/impl/Stubs/dto/MovementProductDtoStubs.java
@@ -1,10 +1,15 @@
-//package com.trade_accounting.services.impl.Stubs.dto;
-//
-//import com.trade_accounting.models.dto.MovementProductDto;
-//
-//public class MovementProductDtoStubs {
-//
-//        public static MovementProductDto getMovementProductDto(Long id) {
-//        return dtoMapper.toMovementProductDto(ModelStubs.getMovementProduct(id));
-//    }
-//}
+package com.trade_accounting.services.impl.Stubs.dto;
+
+import com.trade_accounting.models.dto.MovementProductDto;
+import com.trade_accounting.services.impl.Stubs.model.MovementProductModelStubs;
+import com.trade_accounting.utils.mapper.MovementProductMapper;
+import org.mapstruct.factory.Mappers;
+
+public class MovementProductDtoStubs {
+
+    private static final MovementProductMapper mapper = Mappers.getMapper(MovementProductMapper.class);
+
+    public static MovementProductDto getDto(Long id) {
+        return mapper.toMovementProductDto(MovementProductModelStubs.getMovementProduct(id));
+    }
+}
