786b10ed9673258ba3f00022868ca4d09175e03c
Pavel Andrusov <pavel.andrusov.89@gmail.com>
2021-07-19 13:49:00 +0300

    Create InternalOrderProduct-before.sql

--- src/test/java/com/trade_accounting/controllers/rest/InternalOrderProductRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/InternalOrderProductRestControllerTest.java
@@ -1,13 +1,47 @@
 package com.trade_accounting.controllers.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.yml"})
+@Sql(value = "/InternalOrderProduct-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@WithUserDetails(value = "karimogon@mail.ru")
 class InternalOrderProductRestControllerTest {
+    @Autowired
+    private InternalOrderProductRestController internalOrderProductRestController;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void getAll() {
+    void testExistence() {
+        assertNotNull(internalOrderProductRestController, "Internal Order Rest Controller is null");
+    }
+
+    @Test
+    void getAll() throws Exception {
+        mockMvc.perform(get("/api/internalorder/product"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(authenticated())
+                .andExpect(jsonPath("$", hasSize(5)));
     }
 
     @Test
