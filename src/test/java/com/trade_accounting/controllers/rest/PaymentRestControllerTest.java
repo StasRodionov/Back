26ae471a1ec90e6ea5125c14c4472acc3d8539fc
Damir333 <damirmv1993@gmail.com>
2021-07-30 13:20:31 +0300

    Создал новый файл PaymentRestControllerTest

---
+++ src/test/java/com/trade_accounting/controllers/rest/PaymentRestControllerTest.java
@@ -0,0 +1,36 @@
+package com.trade_accounting.controllers.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentRestControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void filterAll() {
+    }
+
+    @Test
+    void search() {
+    }
+}
