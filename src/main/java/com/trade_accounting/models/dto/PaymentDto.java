afe076c8df84dcb1fe1908ba7ca7d7650b6ec331
Damir333 <damirmv1993@gmail.com>
2021-07-30 13:55:57 +0300

    commit 3

--- src/main/java/com/trade_accounting/models/dto/PaymentDto.java
+++ src/main/java/com/trade_accounting/models/dto/PaymentDto.java
@@ -1,17 +1,11 @@
 package com.trade_accounting.models.dto;
 
-import com.trade_accounting.models.Company;
-import com.trade_accounting.models.Contract;
-import com.trade_accounting.models.Contractor;
 import com.trade_accounting.models.PaymentMethods;
-import com.trade_accounting.models.Project;
 import com.trade_accounting.models.TypeOfPayment;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
--- src/test/java/com/trade_accounting/controllers/rest/PaymentRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/PaymentRestControllerTest.java
@@ -1,10 +1,34 @@
 package com.trade_accounting.controllers.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.yml"})
+@Sql(value = "/Payment-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@WithUserDetails(value = "karimogon@mail.ru")
+@RequiredArgsConstructor
 class PaymentRestControllerTest {
+    @Autowired
+    private PaymentRestController paymentRestController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testExistence() {
+        assertNotNull(paymentRestController, "Payment Rest Controller is null");
+    }
 
     @Test
     void getAll() {
