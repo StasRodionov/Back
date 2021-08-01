79ea07580e186bb416c65c3dfd4f60a19ba1330d
Damir333 <damirmv1993@gmail.com>
2021-08-01 14:21:22 +0300

    Task begin

---
+++ src/test/java/com/trade_accounting/controllers/rest/RoleRestControllerTest.java
@@ -0,0 +1,28 @@
+package com.trade_accounting.controllers.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleRestControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
