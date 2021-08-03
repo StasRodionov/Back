393b9820ee11c8315145efbcc0d9ec669e126c00
Damir333 <damirmv1993@gmail.com>
2021-08-03 21:17:01 +0300

    Доработал поле List<Long> contactIds

--- src/main/java/com/trade_accounting/services/interfaces/ContactService.java
+++ src/main/java/com/trade_accounting/services/interfaces/ContactService.java
@@ -2,6 +2,9 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.dto.ContactDto;
 
-public interface ContactService extends AbstractService<ContactDto> {
+import java.util.List;
 
+public interface ContactService extends AbstractService<ContactDto> {
+    @Override
+    List<ContactDto> getAll();
 }
