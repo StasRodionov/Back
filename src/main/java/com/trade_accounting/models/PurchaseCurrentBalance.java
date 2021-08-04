823da3a9b87f49565b6ba422baf3e66457a3deaa
EvgenyMaster <evgenymaster6162@gmail.com>
2021-08-04 07:58:50 +0300

    delete useless annotations, add javadoc @param

--- src/main/java/com/trade_accounting/models/PurchaseCurrentBalance.java
+++ src/main/java/com/trade_accounting/models/PurchaseCurrentBalance.java
@@ -3,44 +3,42 @@ package com.trade_accounting.models;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+/**
+ * Класс-модель текущий остаток в закупках
+ *
+ * @param id                        - номер остатка товара
+ * @param restOfTheWarehouse        - остаток на складе
+ * @param productsReserve           - резерв товара
+ * @param productsAwaiting          - товара в ожидании
+ * @param productsAvailableForOrder - товара доступно для заказа
+ * @param daysStoreOnTheWarehouse   - дни хранения на складе
+ */
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-@Getter
-@Setter
-@ToString
 @Table(name = "purchase_current_balance")
 public class PurchaseCurrentBalance {
     @Id
     @NotNull
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "rest")
     private Long restOfTheWarehouse;
 
-    @Column(name = "reserve")
     private Long productsReserve;
 
-    @Column(name = "await")
-    private Long productsAwaitingOrder;
+    private Long productsAwaiting;
 
-    @Column(name = "available")
     private Long productsAvailableForOrder;
 
-    @Column(name = "days_store")
     private Long daysStoreOnTheWarehouse;
 }
