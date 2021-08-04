10c7bfb4ac609f6d19c4f7797f0536dd05d4da4c
Damir333 <damirmv1993@gmail.com>
2021-08-04 09:42:28 +0300

    task done

--- src/main/java/com/trade_accounting/services/interfaces/ContactService.java
+++ src/main/java/com/trade_accounting/services/interfaces/ContactService.java
@@ -5,6 +5,4 @@ import com.trade_accounting.models.dto.ContactDto;
 import java.util.List;
 
 public interface ContactService extends AbstractService<ContactDto> {
-    @Override
-    List<ContactDto> getAll();
 }
