51326406c247fa1cb2722270ccb42cb04c36b846
Damir333 <damirmv1993@gmail.com>
2021-08-04 09:43:45 +0300

    task done final

--- src/main/java/com/trade_accounting/services/interfaces/ContactService.java
+++ src/main/java/com/trade_accounting/services/interfaces/ContactService.java
@@ -2,7 +2,5 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.dto.ContactDto;
 
-import java.util.List;
-
 public interface ContactService extends AbstractService<ContactDto> {
 }
