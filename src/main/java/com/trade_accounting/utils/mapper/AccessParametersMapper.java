f055483b7f47eb5647e76cfa372e33100cfa3f81
Damir333 <damirmv1993@gmail.com>
2021-08-05 09:38:54 +0300

    2

--- src/main/java/com/trade_accounting/utils/mapper/AccessParametersMapper.java
+++ src/main/java/com/trade_accounting/utils/mapper/AccessParametersMapper.java
@@ -8,8 +8,32 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AccessParametersMapper {
-
     //AccessParameters
+//    default AccessParameters toModel(AccessParametersDto accessParametersDto) {
+//        if (accessParametersDto == null) {
+//            return null;
+//        }
+//
+//        return AccessParameters.builder()
+//                .id(accessParametersDto.getId())
+//                .generalAccess(accessParametersDto.getGeneralAccess())
+//                .build();
+//    }
+//
+//    default AccessParametersDto toDto(AccessParameters accessParameters) {
+//        AccessParametersDto accessParametersDto = new AccessParametersDto();
+//        if (accessParameters == null || accessParameters.getEmployee().getId() == null
+//                || accessParameters.getDepartment().getId() == null) {
+//            return null;
+//        } else {
+//            accessParametersDto.setId(accessParameters.getId());
+//            accessParametersDto.setGeneralAccess(accessParameters.getGeneralAccess());
+//            accessParametersDto.setEmployeeId(accessParameters.getEmployee().getId());
+//            accessParametersDto.setDepartmentId(accessParameters.getDepartment().getId());
+//            return accessParametersDto;
+//        }
+//    }
+
     default AccessParametersDto toDto(AccessParameters accessParameters) {
         if (accessParameters == null) {
             return null;
