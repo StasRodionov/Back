9724ea1daf9e30ec0b3647b1fd12b94dff557a39
Andrey Melnikov <a.melnikov1310@ya.ru>
2021-08-06 15:25:27 +0300

    пауза на на тесте priceListRestController метод getById

--- src/test/java/com/trade_accounting/controllers/rest/PriceListRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/PriceListRestControllerTest.java
@@ -1,18 +1,26 @@
 package com.trade_accounting.controllers.rest;
 
+import com.trade_accounting.models.dto.PriceListDto;
 import lombok.RequiredArgsConstructor;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
 
 /**
  * @author Andrey Melnikov
+ * @since 05.08.2021
  */
 
 @SpringBootTest
@@ -30,9 +38,28 @@ public class PriceListRestControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void testExisting(){
+    void testExisting() {
         Assertions.assertNotNull(controller, "Failure - controller is null");
     }
 
+    @Test
+    void getAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/priceList"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(SecurityMockMvcResultMatchers.authenticated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)));
+    }
+
+    void getById(){
+        PriceListDto priceListDto = PriceListDto.builder()
+                .id(1L)
+                .number("number1")
+                .time(LocalDateTime.now()) // да твоюж мать ... о5 ДТО не доделан ...(((
+                .companyId(1L)
+                .sent(0L)
+                .printed(1L)
+                .commentary("comment1")
+                .build();
+    }
 
 }
--- src/test/resources/PriceList-before.sql
+++ src/test/resources/PriceList-before.sql
@@ -2,9 +2,9 @@ TRUNCATE price_lists CASCADE;
 
 INSERT INTO price_lists (id, commentary, number, printed, sent, time, company_id)
 VALUES
-    (1, 'comment1', 'number1', 1, 1, '1234-12-12 12:34', 1),
-    (2, 'comment2', 'number2', 2, 2, '1234-12-12 12:34', 2),
-    (3, 'comment3', 'number3', 3, 3, '1234-12-12 12:34', 3);
+    (1, 'comment1', 'number1', 1, 0, '1234-12-12 12:34', 1),
+    (2, 'comment2', 'number2', 0, 1, '1234-12-12 12:34', 2),
+    (3, 'comment3', 'number3', 1, 0, '1234-12-12 12:34', 3);
 
-SELECT setval('price_list_id_seq', max(id))
+SELECT setval('price_lists_id_seq', max(id))
 FROM price_lists;
