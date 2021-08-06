125af021c706893716c3c6d5baf4b1c710c1de09
Damir333 <damirmv1993@gmail.com>
2021-08-06 09:56:58 +0300

    task begin

---
+++ src/test/java/com/trade_accounting/services/impl/RequestsProductionsServiceImplTest.java
@@ -0,0 +1,32 @@
+package com.trade_accounting.services.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestsProductionsServiceImplTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void saveOrUpdate() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
