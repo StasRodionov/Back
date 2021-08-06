fe6f87c29e1a46f8bcbc99d04dc87ece51269807
Damir333 <damirmv1993@gmail.com>
2021-08-06 15:09:45 +0300

    Task begin

---
+++ src/test/java/com/trade_accounting/controllers/rest/ContractRestControllerTest.java
@@ -0,0 +1,36 @@
+package com.trade_accounting.controllers.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractRestControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void testGetAll() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
