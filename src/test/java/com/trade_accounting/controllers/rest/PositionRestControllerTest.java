025323b8ba37875a24d9e90eca6196535d6490cd
Stanislav Dusiak <stanislav.dusiak@gmail.com>
2021-08-06 09:36:19 +0300

    test1

--- src/test/java/com/trade_accounting/controllers/rest/PositionRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/PositionRestControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,7 +30,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.yml"})
 @Sql(value = "/Position-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @WithMockUser
-//@WithUserDetails(value = "veraogon@mail.ru")
 @RequiredArgsConstructor
 class PositionRestControllerTest {
 
