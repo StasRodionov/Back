9542afbcad7bd5c95df45edacf448f6ad6c8e9e7
KlipovMV <mk2295@ya.ru>
2021-08-06 20:52:53 +0300

    Updated TechnicalCardInit and TechnicalCardRestControllerTest

--- src/main/java/com/trade_accounting/config/init/TechnicalCardInit.java
+++ src/main/java/com/trade_accounting/config/init/TechnicalCardInit.java
@@ -1,76 +1,64 @@
-//package com.trade_accounting.config.init;
-//
-//import com.trade_accounting.models.TechnicalCardGroup;
-//import com.trade_accounting.models.TechnicalCardProduction;
-//import com.trade_accounting.models.dto.TechnicalCardDto;
-//import com.trade_accounting.models.dto.TechnicalCardGroupDto;
-//import com.trade_accounting.models.dto.TechnicalCardProductionDto;
-//import com.trade_accounting.repositories.TechnicalCardProductionRepository;
-//import com.trade_accounting.services.interfaces.TechnicalCardGroupService;
-//import com.trade_accounting.services.interfaces.TechnicalCardService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class TechnicalCardInit extends InitData {
-//
-//    private final TechnicalCardService technicalCardService;
-//    private final TechnicalCardGroupService technicalCardGroupService;
-//    private final TechnicalCardProduction;
-//
-//    @Override
-//    void init() {
-//        initTechnicalCards();
-//    }
-//
-//    public void initTechnicalCards() {
-//        List<TechnicalCardGroupDto> technicalCardGroups = technicalCardGroupService.getAll()
-//                .stream().limit(3).collect(Collectors.toList());
-//        List<TechnicalCardProduction> finalProduct = technicalCardProductionRepository.findAll()
-//                .stream().limit(3).collect(Collectors.toList());
-//        List<TechnicalCardProduction> materials =
-//        technicalCardService.create(new TechnicalCardDto(
-//                null,
-//                "Техническая карта №1",
-//                "Комментарий1",
-//                "1000",
-//                technicalCardGroupService.getById(1L),
-//                ,
-//                List.of(new TechnicalCardProductionDto(null, 2L, 3L),
-//                        new TechnicalCardProductionDto(null, 2L, 4L))));
-//        technicalCardService.create(new TechnicalCardDto(
-//                null,
-//                "Техническая карта №2",
-//                "Комментарий2",
-//                "1100",
-//                technicalCardGroupService.getById(1L),
-//                List.of(new TechnicalCardProductionDto(null, 2L, 5L),
-//                        new TechnicalCardProductionDto(null, 2L, 6L)),
-//                List.of(new TechnicalCardProductionDto(null, 2L, 7L),
-//                        new TechnicalCardProductionDto(null, 2L, 8L))));
-//        technicalCardService.create(new TechnicalCardDto(
-//                null,
-//                "Техническая карта №3",
-//                "Комментарий3",
-//                "1200",
-//                technicalCardGroupService.getById(2L),
-//                List.of(new TechnicalCardProductionDto(null, 2L, 9L),
-//                        new TechnicalCardProductionDto(null, 2L, 10L)),
-//                List.of(new TechnicalCardProductionDto(null, 2L, 11L),
-//                        new TechnicalCardProductionDto(null, 2L, 12L))));
-//        technicalCardService.create(new TechnicalCardDto(
-//                null,
-//                "Техническая карта №4",
-//                "Комментарий4",
-//                "1300",
-//                technicalCardGroupService.getById(2L),
-//                List.of(new TechnicalCardProductionDto(null, 2L, 13L),
-//                        new TechnicalCardProductionDto(null, 2L, 14L)),
-//                List.of(new TechnicalCardProductionDto(null, 2L, 15L),
-//                        new TechnicalCardProductionDto(null, 2L, 16L))));
-//    }
-//}
+package com.trade_accounting.config.init;
+
+import com.trade_accounting.models.TechnicalCardGroup;
+import com.trade_accounting.models.TechnicalCardProduction;
+import com.trade_accounting.models.dto.TechnicalCardDto;
+import com.trade_accounting.models.dto.TechnicalCardGroupDto;
+import com.trade_accounting.models.dto.TechnicalCardProductionDto;
+import com.trade_accounting.repositories.TechnicalCardProductionRepository;
+import com.trade_accounting.services.interfaces.TechnicalCardGroupService;
+import com.trade_accounting.services.interfaces.TechnicalCardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class TechnicalCardInit extends InitData {
+
+    private final TechnicalCardService technicalCardService;
+    private final TechnicalCardGroupService technicalCardGroupService;
+    private final TechnicalCardProductionRepository technicalCardProductionRepository;
+
+    @Override
+    void init() {
+        initTechnicalCards();
+    }
+
+    public void initTechnicalCards() {
+        List<TechnicalCardGroupDto> technicalCardGroups = technicalCardGroupService.getAll()
+                .stream().limit(3).collect(Collectors.toList());
+        List<TechnicalCardProduction> finalProducts = technicalCardProductionRepository.findAll()
+                .stream().limit(3).collect(Collectors.toList());
+        List<Long> listFinalProducts = new ArrayList<>();
+        for (TechnicalCardProduction finalProduct : finalProducts) {
+            listFinalProducts.add(finalProduct.getId());
+        }
+        List<TechnicalCardProduction> materials = technicalCardProductionRepository.findAll()
+                .stream().limit(3).collect(Collectors.toList());
+        List<Long> listMaterials = new ArrayList<>();
+        for (TechnicalCardProduction material : materials) {
+            listMaterials.add(material.getId());
+        }
+        int count = 1;
+        int count2 = 100;
+        for (TechnicalCardGroupDto technicalCardGroupDto : technicalCardGroups) {
+            technicalCardService.create(
+                    TechnicalCardDto.builder()
+                            .id(null)
+                            .name("Техническая карта №" + count)
+                            .comment("Комментарий" + count)
+                            .productionCost("1000")
+                            .technicalCardGroupId(technicalCardGroupDto.getId())
+                            .finalProductionId(listFinalProducts)
+                            .materialsId(listMaterials)
+                            .build()
+            );
+            count++;
+            count2++;
+        }
+    }
+}
