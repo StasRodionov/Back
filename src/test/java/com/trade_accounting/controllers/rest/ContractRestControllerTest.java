a0e51fc432b1403240859fd185c773e9cc2e09ed
Damir333 <damirmv1993@gmail.com>
2021-08-06 17:11:09 +0300

    1

--- src/test/java/com/trade_accounting/controllers/rest/ContractRestControllerTest.java
+++ src/test/java/com/trade_accounting/controllers/rest/ContractRestControllerTest.java
@@ -1,9 +1,21 @@
 package com.trade_accounting.controllers.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.yml"})
+@Sql(value = "/InternalOrder-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@WithUserDetails(value = "karimogon@mail.ru")
+@RequiredArgsConstructor
 class ContractRestControllerTest {
 
     @Test
