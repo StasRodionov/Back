4e9a656b24e05ab1c63565a398d88c760c2a8bd7
Damir333 <damirmv1993@gmail.com>
2021-08-06 10:42:51 +0300

    1

--- src/main/java/com/trade_accounting/repositories/RequestsProductionsRepository.java
+++ src/main/java/com/trade_accounting/repositories/RequestsProductionsRepository.java
@@ -2,8 +2,10 @@ package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.RequestsProductions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RequestsProductionsRepository extends JpaRepository<RequestsProductions,Long> {
+public interface RequestsProductionsRepository extends JpaRepository<RequestsProductions,Long>,
+        JpaSpecificationExecutor<RequestsProductions> {
 }
--- src/test/java/com/trade_accounting/services/impl/RequestsProductionsServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/RequestsProductionsServiceImplTest.java
@@ -1,13 +1,51 @@
 package com.trade_accounting.services.impl;
 
+import com.trade_accounting.models.dto.RequestsProductionsDto;
+import com.trade_accounting.repositories.RequestsProductionsRepository;
+import com.trade_accounting.repositories.TechnicalCardRepository;
+import com.trade_accounting.repositories.WarehouseRepository;
+import com.trade_accounting.services.impl.Stubs.model.RequestsProductionsModelStubs;
+import com.trade_accounting.utils.mapper.RequestsProductionsMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 class RequestsProductionsServiceImplTest {
 
+    @Mock
+    TechnicalCardRepository technicalCardRepository;
+
+    @Mock
+    WarehouseRepository warehouseRepository;
+
+    @Mock
+    RequestsProductionsRepository requestsProductionsRepository;
+
+    @Spy
+    RequestsProductionsMapper requestsProductionsMapper;
+
+    @InjectMocks
+    RequestsProductionsServiceImpl requestsProductionsService;
+
+
     @Test
     void getAll() {
+        when(requestsProductionsRepository.findAll()).thenReturn(
+                        List.of(
+                                RequestsProductionsModelStubs.getRequestProductions(1L),
+                                RequestsProductionsModelStubs.getRequestProductions(2L),
+                                RequestsProductionsModelStubs.getRequestProductions(3L)
+                        ));
+
+        List<RequestsProductionsDto> requestsProductionsDtos = requestsProductionsService.getAll();
+
+        assertEquals(3, requestsProductionsDtos.size());
     }
 
     @Test
