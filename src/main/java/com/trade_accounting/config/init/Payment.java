2c3a564907a12e9b8747e214ad92438665868ee6
Damir333 <damirmv1993@gmail.com>
2021-08-09 09:54:06 +0300

    1

--- src/main/java/com/trade_accounting/config/init/Payment.java
+++ src/main/java/com/trade_accounting/config/init/Payment.java
@@ -1,84 +1,84 @@
-package com.trade_accounting.config.init;
-
-import com.trade_accounting.models.PaymentMethods;
-import com.trade_accounting.models.TypeOfPayment;
-import com.trade_accounting.models.dto.CompanyDto;
-import com.trade_accounting.models.dto.ContractDto;
-import com.trade_accounting.models.dto.ContractorDto;
-import com.trade_accounting.models.dto.PaymentDto;
-import com.trade_accounting.models.dto.ProjectDto;
-import com.trade_accounting.services.interfaces.CompanyService;
-import com.trade_accounting.services.interfaces.ContractService;
-import com.trade_accounting.services.interfaces.ContractorService;
-import com.trade_accounting.services.interfaces.PaymentService;
-import com.trade_accounting.services.interfaces.ProjectService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Component
-@RequiredArgsConstructor
-public class Payment extends InitData {
-    private final PaymentService paymentService;
-    private final CompanyService companyService;
-    private final ContractorService contractorService;
-    private final ProjectService projectService;
-    private final ContractService contractService;
-
-    @Override
-    void init() {
-        initPayment();
-    }
-
-    public void initPayment() {
-        List<CompanyDto> companyDtos = companyService.getAll().stream().limit(3).collect(Collectors.toList());
-        List<ContractorDto> contractorDtos = contractorService.getAll().stream().limit(3).collect(Collectors.toList());
-        List<ProjectDto> projectDtos = projectService.getAll().stream().limit(3).collect(Collectors.toList());
-        List<ContractDto> contractDtos = contractService.getAll().stream().limit(3).collect(Collectors.toList());
-        int count = 1;
-        for (CompanyDto companyDto : companyDtos) {
-            for (ContractorDto contractorDto : contractorDtos) {
-                for (ContractDto contractDto : contractDtos) {
-                    for (ProjectDto projectDto : projectDtos) {
-                        paymentService.create(
-                                PaymentDto.builder()
-                                        .id(null)
-                                        .number("0000" + count)
-                                        .sum(BigDecimal.valueOf(100))
-                                        .paymentMethods("CASH")
-                                        .typeOfPayment("INCOMING")
-                                        .time(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                                        .companyId(companyDto.getId())
-                                        .contractorId(contractorDto.getId())
-                                        .contractId(contractDto.getId())
-                                        .projectId(projectDto.getId())
-                                        .build()
-                        );
-                        count++;
-
-                        paymentService.create(PaymentDto.builder()
-                                .id(null)
-                                .number("0000" + count)
-                                .sum(BigDecimal.valueOf(50))
-                                .paymentMethods("BANK")
-                                .typeOfPayment("OUTGOING")
-                                .time(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                                .companyId(companyDto.getId())
-                                .contractorId(contractorDto.getId())
-                                .contractId(contractDto.getId())
-                                .projectId(projectDto.getId())
-                                .build()
-                        );
-                        count++;
-                    }
-                }
-            }
-        }
-    }
-
-}
+//package com.trade_accounting.config.init;
+//
+//import com.trade_accounting.models.PaymentMethods;
+//import com.trade_accounting.models.TypeOfPayment;
+//import com.trade_accounting.models.dto.CompanyDto;
+//import com.trade_accounting.models.dto.ContractDto;
+//import com.trade_accounting.models.dto.ContractorDto;
+//import com.trade_accounting.models.dto.PaymentDto;
+//import com.trade_accounting.models.dto.ProjectDto;
+//import com.trade_accounting.services.interfaces.CompanyService;
+//import com.trade_accounting.services.interfaces.ContractService;
+//import com.trade_accounting.services.interfaces.ContractorService;
+//import com.trade_accounting.services.interfaces.PaymentService;
+//import com.trade_accounting.services.interfaces.ProjectService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//import java.math.BigDecimal;
+//import java.time.LocalDateTime;
+//import java.time.format.DateTimeFormatter;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@Component
+//@RequiredArgsConstructor
+//public class Payment extends InitData {
+//    private final PaymentService paymentService;
+//    private final CompanyService companyService;
+//    private final ContractorService contractorService;
+//    private final ProjectService projectService;
+//    private final ContractService contractService;
+//
+//    @Override
+//    void init() {
+//        initPayment();
+//    }
+//
+//    public void initPayment() {
+//        List<CompanyDto> companyDtos = companyService.getAll().stream().limit(3).collect(Collectors.toList());
+//        List<ContractorDto> contractorDtos = contractorService.getAll().stream().limit(3).collect(Collectors.toList());
+//        List<ProjectDto> projectDtos = projectService.getAll().stream().limit(3).collect(Collectors.toList());
+//        List<ContractDto> contractDtos = contractService.getAll().stream().limit(3).collect(Collectors.toList());
+//        int count = 1;
+//        for (CompanyDto companyDto : companyDtos) {
+//            for (ContractorDto contractorDto : contractorDtos) {
+//                for (ContractDto contractDto : contractDtos) {
+//                    for (ProjectDto projectDto : projectDtos) {
+//                        paymentService.create(
+//                                PaymentDto.builder()
+//                                        .id(null)
+//                                        .number("0000" + count)
+//                                        .sum(BigDecimal.valueOf(100))
+//                                        .paymentMethods("CASH")
+//                                        .typeOfPayment("INCOMING")
+//                                        .time(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+//                                        .companyId(companyDto.getId())
+//                                        .contractorId(contractorDto.getId())
+//                                        .contractId(contractDto.getId())
+//                                        .projectId(projectDto.getId())
+//                                        .build()
+//                        );
+//                        count++;
+//
+//                        paymentService.create(PaymentDto.builder()
+//                                .id(null)
+//                                .number("0000" + count)
+//                                .sum(BigDecimal.valueOf(50))
+//                                .paymentMethods("BANK")
+//                                .typeOfPayment("OUTGOING")
+//                                .time(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+//                                .companyId(companyDto.getId())
+//                                .contractorId(contractorDto.getId())
+//                                .contractId(contractDto.getId())
+//                                .projectId(projectDto.getId())
+//                                .build()
+//                        );
+//                        count++;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
+//}
--- src/main/java/com/trade_accounting/config/init/PriceListInit.java
+++ src/main/java/com/trade_accounting/config/init/PriceListInit.java
@@ -1,38 +1,38 @@
-package com.trade_accounting.config.init;
-
-
-import com.trade_accounting.models.dto.PriceListDto;
-import com.trade_accounting.services.interfaces.PriceListService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-
-/**
- * @author by Stanislav Dusyak and Pavel Andrusov
- */
-
-@Component
-@RequiredArgsConstructor
-public class PriceListInit extends InitData {
-    private final PriceListService priceListService;
-
-    @Override
-    void init() {
-        initPriceList();
-    }
-
-    private void initPriceList() {
-        for (long i = 1L; i < 5; i++) {
-            priceListService.create(PriceListDto.builder()
-                    .number("0001" + i)
-                    .commentary("Тестовый комментарий" + " " + i)
-                    .time(LocalDateTime.now())
-                    .companyId(i)
-                    .sent(0L)
-                    .printed(1L)
-                    .build()
-            );
-        }
-    }
-}
+//package com.trade_accounting.config.init;
+//
+//
+//import com.trade_accounting.models.dto.PriceListDto;
+//import com.trade_accounting.services.interfaces.PriceListService;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//
+///**
+// * @author by Stanislav Dusyak and Pavel Andrusov
+// */
+//
+//@Component
+//@RequiredArgsConstructor
+//public class PriceListInit extends InitData {
+//    private final PriceListService priceListService;
+//
+//    @Override
+//    void init() {
+//        initPriceList();
+//    }
+//
+//    private void initPriceList() {
+//        for (long i = 1L; i < 5; i++) {
+//            priceListService.create(PriceListDto.builder()
+//                    .number("0001" + i)
+//                    .commentary("Тестовый комментарий" + " " + i)
+//                    .time(LocalDateTime.now())
+//                    .companyId(i)
+//                    .sent(0L)
+//                    .printed(1L)
+//                    .build()
+//            );
+//        }
+//    }
+//}
