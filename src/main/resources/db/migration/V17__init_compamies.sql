9f54ad97ec5cb2a421d689ca95dbfafb8f569ea2
Damir333 <damirmv1993@gmail.com>
2021-08-09 18:11:13 +0300

    7

--- src/main/resources/db/migration/V17__init_compamies.sql
+++ src/main/resources/db/migration/V17__init_compamies.sql
@@ -1,10 +1,10 @@
-INSERT INTO companies(id, name, inn, sort_number, phone, fax, email, payer_vat, comment_to_address, leader,
-                      leader_manager_position, leader_signature, chief_accountant, chief_accountant_signature,
-                      stamp)
-VALUES (1, 'OOO "Организация №1"', '1234', '0001', '+79436527610', '810-41-1234567823', 'veraogon@mail.ru', true,
-        'something comment', 'testLeader', 'testLeaderMeneger', 'testLeaderSignature', 'chiefTest',
-        'chiefTestAccount', 'stampTest'),
-       (2, 'OOO "Организация №2"', '4321', '0002', '+76572518965', '810-41-1234567824', 'karimogon@mail.ru', true,
-        'comment', 'Leader', 'testLeaderMeneger', 'LeaderSignature', 'chief', 'chiefAccount', 'stamp'),
-       (2, 'OOO "Организация №3"', '4321', '0002', '+76572518965', '810-41-1234567824', 'karimogon@mail.ru', true,
-        'comment', 'Leader', 'testLeaderMeneger', 'LeaderSignature', 'chief', 'chiefAccount', 'stamp');
+-- INSERT INTO companies(id, name, inn, sort_number, phone, fax, email, payer_vat, comment_to_address, leader,
+--                       leader_manager_position, leader_signature, chief_accountant, chief_accountant_signature,
+--                       stamp)
+-- VALUES (1, 'OOO "Организация №1"', '1234', '0001', '+79436527610', '810-41-1234567823', 'veraogon@mail.ru', true,
+--         'something comment', 'testLeader', 'testLeaderMeneger', 'testLeaderSignature', 'chiefTest',
+--         'chiefTestAccount', 'stampTest'),
+--        (2, 'OOO "Организация №2"', '4321', '0002', '+76572518965', '810-41-1234567824', 'karimogon@mail.ru', true,
+--         'comment', 'Leader', 'testLeaderMeneger', 'LeaderSignature', 'chief', 'chiefAccount', 'stamp'),
+--        (2, 'OOO "Организация №3"', '4321', '0002', '+76572518965', '810-41-1234567824', 'karimogon@mail.ru', true,
+--         'comment', 'Leader', 'testLeaderMeneger', 'LeaderSignature', 'chief', 'chiefAccount', 'stamp');
