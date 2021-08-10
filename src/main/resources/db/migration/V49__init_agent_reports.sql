9ac0611920a53256b14b974efad41c6d93857336
klyanskis <mklyanskis@gmail.com>
2021-08-10 12:35:24 +0700

    working_version_1.1?

---
+++ src/main/resources/db/migration/V49__init_agent_reports.sql
@@ -0,0 +1,6 @@
+insert into public.agent_reports (id, comitent_sum, commentary, document_type, number, paid, printed, remuniration_sum, sent, status, sum, time, company_id, contractor_id)
+values  (1, 1, 'Comment 1', '.odt', '1', 1, 1, 1, 1, 'Status 1', 1, '2021-08-10 12:15:00.000000', 1, 1),
+        (2, 1, 'Comment 1', '.odt', '1', 1, 1, 1, 1, 'Status 2', 1, '2021-08-10 12:15:00.000000', 1, 1),
+        (3, 1, 'Comment 1', '.odt', '1', 1, 1, 1, 1, 'Status 3', 1, '2021-08-10 12:15:00.000000', 1, 1),
+        (4, 1, 'Comment 1', '.odt', '1', 1, 1, 1, 1, 'Status 4', 1, '2021-08-10 12:15:00.000000', 1, 1),
+        (5, 1, 'Comment 1', '.odt', '1', 1, 1, 1, 1, 'Status 5', 1, '2021-08-10 12:15:00.000000', 1, 1);
