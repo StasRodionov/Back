d7c21b51864ccad79d485a0efa7ff2cc72ab25d0
Stanislav Dusiak <stanislav.dusiak@gmail.com>
2021-08-12 15:16:26 +0300

    fixWarehouseMapper(add@Spy)

--- src/test/java/com/trade_accounting/services/impl/WarehouseServiceTest.java
+++ src/test/java/com/trade_accounting/services/impl/WarehouseServiceTest.java
@@ -3,10 +3,13 @@ package com.trade_accounting.services.impl;
 import com.trade_accounting.models.Warehouse;
 import com.trade_accounting.models.dto.WarehouseDto;
 import com.trade_accounting.repositories.WarehouseRepository;
+import com.trade_accounting.utils.mapper.PriceListMapper;
+import com.trade_accounting.utils.mapper.WarehouseMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -26,6 +29,9 @@ class WarehouseServiceTest {
 
     @Mock
     private WarehouseRepository warehouseRepository;
+
+    @Spy
+    private WarehouseMapper warehouseMapper;
 
     @InjectMocks
     private WarehouseServiceImpl warehouseService;
