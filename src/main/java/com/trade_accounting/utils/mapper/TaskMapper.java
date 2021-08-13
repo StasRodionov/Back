473fb55998c23bba75cb5899834927462d164344
UsoltsevE <zhk23@mail.ru>
2021-08-13 14:29:16 +0300

    Done v2 (добавил тесты методов класса TaskServiceImpl)

--- src/main/java/com/trade_accounting/utils/mapper/TaskMapper.java
+++ src/main/java/com/trade_accounting/utils/mapper/TaskMapper.java
@@ -1,25 +1,59 @@
 package com.trade_accounting.utils.mapper;
 
 import com.trade_accounting.models.Task;
+import com.trade_accounting.models.TaskComment;
 import com.trade_accounting.models.dto.TaskDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
+
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
-    //Task *Test
-    @Mappings({
-            @Mapping(source = "taskEmployee.id", target = "employeeId"),
-            @Mapping(source = "taskAuthor.id", target = "taskAuthorId")
-    })
-    TaskDto taskToTaskDto(Task task);
 
-    @Mappings({
-            @Mapping(source = "employeeId", target = "taskEmployee.id"),
-            @Mapping(source = "taskAuthorId", target = "taskAuthor.id"),
-            @Mapping(target = "creationDateTime", ignore = true),
-            @Mapping(target = "deadlineDateTime", ignore = true)
-    })
-    Task taskDtoToTask(TaskDto taskDto);
+    default Task taskDtoToTask(TaskDto taskdto) {
+
+        if (taskdto == null) {
+            return null;
+        }
+
+        return Task.builder()
+                .id(taskdto.getId())
+                .description(taskdto.getDescription())
+                .completed(taskdto.isCompleted())
+                .build();
+    }
+
+    default TaskDto taskToTaskDto(Task task) {
+
+        TaskDto taskDto = new TaskDto();
+        if (task == null) {
+            return null;
+        } else {
+            taskDto.setId(task.getId());
+            taskDto.setDescription(task.getDescription());
+            taskDto.setCompleted(task.isCompleted());
+            taskDto.setCreationDateTime(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(task.getCreationDateTime()));
+            taskDto.setDeadlineDateTime(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(task.getDeadlineDateTime()));
+
+            if (task.getTaskEmployee() == null) {
+                return null;
+            } else {
+                taskDto.setEmployeeId(task.getTaskEmployee().getId());
+
+                if (task.getTaskAuthor() == null) {
+                    return null;
+                } else {
+                    taskDto.setTaskAuthorId(task.getTaskAuthor().getId());
+
+                    if (task.getTaskComments() == null) {
+                        return null;
+                    } else {
+                        taskDto.setTaskCommentsIds(task.getTaskComments().stream().map(TaskComment::getId).collect(Collectors.toList()));
+                        return taskDto;
+                    }
+                }
+            }
+        }
+    }
 }
