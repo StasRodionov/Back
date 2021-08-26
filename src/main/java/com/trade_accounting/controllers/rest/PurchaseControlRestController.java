29ac6b9eff5ce597efcd4f1a792b7525258286c5
Daniil210 <den.iv0008@gmail.com>
2021-08-26 18:02:50 +0300

    PurchaseControllerRestController fix

--- src/main/java/com/trade_accounting/controllers/rest/PurchaseControlRestController.java
+++ src/main/java/com/trade_accounting/controllers/rest/PurchaseControlRestController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -77,7 +78,7 @@ public class PurchaseControlRestController {
         return ResponseEntity.ok(purchaseControlService.create(purchaseControlDto));
     }
 
-    @PostMapping
+    @PutMapping
     @ApiOperation(value = "update", notes = "Изменение информации о закупке")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Информация о закупке обновлена"),
