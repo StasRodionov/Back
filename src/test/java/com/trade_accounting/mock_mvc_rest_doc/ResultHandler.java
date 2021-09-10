9cd0810fadfd83ce10e334187d3e01069d610f1f
verafil <9601604@gmail.com>
2021-09-10 07:47:21 +0300

    del handler

--- src/test/java/com/trade_accounting/mock_mvc_rest_doc/ResultHandler.java
+++
@@ -1,7 +0,0 @@
-package com.trade_accounting.mock_mvc_rest_doc;
-
-import org.springframework.test.web.servlet.MvcResult;
-
-public interface ResultHandler {
-    void handle(MvcResult result) throws Exception;
-}
