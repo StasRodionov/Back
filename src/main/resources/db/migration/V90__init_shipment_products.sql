51f1116b405bcae08f7b40d6f1dddf5971aae307
Nickshugo <setos.rus@gmail.com>
2021-09-20 10:39:33 +0300

    add Money Profit Loss

--- src/main/resources/db/migration/V90__init_shipment_products.sql
+++ src/main/resources/db/migration/V90__init_shipment_products.sql
@@ -1,4 +1,4 @@
 INSERT INTO shipment_products (id, product_id, amount, price)
 VALUES (1, 1, 10, 100),
-       (2, 1, 15, 150),
-       (3, 1, 40, 95);
+       (2, 2, 15, 150),
+       (3, 3, 40, 95);
---
+++ src/main/resources/db/migration/V92__init_acceptance_production.sql
@@ -0,0 +1,5 @@
+INSERT INTO acceptance_productions (id, product_id, amount, price)
+VALUES (1, 1, 1000, 20),
+       (2, 2, 1000, 15),
+       (3, 3, 1000, 10),
+       (4, 4, 1000, 5);
