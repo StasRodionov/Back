1b7f0201e43dbebabe0fba528ec5897ce73bd2af
Nickshugo <setos.rus@gmail.com>
2021-09-22 14:35:17 +0300

    add init loss and loss products

---
+++ src/main/resources/db/migration/V100__init_loss_loss_products.sql
@@ -0,0 +1,11 @@
+INSERT INTO loss_loss_products (loss_id, loss_products_id)
+values  (1, 1),
+        (1, 2),
+        (1, 3),
+        (2, 4),
+        (2, 5),
+        (2, 6),
+        (3, 7),
+        (3, 8),
+        (3, 9),
+        (3, 10);
