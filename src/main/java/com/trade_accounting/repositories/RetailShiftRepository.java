08645cce83e0f0cf67594b5c9898fcfb0e2f7f98
Artemii131994 <artemii131994@gmail.com>
2021-10-07 21:35:19 +0400

    add_searchRetailShift

--- src/main/java/com/trade_accounting/repositories/RetailShiftRepository.java
+++ src/main/java/com/trade_accounting/repositories/RetailShiftRepository.java
@@ -2,10 +2,20 @@ package com.trade_accounting.repositories;
 
 import com.trade_accounting.models.InternalOrder;
 import com.trade_accounting.models.RetailShift;
+import com.trade_accounting.models.TechnicalOperations;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RetailShiftRepository extends JpaRepository<RetailShift, Long> {
+import java.util.List;
 
+@Repository
+public interface RetailShiftRepository extends JpaRepository<RetailShift, Long>, JpaSpecificationExecutor<RetailShift> {
+
+    @Query("from RetailShift t" +
+            " where lower(concat(t.id, ' ', t.comment))" +
+            " like lower(concat('%', :req, '%'))")
+    List<RetailShift> search(@Param("req") String request);
 }
