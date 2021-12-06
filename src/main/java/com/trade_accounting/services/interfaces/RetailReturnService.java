d6f416671f19cbfef287e86bd010fa8567563f79
Gwinhard <artmnvigor@gmail.com>
2021-12-06 23:18:06 +0700

    realize filter for RetailReturns tab

--- src/main/java/com/trade_accounting/services/interfaces/RetailReturnService.java
+++ src/main/java/com/trade_accounting/services/interfaces/RetailReturnService.java
@@ -2,6 +2,12 @@ package com.trade_accounting.services.interfaces;
 
 import com.trade_accounting.models.RetailReturn;
 import com.trade_accounting.models.dto.RetailReturnDto;
+import com.trade_accounting.models.dto.RetailSalesDto;
 
-public interface RetailReturnService extends AbstractService<RetailReturnDto>, SearchableService<RetailReturn,RetailReturnDto> {
+import java.util.List;
+
+public interface RetailReturnService extends AbstractService<RetailReturnDto>, SearchableService<RetailReturn, RetailReturnDto> {
+
+    List<RetailReturnDto> search(String searchTerm);
+
 }
