794551f420ffd7d13312cc592481b6d77bf51830
Vishnevskaia <a.vishnevskaia@gmail.com>
2021-12-20 16:37:12 +0300

    Тесты для ShipmentProductServiceImpl

--- src/test/java/com/trade_accounting/services/impl/Stubs/dto/ShipmentProductDtoStubs.java
+++ src/test/java/com/trade_accounting/services/impl/Stubs/dto/ShipmentProductDtoStubs.java
@@ -1,10 +1,7 @@
 package com.trade_accounting.services.impl.Stubs.dto;
 
-import com.trade_accounting.models.dto.RetailSalesDto;
 import com.trade_accounting.models.dto.ShipmentProductDto;
-import com.trade_accounting.services.impl.Stubs.model.RetailSalesModelStubs;
 import com.trade_accounting.services.impl.Stubs.model.ShipmentProductModelStubs;
-import com.trade_accounting.utils.mapper.RetailSalesMapper;
 import com.trade_accounting.utils.mapper.ShipmentProductMapper;
 import org.mapstruct.factory.Mappers;
 
