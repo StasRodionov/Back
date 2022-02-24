68d58106046a888914b69467f703ca5906bb7ed3
APAXNC <dobro240chan@gmail.com>
2022-02-24 22:27:27 +0300

    Доработать интерфейс LossMapper в части добавления полей в метод toModel. Доработал метод toModel в части добавления полей.

--- src/main/java/com/trade_accounting/utils/mapper/finance/LossMapper.java
+++ src/main/java/com/trade_accounting/utils/mapper/finance/LossMapper.java
@@ -1,33 +1,48 @@
 package com.trade_accounting.utils.mapper.finance;
 
+import com.trade_accounting.models.entity.company.Company;
 import com.trade_accounting.models.entity.finance.Loss;
 import com.trade_accounting.models.entity.finance.LossProduct;
 import com.trade_accounting.models.dto.finance.LossDto;
+import com.trade_accounting.models.entity.warehouse.Warehouse;
 import org.mapstruct.Mapper;
 
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface LossMapper {
 
     default Loss toModel(LossDto lossDto) {
+
         if (lossDto == null) {
             return null;
         }
 
+        List<LossProduct> lossProducts =
+                lossDto.getLossProductsIds()
+                        .stream()
+                        .map(element -> LossProduct.builder().id(element).build())
+                        .collect(Collectors.toList());
+
         return Loss.builder()
                 .id(lossDto.getId())
+                .date(LocalDateTime.parse(lossDto.getDate()))
+                .warehouse(Warehouse.builder().id(lossDto.getWarehouseId()).build())
+                .company(Company.builder().id(lossDto.getCompanyId()).build())
                 .isSent(lossDto.getIsSent())
                 .isPrint(lossDto.getIsPrint())
                 .comment(lossDto.getComment())
+                .lossProducts(lossProducts)
                 .isRecyclebin(lossDto.getIsRecyclebin())
                 .build();
     }
 
-
     default LossDto toDto(Loss loss) {
         LossDto lossDto = new LossDto();
+
         if (loss == null) {
             return null;
         } else {
