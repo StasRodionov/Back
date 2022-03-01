f07d33e74c0ec27f0d6926b0de7d933dde08c61d
vlade <Ceo1949@yandex.ru>
2022-03-01 19:38:56 +0800

    Реализовал модульные тесты для RetailCloudCheckServiceImpl, RetailMakingServiceImpl и RetailShiftServiceImpl. Второй коммит - уже адекватный. Изменения в сервисах в методах getById, create и update, прописал RetailCloudCheckMapper, RetailMakingMapper и RetailShiftMapper в utils/mapper/retail. Поменял тип Long на BigDecimal в поле total класса RetailCloudCheckDto и в других классах, где используется total

--- src/main/java/com/trade_accounting/services/impl/retail/RetailMakingServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/retail/RetailMakingServiceImpl.java
@@ -1,15 +1,21 @@
 package com.trade_accounting.services.impl.retail;
 
-import com.trade_accounting.models.entity.retail.RetailMaking;
 import com.trade_accounting.models.dto.retail.RetailMakingDto;
+import com.trade_accounting.models.entity.company.Company;
+import com.trade_accounting.models.entity.retail.RetailMaking;
+import com.trade_accounting.models.entity.retail.RetailStore;
+import com.trade_accounting.repositories.company.CompanyRepository;
 import com.trade_accounting.repositories.retail.RetailMakingRepository;
+import com.trade_accounting.repositories.retail.RetailStoreRepository;
 import com.trade_accounting.services.interfaces.retail.RetailMakingService;
 import com.trade_accounting.utils.mapper.retail.RetailMakingMapper;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -17,13 +23,16 @@ import java.util.stream.Collectors;
 public class RetailMakingServiceImpl implements RetailMakingService {
 
     private final RetailMakingRepository retailMakingRepository;
-
     private final RetailMakingMapper retailMakingMapper;
+    private final RetailStoreRepository retailStoreRepository;
+    private final CompanyRepository companyRepository;
 
     public RetailMakingServiceImpl(RetailMakingRepository retailMakingRepository,
-                                   RetailMakingMapper retailMakingMapper) {
+                                   RetailMakingMapper retailMakingMapper, RetailStoreRepository retailStoreRepository, CompanyRepository companyRepository) {
         this.retailMakingRepository = retailMakingRepository;
         this.retailMakingMapper = retailMakingMapper;
+        this.retailStoreRepository = retailStoreRepository;
+        this.companyRepository = companyRepository;
     }
 
     @Override
@@ -35,16 +44,18 @@ public class RetailMakingServiceImpl implements RetailMakingService {
 
     @Override
     public RetailMakingDto getById(Long id) {
-        return retailMakingMapper.toDto(retailMakingRepository.getOne(id));
+        Optional<RetailMaking> retailCloudCheck = retailMakingRepository.findById(id);
+        return retailMakingMapper.toDto(retailCloudCheck.orElse(new RetailMaking()));
     }
 
     @Override
     public RetailMakingDto create(RetailMakingDto dto) {
-        RetailMaking retailMaking = retailMakingRepository.save(retailMakingMapper.toModel(dto));
-        if (dto.getId() == null) {
-            dto.setId(retailMaking.getId());
-        }
-        return retailMakingMapper.toDto(retailMaking);
+        RetailMaking retailMaking = retailMakingMapper.toModel(dto);
+        Optional<RetailStore> retailStore = retailStoreRepository.findById(dto.getRetailStoreId());
+        Company company = companyRepository.getCompaniesById(dto.getRetailStoreId());
+        retailMaking.setRetailStore(retailStore.orElse(null));
+        retailMaking.setCompany(company);
+        return retailMakingMapper.toDto(retailMakingRepository.save(retailMaking));
     }
 
     @Override
@@ -55,19 +66,27 @@ public class RetailMakingServiceImpl implements RetailMakingService {
     @Override
     public void deleteById(Long id) {
         retailMakingRepository.deleteById(id);
-
     }
 
     @Override
     public List<RetailMakingDto> search(Specification<RetailMaking> spec) {
-        return executeSearch(retailMakingRepository, retailMakingMapper::toDto, spec);
+        List<RetailMaking> retailReturnList = retailMakingRepository.findAll(spec);
+
+        List<RetailMakingDto> retailMakingDtoList = new ArrayList<>();
+        for (RetailMaking retailMaking : retailReturnList) {
+            retailMakingDtoList.add(retailMakingMapper.toDto(retailMaking));
+        }
+        return retailMakingDtoList;
     }
 
     @Override
     public List<RetailMakingDto> search(String search) {
-        return retailMakingRepository.search(search).stream()
-                .map(retailMakingMapper::toDto)
-                .collect(Collectors.toList());
+        if ("null".equals(search) || search.isEmpty()) {
+            List<RetailMaking> list = retailMakingRepository.findAll();
+            return list.stream().map(retailMakingMapper::toDto).collect(Collectors.toList());
+        } else {
+            List<RetailMaking> list = retailMakingRepository.search(search);
+            return list.stream().map(retailMakingMapper::toDto).collect(Collectors.toList());
+        }
     }
-
 }
