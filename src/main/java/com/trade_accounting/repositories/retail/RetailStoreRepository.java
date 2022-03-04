0f2fde8d4f7b2a0caba5cceb39eda08a69236636
vlade <Ceo1949@yandex.ru>
2022-03-04 12:06:01 +0800

    Реализовал фильтр RetailStore -> @GetMapping("/searchRetailStoreByFilter") в RetailStoreRestController, а также прописал @GetMapping("/search"). В интерфейсе RetailStoreRepository добавил Query, в сервисе RetailStoreServiceImpl прописал методы search и еще поменял пароль в ресурсах тестов с postgres на root для проверки тестов на рестконтроллерах

--- src/main/java/com/trade_accounting/repositories/retail/RetailStoreRepository.java
+++ src/main/java/com/trade_accounting/repositories/retail/RetailStoreRepository.java
@@ -2,9 +2,18 @@ package com.trade_accounting.repositories.retail;
 
 import com.trade_accounting.models.entity.retail.RetailStore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RetailStoreRepository extends JpaRepository<RetailStore, Long> {
+import java.util.List;
 
+@Repository
+public interface RetailStoreRepository extends JpaRepository<RetailStore, Long>, JpaSpecificationExecutor<RetailStore> {
+
+    @Query("from RetailStore s" +
+            " where lower(concat(s.id, ' '))" +
+            " like lower(concat('%', :req, '%'))")
+    List<RetailStore> search(@Param("req") String request);
 }
