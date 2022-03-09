1443e3587a1ac6b6442143e90ba61a8224fe62c7
artem <artem-3010@yandex.ru>
2022-03-10 00:34:44 +0500

    Написать тесты для вкладки "Показатель" -> "Аудит" тест сервиса

--- src/test/java/com/trade_accounting/services/impl/indicators/AuditServiceImplTest.java
+++ src/test/java/com/trade_accounting/services/impl/indicators/AuditServiceImplTest.java
@@ -4,13 +4,10 @@ import com.trade_accounting.Stubs.ModelStubs;
 import com.trade_accounting.models.dto.indicators.AuditDto;
 import com.trade_accounting.models.entity.indicators.Audit;
 import com.trade_accounting.repositories.indicators.AuditRepository;
-import com.trade_accounting.services.interfaces.indicators.AuditService;
-import com.trade_accounting.utils.mapper.client.DepartmentMapperImpl;
 import com.trade_accounting.utils.mapper.indicators.AuditMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,7 +15,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +30,9 @@ class AuditServiceImplTest {
     @Mock
     AuditRepository auditRepository;
 
+    @Autowired
+    AuditMapper mapper;
+
     @Test
     void getAll() {
         List<Audit> repoTestStub = Stream.of(
@@ -37,14 +40,44 @@ class AuditServiceImplTest {
                 ModelStubs.getAudit(2L),
                 ModelStubs.getAudit(3L),
                 ModelStubs.getAudit(4L)
-        )
-                .collect(Collectors.toList());
+        ).collect(Collectors.toList());
         when(auditRepository.findAll()).thenReturn(repoTestStub);
         List<AuditDto> actual = auditService.getAll();
         assertNotNull(actual, "Expected not null");
-        assertEquals(actual.size(), repoTestStub.size());
+        assertEquals(repoTestStub.size(), actual.size());
         for (int i = 0; i < repoTestStub.size(); i++) {
             assertEquals(repoTestStub.get(0).getId(), actual.get(0).getId());
         }
+    }
+
+
+    @Test
+    void getById() {
+        Audit expect = ModelStubs.getAudit(1L);
+        when(auditRepository.getOne(1L)).thenReturn(expect);
+        AuditDto actual = auditService.getById(1L);
+        assertNotNull(actual, "Expected not null");
+        assertEquals(expect.getId(), actual.getId());
+    }
+
+    @Test
+    void create() {
+        saveOrUpdate();
+    }
+
+    @Test
+    void update() {
+        saveOrUpdate();
+    }
+
+    private void saveOrUpdate(){
+        when(auditRepository.save(any(Audit.class)))
+                .thenReturn(ModelStubs.getAudit(1L));
+
+        AuditDto actual = auditService
+                .create(ModelStubs.getAuditDto(1L));
+
+        assertEquals(1L, actual.getId());
+        verify(auditRepository).save(any(Audit.class));
     }
 }
