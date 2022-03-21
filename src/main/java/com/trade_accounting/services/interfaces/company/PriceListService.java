15a117d551a6b16ed559d54baf0328a391d001bf
RinatMans <91965034+RinatMans@users.noreply.github.com>
2022-03-21 16:51:38 +0500

    новый фильтр не привязанный к гриду

--- src/main/java/com/trade_accounting/services/interfaces/company/PriceListService.java
+++ src/main/java/com/trade_accounting/services/interfaces/company/PriceListService.java
@@ -2,6 +2,8 @@ package com.trade_accounting.services.interfaces.company;
 
 import com.trade_accounting.models.dto.company.PriceListDto;
 import com.trade_accounting.services.interfaces.util.AbstractService;
+import java.util.List;
 
 public interface PriceListService extends AbstractService<PriceListDto> {
+    List<PriceListDto> getAllForFilter(String string);
 }
