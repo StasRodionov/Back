9ef89eabc733f7dde6805f8f285a2c909b7a9b89
Victor <hools252@mail.ru>
2022-04-14 19:20:35 +0300

    start commit

--- src/main/java/com/trade_accounting/services/impl/client/AccountServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/client/AccountServiceImpl.java
@@ -2,9 +2,12 @@ package com.trade_accounting.services.impl.client;
 
 import com.trade_accounting.models.dto.client.AccountDto;
 import com.trade_accounting.models.dto.client.EmployeeDto;
-import com.trade_accounting.models.entity.client.Account;
-import com.trade_accounting.models.entity.client.Employee;
+import com.trade_accounting.models.entity.client.*;
+import com.trade_accounting.models.entity.util.Image;
 import com.trade_accounting.repositories.client.AccountRepository;
+import com.trade_accounting.repositories.client.DepartmentRepository;
+import com.trade_accounting.repositories.client.EmployeeRepository;
+import com.trade_accounting.repositories.client.PositionRepository;
 import com.trade_accounting.services.interfaces.client.AccountService;
 import com.trade_accounting.services.interfaces.client.EmployeeService;
 import com.trade_accounting.utils.mapper.client.AccountMapper;
@@ -14,6 +17,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 
 @Service
 @Transactional
@@ -24,6 +31,10 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final AccountMapper accountMapper;
     private final EmployeeMapper employeeMapper;
+
+    private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
+    private final PositionRepository positionRepository;
 
     @Override
     public AccountDto create(AccountDto account, EmployeeDto employee) {
@@ -36,27 +47,35 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account getByEmployee(Employee employee) {
-        return null;
+        return employee.getAccount();
     }
 
     @Override
     public List<EmployeeDto> getAll() {
-        return null;
+
+        return employeeRepository.findAll().stream()
+                .map(employeeMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public EmployeeDto getById(Long id) {
-        return null;
+     Optional<Employee> employee = employeeRepository.findById(id);
+        return employeeMapper.toDto(employee.orElse(new Employee()));
     }
 
     @Override
     public EmployeeDto create(EmployeeDto dto) {
-        return null;
+        Employee employee = employeeMapper.toModel(dto);
+        return employeeMapper.toDto(employeeRepository.save(employee));
     }
 
     @Override
-    public EmployeeDto update(EmployeeDto dto) {
-        return null;
+    public EmployeeDto update(EmployeeDto empDto) {
+
+      Employee emp = employeeMapper.toModel(empDto);
+         return null;
+
     }
 
     @Override
