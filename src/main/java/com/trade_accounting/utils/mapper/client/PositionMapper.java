b6468b730614ec61d6de66ecdec65798c7049124
bakhtievailmira <bakhtieva.ilmira@gmail.com>
2022-04-21 18:23:34 +0300

    Починила вкладку Профиль-Настройки-Сотрудники

--- src/main/java/com/trade_accounting/utils/mapper/client/PositionMapper.java
+++ src/main/java/com/trade_accounting/utils/mapper/client/PositionMapper.java
@@ -3,11 +3,13 @@ package com.trade_accounting.utils.mapper.client;
 import com.trade_accounting.models.entity.client.Position;
 import com.trade_accounting.models.dto.client.PositionDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PositionMapper {
     //Position
     Position toModel(PositionDto position);
 
+    @Mapping(target = "id", source = "position.id")
     PositionDto toDto(Position position);
 }
