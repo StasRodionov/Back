e8714cb9ac0ea6509cde0397fe91ff3c59512ae9
kkn89 <konstantinkoblyakov@gmail.com>
2022-04-22 12:24:28 +0300

    Починил маппинги в пакетах: company, fias, finance, indicators, invoice.

--- src/main/java/com/trade_accounting/utils/mapper/finance/ReturnAmountByProductMapper.java
+++ src/main/java/com/trade_accounting/utils/mapper/finance/ReturnAmountByProductMapper.java
@@ -13,9 +13,7 @@ public interface ReturnAmountByProductMapper {
 //    @Mapping(source = "contractorId", target = "contractor.id")
     ReturnAmountByProduct toModel(ReturnAmountByProductDto returnAmountByProductDto);
 
-    @Mapping(target = "productId", source = "product.id")
-    @Mapping(target = "invoiceId", source = "invoice.id")
-    @Mapping(target = "contractorId", source = "contractor.id")
+
     ReturnAmountByProductDto toDto(ReturnAmountByProduct returnAmountByProduct);
 
 }
