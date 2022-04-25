19144ceaae681dca6e7db0ddbae1a190a0eb15c8
Victor <hools252@mail.ru>
2022-04-25 14:04:58 +0300

    start commit3

---
+++ src/test/java/com/trade_accounting/services/impl/client/AccountServiceImplTest.java
@@ -0,0 +1,130 @@
+package com.trade_accounting.services.impl.client;
+
+import com.trade_accounting.Stubs.ModelStubs;
+import com.trade_accounting.Stubs.dto.client.EmployeeDtoStubs;
+import com.trade_accounting.models.dto.client.DepartmentDto;
+import com.trade_accounting.models.dto.client.EmployeeDto;
+import com.trade_accounting.models.entity.client.Department;
+import com.trade_accounting.models.entity.client.Employee;
+import com.trade_accounting.repositories.client.AccountRepository;
+import com.trade_accounting.repositories.client.EmployeeRepository;
+import com.trade_accounting.utils.mapper.client.EmployeeMapperImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@Slf4j
+class AccountServiceImplTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @Spy
+    private EmployeeMapperImpl employeeMapper;
+
+    @InjectMocks
+    private  AccountServiceImpl accountService;
+
+
+    @Test
+    void create_shouldPassInstructionsSuccessfulCreate(){
+        accountService.create(EmployeeDtoStubs.getEmployeeDto(1L));
+
+        verify(employeeRepository).save(any(Employee.class));
+        log.info("employee was save repository");
+    }
+
+    @Test
+    void getAll_shouldReturnListFilledEmployeeDto(){
+
+        when(employeeRepository.findAll())
+                .thenReturn(
+                        Stream.of(
+                                        ModelStubs.getEmployee(1L),
+                                        ModelStubs.getEmployee(2L),
+                                        ModelStubs.getEmployee(3L)
+                                )
+                                .collect(Collectors.toList())
+                );
+
+        List<EmployeeDto> employeeDtoList = accountService.getAll();
+
+        assertNotNull(employeeDtoList, "Failure - expected that list of employeeDto not null");
+        assertTrue(employeeDtoList.size() > 0, "failure - expected that size of list of employeeDto greater than 0");
+
+        for (EmployeeDto employeeDto : employeeDtoList) {
+              employeeDtoIsCorrectlyInited(employeeDto);
+        }
+
+    }
+
+    @Test
+    void getAll_shouldReturnEmptyListEmployeeDto() {
+        when(employeeRepository.findAll())
+                .thenReturn(new ArrayList<>());
+
+        List<EmployeeDto> employees = accountService.getAll();
+
+        assertNotNull(employees, "failure - expected that a list of employeeDto not null");
+        assertEquals(0, employees.size(), "failure - expected that size of list of employeeDto equals 0");
+    }
+
+    @Test
+    void getById_shouldReturnFilledEmployeeDto() {
+        Optional<Employee> employeeRepo = Optional.of(ModelStubs.getEmployee(1L));
+
+        when(employeeRepository.findById(anyLong()))
+                .thenReturn(employeeRepo);
+
+        EmployeeDto employeeDto = accountService.getById(1L);
+
+        employeeDtoIsCorrectlyInited(employeeDto);
+        log.info("employee get from repository");
+    }
+
+    @Test
+    void update_shouldPassInstructionsSuccessfulUpdate(){
+        accountService.update(EmployeeDtoStubs.getEmployeeDto(1L));
+
+        verify(employeeRepository).save(any(Employee.class));
+        log.info("was succesfully update");
+    }
+
+
+    @Test
+    void deleteById_shouldPassInstructionsSuccessfulDelete() {
+        accountService.deleteById(1L);
+
+        verify(accountRepository).deleteById(1L);
+        log.info("was delete from repository");
+    }
+
+
+    void employeeDtoIsCorrectlyInited(EmployeeDto employeeDto) {
+        assertNotNull(employeeDto, "failure - fail in passed employeeDto");
+        assertNotNull(employeeDto.getId(), "failure - fail in field 'id' into employeeDto");
+        assertNotNull(employeeDto.getEmail(), "failure - fail in field 'email' into employeeDto");
+        assertNotNull(employeeDto.getSortNumber(), "failure - fail in field 'sortNumber' into employeeDto");
+    }
+
+
+}
