5c25a668fe2f4187cc07cd60a982a7b7308aeff4
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-06-24 16:08:28 +0300

    Добавил новые поля в ProductGroupMapper

--- src/main/java/com/trade_accounting/utils/mapper/warehouse/ProductGroupMapper.java
+++ src/main/java/com/trade_accounting/utils/mapper/warehouse/ProductGroupMapper.java
@@ -8,9 +8,15 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ProductGroupMapper {
     @Mapping(source = "parentId", target = "parent.id")
+    @Mapping(source = "taxSystemId", target = "taxSystem.id")
+    @Mapping(source = "employeeId", target = "employee.id")
+    @Mapping(source = "departmentId", target = "department.id")
     ProductGroup toModel(ProductGroupDto productGroupDto);
 
     @Mapping(source = "parent.id", target = "parentId")
+    @Mapping(source = "taxSystem.id", target = "taxSystemId")
+    @Mapping(source = "employee.id", target = "employeeId")
+    @Mapping(source = "department.id", target = "departmentId")
     ProductGroupDto toDto(ProductGroup productGroup);
 
 }
