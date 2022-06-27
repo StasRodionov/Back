11c20ce8ec198f8e668e9eec1a7fa3d37626e5dc
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-06-27 19:22:20 +0300

    Добавил ProductGroupRestControllerTest и методы testExistence и testGetAll

---
+++ src/test/java/com/trade_accounting/controllers/rest/warehouse/KitRestControllerTest.java
@@ -0,0 +1,111 @@
+package com.trade_accounting.controllers.rest.warehouse;
+
+
+import com.google.gson.Gson;
+import com.trade_accounting.models.dto.warehouse.KitDto;
+import com.trade_accounting.models.dto.warehouse.ProductGroupDto;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = {"spring.config.location = src/test/resources/application-test.yml"})
+@Sql(value = "/Kit-before.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@WithUserDetails(value = "karimogon@mail.ru")
+@AutoConfigureRestDocs(outputDir = "target/snippets", uriScheme = "http", uriPort = 4444)
+public class KitRestControllerTest {
+
+    @Autowired
+    private KitRestController kitRestController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @SneakyThrows
+    @Test
+    void testExistence() {
+        assertNotNull(kitRestController, "KitRestController is null");
+    }
+
+    @SneakyThrows
+    @Test
+    void testGetAll() {
+        mockMvc.perform(get("/api/kit"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(authenticated())
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"));
+    }
+
+    @SneakyThrows
+    @Test
+    void testGetById() {
+        KitDto kitDto = KitDto.builder()
+                .id(1L)
+                .name("Джентельменский набор №1")
+                .description("Костюм тройка, туфли, галстук")
+                .archive(false)
+                .weight(BigDecimal.valueOf(0.0))
+                .volume(BigDecimal.valueOf(0.0))
+                .purchasePrice(BigDecimal.valueOf(33.33))
+                .unitId(1L)
+                .contractorId(1L)
+                .taxSystemId(1L)
+                .itemNumber(4)
+                .saleTax("20")
+                .employeeId(1L)
+                .departmentId(1L)
+                .accessAll(false)
+                .build();
+
+        String kitDtoJson = new Gson().toJson(kitDto);
+
+        mockMvc.perform(get("/api/kit/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(authenticated())
+                .andExpect(content().json(kitDtoJson))
+                .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"));
+    }
+
+
+//    INSERT INTO kits (
+//    id,
+//    name,
+//    description,
+//    archive,
+//    weight,
+//    volume,
+//    purchase_price,
+//    unit_id,
+//    contractor_id,
+//    tax_system_id,
+//    item_number,
+//    sale_tax,
+//    employee_id,
+//    department_id,
+//    access_all)
+//VALUES (1, 'Джентельменский набор №1', 'Костюм тройка, туфли, галстук', false, 0, 0, 33.33, 1, 1, 1, 4, '20', 1, 1, false ),
+
+
+
+}
