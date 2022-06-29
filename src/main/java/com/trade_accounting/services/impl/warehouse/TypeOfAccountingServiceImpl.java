7dfdb780886d39c87f2431e41bed51e4fa6b926c
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-06-29 15:37:59 +0300

    Добавил TypeOfAccountingServiceImpl + getAll

--- src/main/java/com/trade_accounting/services/impl/warehouse/TypeOfAccountingServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/warehouse/TypeOfAccountingServiceImpl.java
@@ -2,20 +2,30 @@ package com.trade_accounting.services.impl.warehouse;
 
 
 import com.trade_accounting.models.dto.warehouse.TypeOfAccountingDto;
+import com.trade_accounting.repositories.warehouse.TypeOfAccountingRepository;
 import com.trade_accounting.services.interfaces.warehouse.TypeOfAccountingService;
+import com.trade_accounting.utils.mapper.warehouse.TypeOfAccountingMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class TypeOfAccountingServiceImpl implements TypeOfAccountingService {
+
+    private final TypeOfAccountingRepository typeOfAccountingRepository;
+
+    private final TypeOfAccountingMapper typeOfAccountingMapper;
+
     @Override
     public List<TypeOfAccountingDto> getAll() {
-        return null;
+        return typeOfAccountingRepository.findAll().stream()
+                .map(typeOfAccountingMapper::toDto)
+                .collect(Collectors.toList());
     }
 
     @Override
