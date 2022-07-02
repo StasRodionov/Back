5916a8729ba075fbfa9fd54268e333c044515b05
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-07-02 19:27:08 +0300

    Добавил в TypeOfPackingRepository метод getOne, что бы при получении несуществующего id получать в БД null без исключений

--- src/main/java/com/trade_accounting/repositories/warehouse/TypeOfPackingRepository.java
+++ src/main/java/com/trade_accounting/repositories/warehouse/TypeOfPackingRepository.java
@@ -3,8 +3,14 @@ package com.trade_accounting.repositories.warehouse;
 
 import com.trade_accounting.models.entity.warehouse.TypeOfPacking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TypeOfPackingRepository extends JpaRepository<TypeOfPacking, Long> {
+
+    @Override
+    @Query("SELECT c FROM TypeOfPacking c WHERE c.id = :id")
+    TypeOfPacking getOne(@Param("id") Long id);
 }
