750aa28b652333586e3d3d58e3180e9ae5275371
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-07-03 19:52:43 +0300

    Добавил миграции

--- src/main/resources/db/migration/V158__init_saleTax.sql
+++ src/main/resources/db/migration/V158__init_saleTax.sql
@@ -13,8 +13,8 @@ INSERT INTO sale_tax (
 VALUES (1, 'без НДС', '1'),
        (2, '0%', '2'),
        (3, '10%', '3'),
-       (3, '18%', '4'),
-       (3, '20%', '5');
+       (4, '18%', '4'),
+       (5, '20%', '5');
 
 SELECT setval('sale_tax_id_seq', max(id))
 FROM sale_tax
