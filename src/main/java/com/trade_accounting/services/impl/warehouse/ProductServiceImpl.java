3df381622812ab32cb1331d701abe92ec4d3634c
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-07-06 23:19:23 +0300

    Добавил в ProductMapper конвертацию в модель + сделал обход null для документов товара

--- src/main/java/com/trade_accounting/services/impl/warehouse/ProductServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/warehouse/ProductServiceImpl.java
@@ -114,11 +114,13 @@ public class ProductServiceImpl implements ProductService {
         savedFiles.forEach(file -> file.setProduct(product));
         product.setFiles(savedFiles);
         List<ProductPrice> prices = new ArrayList<>();
+
         try {
             product.getProductPrices()
                     .forEach(productPrice -> prices.add(productPriceRepository.getOne(productPrice.getId())));
             product.setProductPrices(prices);
         } catch (NullPointerException ignored) {}
+
         productRepository.saveAndFlush(product);
         return dto;
     }
