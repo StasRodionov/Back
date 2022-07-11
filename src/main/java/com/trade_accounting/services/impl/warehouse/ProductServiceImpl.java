14ebf071b8c0327335a8280fb3ef788a7fd7c598
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-07-11 14:42:12 +0300

    Связал Product и SaleTax

--- src/main/java/com/trade_accounting/services/impl/warehouse/ProductServiceImpl.java
+++ src/main/java/com/trade_accounting/services/impl/warehouse/ProductServiceImpl.java
@@ -202,19 +202,7 @@ public class ProductServiceImpl implements ProductService {
         if (dto.getSaleTaxId() != null) {
             product.setSaleTaxEntity(saleTaxRepository.getOne(dto.getSaleTaxId()));
         } else {
-            product.setSaleTaxEntity(null);
-        }
-
-        if (dto.getSaleTaxId() != null) {
-            product.setSaleTaxEntity(saleTaxRepository.getOne(dto.getSaleTaxId()));
-        } else {
-            product.setSaleTaxEntity(null);
-        }
-
-        if (dto.getSaleTaxId() != null) {
-            product.setSaleTaxEntity(saleTaxRepository.getOne(dto.getSaleTaxId()));
-        } else {
-            product.setSaleTaxEntity(null);
+            product.setTypeOfPacking(null);
         }
 
         if (dto.getSaleTaxId() != null) {
